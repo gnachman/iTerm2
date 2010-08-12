c5a5df298153197bdb00ae84175cabb281321a25
georgen@google.com <georgen@google.com@5c829b59-20ba-c297-566c-b9ce016b2d6a>
2010-08-12 20:58:01 +0000

    Update release process

--- release.sh
+++ release.sh
@@ -11,9 +11,11 @@ DATE=$(date +"%a, %d %b %Y %H:%M:%S %z")
 VERSION=$(date +"%Y%m%d")
 cp ../../appcasts/template.xml /tmp
 cat /tmp/template.xml | \
-
 sed -e "s/%VER%/${VERSION}/" | \
 sed -e "s/%DATE%/${DATE}/" | \
 sed -e "s/%NAME%/${NAME}/" | \
 sed -e "s/%LENGTH%/$LENGTH/" |
 sed -e "s,%SIG%,${SIG}," > ../../appcasts/testing.xml
+echo "Go upload the iTerm-${NAME}.zip, then run:"
+echo "svn commit -m $NAME"
+echo "cd .. && ./release2.sh $NAME"
