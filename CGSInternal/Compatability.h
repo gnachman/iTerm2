6fff453d6221b329f956442306dd99d68bf816c6
georgen@google.com <georgen@google.com@5c829b59-20ba-c297-566c-b9ce016b2d6a>
2010-08-13 18:16:11 +0000

    Rename Compability to Compatability, and add Compatability.h which was missed in the previous checkin.

---
+++ CGSInternal/Compatability.h
@@ -0,0 +1,21 @@
+/*
+ *  Compatability.h
+ *  iTerm
+ *
+ *  Created by Marc on 10.08.10.
+ *  Copyright 2010 Marc Haisenko. All rights reserved.
+ *
+ */
+
+#pragma once
+
+#include <Carbon/Carbon.h>
+
+#ifndef CG_EXTERN_C_BEGIN
+#define CG_EXTERN_C_BEGIN
+#define CG_EXTERN_C_END
+#endif
+
+#ifndef CG_EXTERN
+#define CG_EXTERN
+#endif
