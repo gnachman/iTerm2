0bd129af1d6210e97367b7d5bf0788ea8cd44316
gnachman <gnachman@5c829b59-20ba-c297-566c-b9ce016b2d6a>
2010-09-03 07:10:58 +0000

    alpha7

--- release.sh
+++ release.sh
@@ -3,10 +3,10 @@
 set -x
 NAME=$1
 cd build/Deployment
-zip -r iTerm-${NAME}.zip iTerm.app
+zip -r iTerm2-${NAME}.zip iTerm.app
 vi ../../appcasts/testing_changes.html
-LENGTH=$(ls -l iTerm-${NAME}.zip | awk '{print $5}')
-ruby "/Users/georgen/Downloads/Sparkle 1.5b6/Extras/Signing Tools/sign_update.rb" iTerm-${NAME}.zip $PRIVKEY > /tmp/sig.txt
+LENGTH=$(ls -l iTerm2-${NAME}.zip | awk '{print $5}')
+ruby "/Users/georgen/Downloads/Sparkle 1.5b6/Extras/Signing Tools/sign_update.rb" iTerm2-${NAME}.zip $PRIVKEY > /tmp/sig.txt
 SIG=$(cat /tmp/sig.txt)
 DATE=$(date +"%a, %d %b %Y %H:%M:%S %z")
 VERSION=$(date +"%Y%m%d")
@@ -17,6 +17,6 @@ sed -e "s/%DATE%/${DATE}/" | \
 sed -e "s/%NAME%/${NAME}/" | \
 sed -e "s/%LENGTH%/$LENGTH/" |
 sed -e "s,%SIG%,${SIG}," > ../../appcasts/testing.xml
-echo "Go upload the iTerm-${NAME}.zip, then run:"
+echo "Go upload the iTerm2-${NAME}.zip, then run:"
 echo "svn commit -m $NAME"
 echo "cd .. && ./release2.sh $NAME"
