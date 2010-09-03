c0d879f5b60244c5463ac90e631c7c12cf9af8d6
gnachman <gnachman@5c829b59-20ba-c297-566c-b9ce016b2d6a>
2010-09-03 07:25:17 +0000

    Alpha7 try 2

--- release2.sh
+++ release2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+set -x
 NAME=$1
 export EDITOR=vi
 svn copy -m $NAME trunk https://iterm2.googlecode.com/svn/tags/$NAME
+cp trunk/appcasts/* fork/appcasts/
 echo Run this:
+svn commit -m $NAME
