e80c18438dc9d7059717f7cd81e1330326adbb03
ionic@ionic.de <ionic@ionic.de@5c829b59-20ba-c297-566c-b9ce016b2d6a>
2010-09-07 16:43:47 +0000

    stringByExpandingTildeInPath is automatically called by stringByStandardizingPath, no need to do this redundantly.

--- PTYTask.m
+++ PTYTask.m
@@ -480,7 +480,8 @@ setup_tty_param(
 					setenv([key UTF8String], [value UTF8String], 1);
 			}
 		}
-		chdir([[[[env objectForKey:@"PWD"] stringByExpandingTildeInPath] stringByStandardizingPath] UTF8String]);
+    // Note: stringByStandardizingPath will automatically call stringByExpandingTildeInPath.
+		chdir([[[env objectForKey:@"PWD"] stringByStandardizingPath] UTF8String]);
 		sts = execvp(argpath, (char* const*)argv);
 
 		/* exec error */
