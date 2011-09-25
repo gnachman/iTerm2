bf9dc01591c370b34958d35f9c0f5448272b9a28
George Nachman <git.xyz@georgester.com>
2011-09-24 17:13:13 -0700

    Rework Coprocess to not use NSTask (kept getting SIGPIPEs for no discernable reason).  Add Run Coprocess to Shell menu. Have PTYTask do routing of coprocess I/O. Rename InteractiveScriptTrigger to CoprocessTrigger. Add visual indicator of coprocess.

---
+++ CoprocessTrigger.h
@@ -0,0 +1,18 @@
+//
+//  InteractiveScriptTrigger.h
+//  iTerm
+//
+//  Created by George Nachman on 9/24/11.
+//  Copyright 2011 Georgetech. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "ScriptTrigger.h"
+
+
+@interface CoprocessTrigger : Trigger {
+
+}
+
+
+@end
---
+++ CoprocessTrigger.m
@@ -0,0 +1,40 @@
+//
+//  InteractiveScriptTrigger.m
+//  iTerm
+//
+//  Created by George Nachman on 9/24/11.
+//  Copyright 2011 Georgetech. All rights reserved.
+//
+
+#import "CoprocessTrigger.h"
+#import "PTYsession.h"
+
+@implementation CoprocessTrigger
+
+- (NSString *)title
+{
+    return @"Run Coprocess…";
+}
+
+- (BOOL)takesParameter
+{
+    return YES;
+}
+
+- (NSString *)paramPlaceholder
+{
+    return @"Enter coprocess command to run";
+}
+
+- (void)executeCommand:(NSString *)command inSession:(PTYSession *)aSession
+{
+    [aSession launchCoprocessWithCommand:command];
+}
+
+- (void)performActionWithValues:(NSArray *)values inSession:(PTYSession *)aSession
+{
+    NSString *command = [self paramWithBackreferencesReplacedWithValues:values];
+    [self executeCommand:command inSession:aSession];
+}
+
+@end
