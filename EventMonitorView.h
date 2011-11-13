71b2dfda23a81b1c974e9b0c547a83d70e2d9129
George Nachman <git.xyz@georgester.com>
2011-11-13 09:37:23 -0800

    Add description to EventMonitorView

--- EventMonitorView.h
+++ EventMonitorView.h
@@ -5,6 +5,8 @@
 //  Created by George Nachman on 11/9/11.
 //  Copyright (c) 2011 George Nachman. All rights reserved.
 //
+//  Pass clicks and taps in a view to PointerPrefsController to set
+//  button/gesture, num clicks, and modifiers.
 
 #import <Cocoa/Cocoa.h>
 
