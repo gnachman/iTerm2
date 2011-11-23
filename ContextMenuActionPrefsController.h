d9b568523b79854a9adddcff2b5285763fde5821
George Nachman <git.xyz@georgester.com>
2011-11-23 08:52:55 -0800

    Add ContextMenuActionPrefsController.?, which was forgotten previously

---
+++ ContextMenuActionPrefsController.h
@@ -0,0 +1,47 @@
+//
+//  ContextMenuActionPrefsController.h
+//  iTerm
+//
+//  Created by George Nachman on 11/18/11.
+//  Copyright 2011 Georgetech. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+typedef enum {
+    kOpenFileContextMenuAction,
+    kOpenUrlContextMenuAction,
+    kRunCommandContextMenuAction,
+    kRunCoprocessContextMenuAction
+} ContextMenuActions;
+
+@protocol ContextMenuActionPrefsDelegate
+
+- (void)contextMenuActionsChanged:(NSArray *)newActions;
+
+@end
+
+
+@interface ContextMenuActionPrefsController : NSWindowController <NSTableViewDelegate, NSTableViewDataSource> {
+    IBOutlet NSTableView *tableView_;
+    IBOutlet NSTableColumn *titleColumn_;
+    IBOutlet NSTableColumn *actionColumn_;
+    IBOutlet NSTableColumn *parameterColumn_;
+    NSMutableArray *model_;
+    NSObject<ContextMenuActionPrefsDelegate> *delegate_;
+    BOOL hasSelection_;
+}
+
+@property (nonatomic, assign) NSObject<ContextMenuActionPrefsDelegate> *delegate;
+@property (nonatomic, assign) BOOL hasSelection;
+
++ (ContextMenuActions)actionForActionDict:(NSDictionary *)dict;
++ (NSString *)titleForActionDict:(NSDictionary *)dict withCaptureComponents:(NSArray *)components;
++ (NSString *)parameterForActionDict:(NSDictionary *)dict withCaptureComponents:(NSArray *)components;
+
+- (IBAction)ok:(id)sender;
+- (void)setActions:(NSArray *)newActions;
+- (IBAction)add:(id)sender;
+- (IBAction)remove:(id)sender;
+
+@end
