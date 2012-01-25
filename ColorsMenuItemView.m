c664b682d706758cb45ae24e2f286b953b71dd59
Andrea Bonomi <andrea.bonomi@gmail.com>
2012-01-25 08:43:23 +0100

    description fix

--- ColorsMenuItemView.m
+++ ColorsMenuItemView.m
@@ -7,7 +7,7 @@
  **
  **  Project: iTerm
  **
- **  Description: Session and window controller for iTerm.
+ **  Description: Colored Tabs.
  **
  **  This program is free software; you can redistribute it and/or modify
  **  it under the terms of the GNU General Public License as published by
