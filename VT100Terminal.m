7086dee634fd3e16a275b29e8b5370bd615e2a35
George Nachman <git.xyz@georgester.com>
2012-02-13 20:14:06 -0800

    Increment tmux version to 0.4

--- VT100Terminal.m
+++ VT100Terminal.m
@@ -734,7 +734,7 @@ static VT100TCC decode_underscore(unsigned char *datap,
         }
 
         if (found && [result.u.string hasPrefix:@"tmux"]) {
-            if ([result.u.string isEqualToString:@"tmux0.3"]) {
+            if ([result.u.string isEqualToString:@"tmux0.4"]) {
                 result.type = UNDERSCORE_TMUX1;
 			} else if ([result.u.string hasPrefix:@"tmux"]) {
 				result.type = UNDERSCORE_TMUX_UNSUPPORTED;
