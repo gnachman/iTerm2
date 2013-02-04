33f002a2114cd704949b8d53f26b34ea0d926f5d
George Nachman <iterm2.xyz@georgester.com>
2013-02-03 19:12:41 -0800

    Uncomment line in continuous build script

--- continuous.sh
+++ continuous.sh
@@ -3,7 +3,7 @@
 set -x
 cd ~/continuous/iTerm2/
 echo Check for updates...
-#git pull origin master | grep "Already up-to-date." && exit
+git pull origin master | grep "Already up-to-date." && exit
 echo Check for build failure...
 (make Nightly > /tmp/continuous.out 2>&1) && exit
 echo "Build failed; send mail."
