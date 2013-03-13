dada095fb35b6e8668db31c4ede6e35bf3f4d0b0
George Nachman <gln@google.com>
2013-03-13 16:58:53 -0700

    Add -y to zip

--- release.sh
+++ release.sh
@@ -22,7 +22,7 @@ mv ~/tmux-for-iTerm2-$COMPACTDATE.tar.gz build/Deployment
 cd build/Deployment
 
 # Create the zip file
-zip -r iTerm2-${NAME}.zip iTerm.app
+zip -ry iTerm2-${NAME}.zip iTerm.app
 
 # Update the list of changes
 vi $SVNDIR/appcasts/testing_changes.html
