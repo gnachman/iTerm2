8c670fd90c4fd4c86f3c54b9074d2c97e78c4e7e
George Nachman <gln@google.com>
2013-09-18 16:43:07 -0700

    Use CGFontGetGlyphAdvances instead of sizeWithAttributes in FontSizeEstimator.m to deal with change in later SDKs

--- FontSizeEstimator.m
+++ FontSizeEstimator.m
@@ -50,7 +50,29 @@
         NSMutableDictionary *dic = [NSMutableDictionary dictionary];
         [dic setObject:aFont forKey:NSFontAttributeName];
         NSSize size = [@"W" sizeWithAttributes:dic];
-        size.width = size.width;
+#if __MAC_OS_X_VERSION_MAX_ALLOWED >= 1080
+        // Something changed at (I think) the 10.8 SDK where 12-pt Courier New's "W" glyph went from
+        // 7 to 8 pixels wide. I want to avoid an angry mob and preserve the existing kerning. This
+        // API seems to do the job, but until we officially transition to the 10.8 sdk I'll keep this
+        // if'ed out. TODO: see if height is affected.
+        CGGlyph glyphs[1];
+        int advances[1];
+        UniChar characters[1];
+        characters[0] = 'W';
+        CTFontRef ctfont = (CTFontRef)aFont;  // Toll-free bridged
+        CGFontRef cgfont = CTFontCopyGraphicsFont(ctfont, NULL);
+        CTFontGetGlyphsForCharacters(ctfont, characters, glyphs, 1);
+        if (CGFontGetGlyphAdvances(cgfont,
+                                   glyphs,
+                                   1,
+                                   advances)) {
+            size.width = advances[0];
+            size.width *= [aFont pointSize];
+            size.width /= CGFontGetUnitsPerEm(cgfont);
+            size.width = floor(size.width);
+        }
+        CGFontRelease(cgfont);
+#endif
         size.height = [aFont ascender] - [aFont descender];
         double baseline = -(floorf([aFont leading]) - floorf([aFont descender]));
         fse.size = size;
