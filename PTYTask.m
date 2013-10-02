426a822e2b0b835445dbc9caca5850aefddbf864
George Nachman <gln@google.com>
2013-10-02 13:21:15 -0700

    Make unblockPipeW nonblocking (pun not intended) to fix bug 2633. The PTYTask thread would get stuck in readTask waiting for the mainThread to complete -[PTYSession readTask:] while the main thread was stuck in -unblock waiting to write to unblockPipeW, which is only read by the PTYTask thread. Since unblockPipeW was blocking, it must have had a lot of pending writes that hadn't been read off, and dropping one on the floor won't matter because the thread will definitely get unblocked.

--- PTYTask.m
+++ PTYTask.m
@@ -115,6 +115,7 @@ static TaskNotifier* taskNotifier = nil;
             return nil;
         }
         fcntl(unblockPipe[0], F_SETFL, O_NONBLOCK);
+        fcntl(unblockPipe[1], F_SETFL, O_NONBLOCK);
         unblockPipeR = unblockPipe[0];
         unblockPipeW = unblockPipe[1];
     }
@@ -537,7 +538,7 @@ setup_tty_param(
     [writeBuffer release];
     [tty release];
     [path release];
-	[command_ release];
+        [command_ release];
 
     @synchronized (self) {
         [[self coprocess] mainProcessDidTerminate];
@@ -564,7 +565,7 @@ static void reapchild(int n)
 
 - (NSString *)command
 {
-	return command_;
+        return command_;
 }
 
 - (void)launchWithPath:(NSString*)progpath
@@ -789,6 +790,9 @@ static void reapchild(int n)
 
     // forward the data to our delegate
     if ([delegate respondsToSelector:@selector(readTask:)]) {
+        // This waitsUntilDone because otherwise we can read data from a child process faster than
+        // we can parse it. The main thread will quickly end up overloaded with calls to readTask:,
+        // never catching up, and never having a chance to draw or respond to input.
         [delegate performSelectorOnMainThread:@selector(readTask:)
                                    withObject:data 
                                 waitUntilDone:YES];
