59306854217b7aafcb1c1ecb44c00f1aeeb9c7ea
George Nachman <gln@google.com>
2013-10-16 11:59:52 -0700

    Remove unused methods from VT100Screen.h

--- VT100Screen.h
+++ VT100Screen.h
@@ -1,32 +1,3 @@
-// -*- mode:objc -*-
-// $Id: VT100Screen.h,v 1.38 2008-09-30 06:21:12 yfabian Exp $
-/*
- **  VT100Screen.h
- **
- **  Copyright (c) 2002, 2003
- **
- **  Author: Fabian, Ujwal S. Setlur
- **         Initial code by Kiichi Kusama
- **
- **  Project: iTerm
- **
- **  Description: Implements the VT100 screen.
- **
- **  This program is free software; you can redistribute it and/or modify
- **  it under the terms of the GNU General Public License as published by
- **  the Free Software Foundation; either version 2 of the License, or
- **  (at your option) any later version.
- **
- **  This program is distributed in the hope that it will be useful,
- **  but WITHOUT ANY WARRANTY; without even the implied warranty of
- **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- **  GNU General Public License for more details.
- **
- **  You should have received a copy of the GNU General Public License
- **  along with this program; if not, write to the Free Software
- **  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- */
-
 #import <Cocoa/Cocoa.h>
 #import "VT100Terminal.h"
 #import "LineBuffer.h"
@@ -117,28 +88,25 @@ void TranslateCharacterSet(screen_char_t *s, int len);
 - (id)initWithTerminal:(VT100Terminal *)terminal;
 - (void)dealloc;
 
-- (NSString *)description;
-
 - (void)setUpScreenWithWidth:(int)width height:(int)height;
 - (void)resizeWidth:(int)new_width height:(int)height;
-- (void)reset;
+
 - (void)resetPreservingPrompt:(BOOL)preservePrompt;
 - (void)resetCharset;
 - (BOOL)usingDefaultCharset;
+
 - (void)setScrollback:(unsigned int)lines;
 - (void)setUnlimitedScrollback:(BOOL)enable;
 - (void)setTerminal:(VT100Terminal *)terminal;
 - (void)setShellTask:(PTYTask *)shell;
 - (void)setSession:(PTYSession *)session;
+
 - (BOOL)vsplitMode;
 - (void)setVsplitMode:(BOOL)mode;
 
 - (PTYTextView *)display;
 - (void)setDisplay:(PTYTextView *)aDisplay;
-
-- (BOOL)blinkingCursor;
 - (void)setBlinkingCursor:(BOOL)flag;
-- (void)processXtermPaste64:(NSString*)commandString;
 - (void)showCursor:(BOOL)show;
 - (void)setPlayBellFlag:(BOOL)flag;
 - (void)setShowBellFlag:(BOOL)flag;
@@ -151,71 +119,32 @@ void TranslateCharacterSet(screen_char_t *s, int len);
 - (void)putToken:(VT100TCC)token;
 - (void)clearBuffer;
 - (void)clearScrollbackBuffer;
+
 - (void)showPrimaryBuffer;
 - (void)showAltBuffer;
-
 - (void)setSendModifiers:(int *)modifiers
                numValues:(int)numValues;
 
 - (void)mouseModeDidChange:(MouseMode)mouseMode;
 
-// internal
 - (void)setString:(NSString *)s ascii:(BOOL)ascii;
 - (void)crlf; // -crlf is called only by tmux integration, so it ignores vsplit mode.
 - (void)linefeed;
 - (void)deleteCharacters:(int)n;
 - (void)backSpace;
-- (void)backTab;
 - (void)setTab;
-- (void)clearTabStop;
-- (BOOL)haveTabStopAt:(int)x;
-- (void)setTabStopAt:(int)x;
-- (void)removeTabStopAt:(int)x;
 - (void)clearScreen;
-- (void)eraseInDisplay:(VT100TCC)token;
-- (void)eraseInLine:(VT100TCC)token;
-- (void)selectGraphicRendition:(VT100TCC)token;
-- (void)cursorLeft:(int)n;
-- (void)cursorRight:(int)n;
-- (void)cursorUp:(int)n;
-- (void)cursorDown:(int)n;
-- (void)cursorToX:(int)x;
-- (void)cursorToY:(int)y;
 - (void)cursorToX:(int)x Y:(int)y;
 - (void)carriageReturn;
 - (void)saveCursorPosition;
 - (void)restoreCursorPosition;
-- (void)setTopBottom:(VT100TCC)token;
 - (void)activateBell;
-- (void)deviceReport:(VT100TCC)token withQuestion:(BOOL)question;
-- (void)deviceAttribute:(VT100TCC)token;
-- (void)secondaryDeviceAttribute:(VT100TCC)token;
-- (void)deleteLines:(int)n;
-- (void)blink;
 
 - (void)setHistory:(NSArray *)history;
 - (void)setAltScreen:(NSArray *)lines;
 - (void)setTmuxState:(NSDictionary *)state;
 
-// Set the char at x,y dirty.
-- (void)setCharDirtyAtX:(int)x Y:(int)y;
-
-// Check if any flag is set at an x,y coordinate in the dirty array
-- (BOOL)isDirtyAtX:(int)x Y:(int)y;
-
-- (void)resetDirty;
 - (void)markAsNeedingCompleteRedraw;
-
-// print to ansi...
-- (BOOL)printToAnsi;
-- (void)setPrintToAnsi:(BOOL)aFlag;
-- (void)printStringToAnsi:(NSString *)aString;
-
-// UI stuff
-- (void)doPrint;
-
-// Is this character double width on this screen?
-- (BOOL)isDoubleWidthCharacter:(unichar)c;
 
 // Set the colors in the prototype char to all text on screen that matches the regex.
 // See kHighlightXxxColor constants at the top of this file for dict keys, values are NSColor*s.
@@ -241,4 +170,3 @@ void TranslateCharacterSet(screen_char_t *s, int len);
 - (void)setAllowTitleReporting:(BOOL)allow;
 
 @end
-
