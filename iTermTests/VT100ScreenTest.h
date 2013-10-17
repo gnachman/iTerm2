d06641e335c3858c8e7971cad5b3af33fb8f180f
George Nachman <gln@google.com>
2013-10-16 21:58:30 -0700

    Remove bogus files from project

---
+++ iTermTests/VT100ScreenTest.h
@@ -0,0 +1,13 @@
+//
+//  VT100ScreenTest.h
+//  iTerm
+//
+//  Created by George Nachman on 10/16/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VT100ScreenTest : NSObject
+
+@end
---
+++ iTermTests/VT100ScreenTest.m
@@ -0,0 +1,13 @@
+//
+//  VT100ScreenTest.m
+//  iTerm
+//
+//  Created by George Nachman on 10/16/13.
+//
+//
+
+#import "VT100ScreenTest.h"
+
+@implementation VT100ScreenTest
+
+@end
