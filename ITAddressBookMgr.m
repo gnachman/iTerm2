bc24b4596002a9f87c593493bcab56e911ffec6e
Sam Doshi <sam@metal-fish.co.uk>
2013-11-23 20:42:30 +0000

    allow the themes to specify if they want sRGB

--- ITAddressBookMgr.m
+++ ITAddressBookMgr.m
@@ -138,7 +138,8 @@
     NSColor* color = [origColor colorUsingColorSpaceName:NSCalibratedRGBColorSpace];
     CGFloat red, green, blue, alpha;
     [color getRed:&red green:&green blue:&blue alpha:&alpha];
-    return [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithFloat:red], @"Red Component",
+    return [NSDictionary dictionaryWithObjectsAndKeys:@"Calibrated", @"Color Space",
+                                                      [NSNumber numberWithFloat:red], @"Red Component",
                                                       [NSNumber numberWithFloat:green], @"Green Component",
                                                       [NSNumber numberWithFloat:blue], @"Blue Component",
                                                       nil];
@@ -146,14 +147,22 @@
 
 + (NSColor*)decodeColor:(NSDictionary*)plist
 {
-    if ([plist count] != 3) {
+    if ([plist count] < 3) {
         return [NSColor blackColor];
     }
 
-    return [NSColor colorWithCalibratedRed:[[plist objectForKey:@"Red Component"] floatValue]
-                                     green:[[plist objectForKey:@"Green Component"] floatValue]
-                                      blue:[[plist objectForKey:@"Blue Component"] floatValue]
-                                     alpha:1.0];
+    NSString *colorSpace = [plist objectForKey:@"Color Space"];
+    if ([colorSpace isEqualToString:@"sRGB"]) {
+        return [NSColor colorWithSRGBRed:[[plist objectForKey:@"Red Component"] floatValue]
+                                   green:[[plist objectForKey:@"Green Component"] floatValue]
+                                    blue:[[plist objectForKey:@"Blue Component"] floatValue]
+                                   alpha:1.0];
+    } else {
+        return [NSColor colorWithCalibratedRed:[[plist objectForKey:@"Red Component"] floatValue]
+                                         green:[[plist objectForKey:@"Green Component"] floatValue]
+                                          blue:[[plist objectForKey:@"Blue Component"] floatValue]
+                                         alpha:1.0];
+    }
 }
 
 - (void)copyProfileToBookmark:(NSMutableDictionary *)dict
