3c6b44a70d8ce0a369c84ec147dc69e93b0af3cb
Sam Doshi <sam@metal-fish.co.uk>
2013-11-24 11:07:31 +0000

    Make sure our fallback is an RGB color
    [NSColor blackColor] returns a color in NSGrayColorSpaceModel, which has
    no red, blue or green components, and will raise an exception if they
    are requested. Instead return black in NSRGBColorSpaceModel.

--- ITAddressBookMgr.m
+++ ITAddressBookMgr.m
@@ -148,7 +148,7 @@
 + (NSColor*)decodeColor:(NSDictionary*)plist
 {
     if ([plist count] < 3) {
-        return [NSColor blackColor];
+        return [NSColor colorWithCalibratedRed:0.0 green:0.0 blue:0.0 alpha:1.0];
     }
 
     NSString *colorSpace = [plist objectForKey:@"Color Space"];
