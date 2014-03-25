bfab9e7a320cecdfbebd80d8878fd3746b905587
bdpdx <brian@balance-software.com>
2014-03-25 00:38:28 -0700

    comment syn -> lnext

--- PTYSession.m
+++ PTYSession.m
@@ -4117,7 +4117,7 @@ static long long timeInTenthsOfSeconds(struct timeval t)
     // 2014.03.25 bd - Control code removal was handled previously in dataByRemovingControlCodes:.
     // The logic here is the same except we operate on NSString instead of NSData.  All control
     // codes except tab (\x09), newline (\x0a), form feed (\x0c), and carriage return (\x0d) are
-    // removed unless we are pasting with literal tabs, in which case we also keep syn (\x16).
+    // removed unless we are pasting with literal tabs, in which case we also keep lnext (\x16).
     NSString *controlCharacters =
         @"\x00\x01\x02\x03\x04\x05\x06\x07\x08"      "\x0b"      "\x0e\x0f"
          "\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f";
