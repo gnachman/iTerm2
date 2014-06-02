7c9fe43eb812cc67f31e68a515444c461cee9c70
Alex Gray <alex@mrgray.com>
2014-06-02 13:28:42 -0400

    just a few embellishments to the (very nice) test setup, purged empty credits.rtf file

--- iTermTests/iTermTests.m
+++ iTermTests/iTermTests.m
@@ -9,12 +9,11 @@
 #import "iTermTests.h"
 #import <objc/runtime.h>
 
-@implementation iTermTest
-@end
+@implementation iTermTest @end
 
-#define DECLARE_TEST(t) \
-@interface t : iTermTest \
-@end
+#define DECLARE_TEST(t) @interface t : iTermTest @end
+#define PERFORM_IF_RESPONDS(SELECT,...) ![test respondsToSelector:@selector(SELECT)] ?: [test SELECT]; NSLog(__VA_ARGS__)
+#define RUN_TEST_CLASSES(...) ({ for (Class x in @[__VA_ARGS__]) RunTestsInObject([[x new] autorelease]); })
 
 DECLARE_TEST(VT100GridTest)
 DECLARE_TEST(VT100ScreenTest)
@@ -23,36 +22,29 @@ DECLARE_TEST(AppleScriptTest)
 DECLARE_TEST(NSStringCategoryTest)
 
 static void RunTestsInObject(iTermTest *test) {
-    NSLog(@"-- Begin tests in %@ --", [test class]);
+
+    NSLog(@"-- Begin tests in %@ --", test.class);
     unsigned int methodCount;
-    Method *methods = class_copyMethodList([test class], &methodCount);
-    for (int i = 0; i < methodCount; i++) {
-        SEL name = method_getName(methods[i]);
-        NSString *stringName = NSStringFromSelector(name);
-        if ([stringName hasPrefix:@"test"]) {
-            if ([test respondsToSelector:@selector(setup)]) {
-                [test setup];
-            }
-            NSLog(@"Running %@", stringName);
-            [test performSelector:name];
-            if ([test respondsToSelector:@selector(teardown)]) {
-                [test teardown];
-            }
-            NSLog(@"Success!");
-        }
+    Method *methods = class_copyMethodList(test.class, &methodCount);
+    for (int i = 0; i < methodCount; i++) { NSString *stringName; SEL selector;
+      selector   = method_getName(methods[i]);
+      stringName = NSStringFromSelector(selector);
+        if (![stringName hasPrefix:@"test"]) continue;
+        PERFORM_IF_RESPONDS(setup, @"Running %@", stringName);
+        [test performSelector:selector];
+        PERFORM_IF_RESPONDS(teardown, @"Success!");
     }
     free(methods);
     NSLog(@"-- Finished tests in %@ --", [test class]);
 }
 
-int main(int argc, const char * argv[]) {
-    RunTestsInObject([[VT100GridTest new] autorelease]);
-    RunTestsInObject([[VT100ScreenTest new] autorelease]);
-    RunTestsInObject([[IntervalTreeTest new] autorelease]);
-    RunTestsInObject([[NSStringCategoryTest new] autorelease]);
-    RunTestsInObject([[AppleScriptTest new] autorelease]);
+int main(int argc, const char * argv[]) { return
 
-    NSLog(@"All tests passed");
-    return 0;
+  RUN_TEST_CLASSES(   VT100GridTest.class,
+                    VT100ScreenTest.class,
+                   IntervalTreeTest.class,
+               NSStringCategoryTest.class,
+                    AppleScriptTest.class),
+
+                    NSLog(@"All tests passed"), 0;
 }
-
