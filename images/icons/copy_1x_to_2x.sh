b2c01739155b5c236e551e4db821e0273f0339fb
George Nachman <github.xyz@georgester.com>
2014-07-29 18:47:58 -0400

    Update icons

---
+++ images/icons/copy_1x_to_2x.sh
@@ -0,0 +1,15 @@
+# Create the following icons:
+# Filename             Size in pixels
+# icon_16x16.png       16x16
+# icon_16x16@2x.png    32x32
+# icon_32x32@2x.png    64x64
+# icon_128x128.png     128x128
+# icon_128x128@2x.png  256x256
+# icon_256x256@2x.png  512x512
+# icon_512x512@2x.png  1024x1024
+#
+# And run this script to fill in the missing ones.
+
+cp icon_16x16@2x.png icon_32x32.png
+cp icon_128x128@2x.png icon_256x256.png
+cp icon_256x256@2x.png icon_512x512.png
