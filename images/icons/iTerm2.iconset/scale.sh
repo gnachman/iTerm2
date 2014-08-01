dcfcd8ecaece44a1d4965a868335174a03278b9f
George Nachman <github.xyz@georgester.com>
2014-08-01 11:29:28 -0700

    Fix up 128x128 icon, delete duplicate icon files

--- images/icons/iTerm2.iconset/scale.sh
+++ images/icons/iTerm2.iconset/scale.sh
@@ -1,7 +1,7 @@
 
 convert -size 512x512 icon_512x512@2x.png -scale 50% icon_256x256@2x.png
 convert -size 512x512 icon_256x256@2x.png -scale 50% icon_128x128@2x.png
-convert -size 512x512 icon_128x128@2x.png -scale 50% icon_128x128x.png
+convert -size 512x512 icon_128x128@2x.png -scale 50% icon_128x128.png
 convert -size 512x512 icon_128x128@2x.png -scale 50% icon_32x32@2x.png
 convert -size 512x512 icon_32x32@2x.png -scale 50% icon_16x16@2x.png
 convert -size 512x512 icon_16x16@2x.png -scale 50% icon_16x16.png
