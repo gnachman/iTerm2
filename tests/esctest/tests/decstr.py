67649a5aee8d18a9b666da75b22d40274f6360a0
George Nachman <gnachman@gmail.com>
2015-03-02 16:16:14 -0800

    Add knownBug annotation for test_DECSTR_DECOM

--- tests/esctest/tests/decstr.py
+++ tests/esctest/tests/decstr.py
@@ -54,6 +54,7 @@ class DECSTRTests(object):
     escio.Write("b")
     AssertScreenCharsInRectEqual(Rect(1, 1, 1, 1), [ "b" ])
 
+  @knownBug(terminal="iTerm2", reason="iTerm2 fails to reset DECOM on DECSTR.")
   def test_DECSTR_DECOM(self):
     # Define a scroll region
     esccmd.DECSTBM(3, 4)
