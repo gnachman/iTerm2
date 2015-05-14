a8b67367a9d30494e629d8bd3066a5cb03c7cfbf
George Nachman <georgen@google.com>
2015-05-14 14:01:18 -0700

    Allow ^N and ^P to work in popups

--- sources/Popup.h
+++ sources/Popup.h
@@ -21,7 +21,7 @@
 - (PTYTextView *)popupVT100TextView;
 - (void)popupInsertText:(NSString *)text;
 // Return YES if the delegate handles it, NO if Popup should handle it.
-- (BOOL)popupKeyDown:(NSEvent *)event currentValue:(NSString *)value;
+- (BOOL)popupHandleSelector:(SEL)selector string:(NSString *)string currentValue:(NSString *)currentValue;
 - (void)popupWillClose:(Popup *)popup;
 
 @end
