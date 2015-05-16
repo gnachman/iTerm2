67e86f4ecb1dab02c6b69396a12c70a94ce6e866
George Nachman <gnachman@gmail.com>
2015-05-15 18:42:19 -0700

    Ensure that ITERM_SESSION_ID gets unique pane numbers (it didn't if a pane was inserted before another pane). Issue 2269

--- sources/PTYSession.m
+++ sources/PTYSession.m
@@ -1084,10 +1084,10 @@ static NSTimeInterval kMinimumPartialLineTriggerCheckInterval = 0.5;
     }
 
     NSWindowController<iTermWindowController> *pty = [_tab realParentWindow];
-    NSString *itermId = [NSString stringWithFormat:@"w%dt%dp%d",
+    NSString *itermId = [NSString stringWithFormat:@"w%dt%dp%lu",
                          [pty number],
                          [_tab realObjectCount] - 1,
-                         [_tab indexOfSessionView:[self view]]];
+                         (unsigned long)_tab.sessions.count];
     env[@"ITERM_SESSION_ID"] = itermId;
     if (_profile[KEY_NAME]) {
         env[@"ITERM_PROFILE"] = [_profile[KEY_NAME] stringByPerformingSubstitutions:substitutions];
