8e1991e53943edaec04bedbb8978eebff32394bc
George Nachman <georgen@google.com>
2015-06-02 16:06:02 -0700

    Make adhoc a little safer and easier to use. Do a clean build first and retry ssh since it often times out waiting for a password

--- tools/adhoc.sh
+++ tools/adhoc.sh
@@ -2,6 +2,7 @@
 COMPACTDATE=$(date +"%Y%m%d_%H%M%S")
 VERSION=$(cat version.txt | sed -e "s/%(extra)s/$COMPACTDATE/")
 NAME=$(echo $VERSION | sed -e "s/\\./_/g")-adhoc
+make clean
 make release
 rm -rf build/Deployment/iTerm.app
 mv build/Deployment/iTerm2.app build/Deployment/iTerm.app
@@ -9,7 +10,8 @@ codesign --deep -s "Developer ID Application: GEORGE NACHMAN" -f "build/Deployme
 codesign --verify --verbose "build/Deployment/iTerm.app" || die "Signature not verified"
 pushd build/Deployment
 zip -ry iTerm2-${NAME}.zip iTerm.app
-scp iTerm2-${NAME}.zip gnachman@iterm2.com:iterm2.com/adhocbuilds/
+scp iTerm2-${NAME}.zip gnachman@iterm2.com:iterm2.com/adhocbuilds/ || \
+  scp iTerm2-${NAME}.zip gnachman@iterm2.com:iterm2.com/adhocbuilds/
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git checkout -b adhoc_$VERSION
 git commit -am "Adhoc build $VERSION"
