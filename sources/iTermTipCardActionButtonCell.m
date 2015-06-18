59035a463c336f998feca45161fb1d7aa06cc1b3
George Nachman <gnachman@gmail.com>
2015-06-17 20:46:40 -0700

    Fix indents, remove dead code

--- sources/iTermTipCardActionButtonCell.m
+++ sources/iTermTipCardActionButtonCell.m
@@ -11,41 +11,36 @@
 @implementation iTermTipCardActionButtonCell
 
 + (NSColor *)blueColor {
-  return [NSColor colorWithCalibratedRed:0.25 green:0.25 blue:0.75 alpha:1];
-}
-
-- (void)drawBezelWithFrame:(NSRect)frame inView:(NSView *)controlView {
-  [[NSColor blackColor] set];
-  NSFrameRect(frame);
+    return [NSColor colorWithCalibratedRed:0.25 green:0.25 blue:0.75 alpha:1];
 }
 
 - (void)drawInteriorWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
-  NSColor *foregroundColor = self.isHighlighted ? [NSColor whiteColor] : [self.class blueColor];
-  NSColor *backgroundColor = self.isHighlighted ? [self.class blueColor] : [NSColor whiteColor];
-  [backgroundColor set];
-  NSRectFill(cellFrame);
+    NSColor *foregroundColor = self.isHighlighted ? [NSColor whiteColor] : [self.class blueColor];
+    NSColor *backgroundColor = self.isHighlighted ? [self.class blueColor] : [NSColor whiteColor];
+    [backgroundColor set];
+    NSRectFill(cellFrame);
 
-  [[NSColor colorWithCalibratedWhite:0.85 alpha:1] set];
-  NSRectFill(NSMakeRect(NSMinX(cellFrame), 0, NSWidth(cellFrame), 0.5));
+    [[NSColor colorWithCalibratedWhite:0.85 alpha:1] set];
+    NSRectFill(NSMakeRect(NSMinX(cellFrame), 0, NSWidth(cellFrame), 0.5));
 
-  NSPoint imageOrigin = NSMakePoint(11, round((cellFrame.size.height - _icon.size.height) / 2));
-  NSSize size = self.icon.size;
-  CGFloat imageOffset = size.width + 7;
-  [self.icon drawInRect:NSMakeRect(imageOrigin.x, imageOrigin.y, size.width, size.height)
-               fromRect:NSMakeRect(0, 0, size.width, size.height)
-              operation:NSCompositeSourceOver
-               fraction:1
-         respectFlipped:YES
-                  hints:nil];
+    NSPoint imageOrigin = NSMakePoint(11, round((cellFrame.size.height - _icon.size.height) / 2));
+    NSSize size = self.icon.size;
+    CGFloat imageOffset = size.width + 7;
+    [self.icon drawInRect:NSMakeRect(imageOrigin.x, imageOrigin.y, size.width, size.height)
+                 fromRect:NSMakeRect(0, 0, size.width, size.height)
+                operation:NSCompositeSourceOver
+                 fraction:1
+           respectFlipped:YES
+                    hints:nil];
 
-  NSColor *textColor = foregroundColor;
-  NSFont *font = [NSFont fontWithName:@"Helvetica Neue" size:14];
-  NSRect textRect = cellFrame;
-  textRect.origin.x += _inset.width + imageOffset;
-  textRect.origin.y += _inset.height;
-  [self.title drawInRect:textRect withAttributes:@{ NSFontAttributeName: font,
-                                                     NSForegroundColorAttributeName: textColor }];
-
+    NSColor *textColor = foregroundColor;
+    NSFont *font = [NSFont fontWithName:@"Helvetica Neue" size:14];
+    NSRect textRect = cellFrame;
+    textRect.origin.x += _inset.width + imageOffset;
+    textRect.origin.y += _inset.height;
+    [self.title drawInRect:textRect withAttributes:@{ NSFontAttributeName: font,
+                                                      NSForegroundColorAttributeName: textColor }];
+    
 }
 
 @end
