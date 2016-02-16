829fc9cddfe4f9e1e037bb47d035ca619c035169
George Nachman <gnachman@gmail.com>
2016-02-15 21:24:09 -0800

    Change how weak references work to be much safer. Objects must opt-in to weak referencing. The previous technique was a failure because associated objects are autoreleased.

--- sources/iTermFullScreenWindowManager.h
+++ sources/iTermFullScreenWindowManager.h
@@ -7,11 +7,12 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "iTermWeakReference.h"
 
 @interface iTermFullScreenWindowManager : NSObject
 
 - (instancetype)initWithClass:(Class)class enterFullScreenSelector:(SEL)selector;
 
-- (void)makeWindowEnterFullScreen:(NSWindow *)window;
+- (void)makeWindowEnterFullScreen:(NSWindow<iTermWeaklyReferenceable> *)window;
 
 @end
