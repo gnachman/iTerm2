a27129a022ca12605ee40ed1f125fdf58cbf6417
George Nachman <gnachman@gmail.com>
2016-02-28 20:05:10 -0800

    Write tests for APS

--- sources/iTermAutomaticProfileSwitcher.h
+++ sources/iTermAutomaticProfileSwitcher.h
@@ -21,10 +21,19 @@ NS_ASSUME_NONNULL_BEGIN
 
 // The session that uses automatic profile switching should implement this protocol.
 @protocol iTermAutomaticProfileSwitcherDelegate<NSObject>
+
+// Make the |savedProfile| into the current profile, respecting overridden fields, etc.
 - (void)automaticProfileSwitcherLoadProfile:(iTermSavedProfile *)savedProfile;
+
+// Returns the current profile, including local changes.
 - (Profile *)automaticProfileSwitcherCurrentProfile;
+
+// Returns the current profile. Only the name and GUID are needed.
 - (iTermSavedProfile *)automaticProfileSwitcherCurrentSavedProfile;
+
+// Returns all profiles in user defaults. Exposed for testing.
 - (NSArray<Profile *> *)automaticProfileSwitcherAllProfiles;
+
 @end
 
 // This class encapsulates the logic needed by automatic profile switching.
@@ -32,6 +41,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 // You should set this or nothing will work.
 @property(nonatomic, assign) id<iTermAutomaticProfileSwitcherDelegate> delegate;
+@property(nonatomic, readonly) NSString *profileStackString;  // For tests
 
 - (instancetype)initWithDelegate:(id<iTermAutomaticProfileSwitcherDelegate>)delegate NS_DESIGNATED_INITIALIZER;
 - (instancetype)initWithDelegate:(id<iTermAutomaticProfileSwitcherDelegate>)delegate
