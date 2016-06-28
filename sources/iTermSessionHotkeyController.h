f408fef88e21f6a53c1d798531dda06a9f0498e7
George Nachman <gnachman@gmail.com>
2016-06-27 22:40:15 -0700

    Add the session hotkey controller to manage registration of per-session hotkeys

---
+++ sources/iTermSessionHotkeyController.h
@@ -0,0 +1,13 @@
+//
+//  iTermSessionHotkeyController.h
+//  iTerm2
+//
+//  Created by George Nachman on 6/27/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iTermSessionHotkeyController : NSObject
+
+@end
---
+++ sources/iTermSessionHotkeyController.m
@@ -0,0 +1,13 @@
+//
+//  iTermSessionHotkeyController.m
+//  iTerm2
+//
+//  Created by George Nachman on 6/27/16.
+//
+//
+
+#import "iTermSessionHotkeyController.h"
+
+@implementation iTermSessionHotkeyController
+
+@end
