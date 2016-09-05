7762e53ac6a3a93d829e8498ba379abbe37172b2
George Nachman <gnachman@gmail.com>
2016-09-05 11:30:31 -0700

    Nil out the delegate of the profile name field in the edit current session prefs panel because it gets made first responder from a delayed perform after its delegate is released (the delayed perform is in openToProfileWithGuid:selectGeneralTab:). When the text field tries to become first responder it checks with its delegate, and that dies when zombies are enabled.

--- sources/ProfilesGeneralPreferencesViewController.m
+++ sources/ProfilesGeneralPreferencesViewController.m
@@ -64,6 +64,11 @@ static const NSInteger kInitialDirectoryTypeAdvancedTag = 3;
     IBOutlet NSButton *_copyProfleToSession;
 }
 
+- (void)dealloc {
+    _profileNameFieldForEditCurrentSession.delegate = nil;
+    [super dealloc];
+}
+
 - (void)awakeFromNib {
     PreferenceInfo *info;
     
