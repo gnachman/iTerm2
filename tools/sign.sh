29817cbb00d755c247c7071cfac7a6580f7b13b1
George Nachman <gnachman@gmail.com>
2016-10-18 22:11:40 -0700

    Codesign manually

--- tools/sign.sh
+++ tools/sign.sh
@@ -1,4 +1,3 @@
 set -x
 security unlock-keychain -p "$ITERM_KEYCHAIN_PASSWORD" "$ITERM_KEYCHAIN"
-codesign --deep -s "H7V7XYVQ7D" -f build/Deployment/iTerm.app
-codesign --keychain "$ITERM_KEYCHAIN" --deep -s "H7V7XYVQ7D" -f build/Nightly/iTerm2.app
+/usr/bin/codesign --deep --force --sign 3E1298F974EB540E3D1D905AA99612231919845E --requirements '=designated => anchor apple generic  and identifier "$self.identifier" and ((cert leaf[field.1.2.840.113635.100.6.1.9] exists) or ( certificate 1[field.1.2.840.113635.100.6.2.6] exists and certificate leaf[field.1.2.840.113635.100.6.1.13] exists  and certificate leaf[subject.OU] = "H7V7XYVQ7D" ))' --timestamp=none "build/Nightly/iTerm2.app"
