e8deed28d61c3f51e2ca87fd88dc7a3eb6ef89ac
George Nachman <gln@whatsapp.com>
2016-11-02 10:27:36 -0700

    There was a duplicate dash in the Find URLs regex which didn't do anything. Removed. Mentioned in a comment in issue 4377.

--- sources/iTermAdvancedSettingsModel.m
+++ sources/iTermAdvancedSettingsModel.m
@@ -100,7 +100,7 @@ DEFINE_BOOL(doNotSetCtype, NO, @"Terminal: Never set the CTYPE environment varia
 DEFINE_FLOAT(smartCursorColorBgThreshold, 0.5, @"Terminal: Threshold for Smart Cursor Color for background color (0 to 1).\n0 means the cursor’s background color will always be the cell’s text color, while 1 means it will always be black or white.");
 DEFINE_FLOAT(smartCursorColorFgThreshold, 0.75, @"Terminal: Threshold for Smart Cursor Color for text color (0 to 1).\n0 means the cursor’s text color will always be the cell’s background color, while 1 means it will always be black or white.");
 DEFINE_STRING(findUrlsRegex,
-              @"https?://([a-z0-9A-Z]+(:[a-zA-Z0-9]+)?@)?[-a-z0-9A-Z\\-]+(\\.[-a-z0-9A-Z\\-]+)*"
+              @"https?://([a-z0-9A-Z]+(:[a-zA-Z0-9]+)?@)?[a-z0-9A-Z\\-]+(\\.[a-z0-9A-Z\\-]+)*"
               @"((:[0-9]+)?)(/[a-zA-Z0-9;:/\\.\\-_+%~?&amp;@=#\\(\\)]*)?",
               @"Terminal: Regular expression for “Find URLs” command.");
 DEFINE_FLOAT(echoProbeDuration, 0.5, @"Terminal: Amount of time to wait while testing if echo is on (seconds).\nThis is used by the password manager to ensure you're at a password prompt.");
