f4082825f3dfa52db08d660ec4821ab6c5bca3fc
George Nachman <gnachman@gmail.com>
2016-11-10 19:27:42 -0800

    Unlock the system keychain in a continued attempt to fix signing

--- tools/sign.sh
+++ tools/sign.sh
@@ -1,4 +1,5 @@
 set -x
-security unlock-keychain -p "$ITERM_KEYCHAIN_PASSWORD" "$ITERM_KEYCHAIN"
+#security unlock-keychain -p "$ITERM_KEYCHAIN_PASSWORD" "$ITERM_KEYCHAIN"
+security unlock-keychain -p "$ITERM_KEYCHAIN_PASSWORD" 
 codesign --deep -s "H7V7XYVQ7D" -f build/Deployment/iTerm.app
 codesign --keychain "$ITERM_KEYCHAIN" --deep -s "H7V7XYVQ7D" -f build/Nightly/iTerm2.app
