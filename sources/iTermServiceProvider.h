83ecb47f67e6f27e932c47c48b20bf75e0d3073a
George Nachman <gnachman@gmail.com>
2016-12-10 22:22:02 -0800

    Clean up finder service code a bit

---
+++ sources/iTermServiceProvider.h
@@ -0,0 +1,13 @@
+//
+//  iTermServiceProvider.h
+//  iTerm2
+//
+//  Created by liupeng on 08/12/2016.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iTermServiceProvider : NSObject
+
+@end
