cf687d7c5435aa68b92b2ba73a6f5f2277573f23
George Nachman <gnachman@gmail.com>
2016-12-14 09:34:41 -0800

    Adhoc build 3.0.20161214_093403

--- sources/iTermFileDescriptorServer.c
+++ sources/iTermFileDescriptorServer.c
@@ -88,10 +88,10 @@ int iTermSelect(int *fds, int count, int *results) {
             }
             FD_SET(fds[i], &readset);
         }
-        syslog(LOG_DEBUG, "Calling select...");
+        syslog(LOG_NOTICE, "Calling select...");
         result = select(max + 1, &readset, NULL, NULL, NULL);
         theError = errno;
-        syslog(LOG_DEBUG, "select returned %d, error = %s", result, strerror(theError));
+        syslog(LOG_NOTICE, "select returned %d, error = %s", result, strerror(theError));
     } while (result == -1 && theError == EINTR);
 
     int n = 0;
@@ -119,7 +119,7 @@ int iTermFileDescriptorServerAccept(int socketFd) {
 }
 
 static int SendFileDescriptorAndWait(int connectionFd) {
-    syslog(LOG_DEBUG, "send master fd and child pid %d", (int)gChildPid);
+    syslog(LOG_NOTICE, "send master fd and child pid %d", (int)gChildPid);
     int rc = SendMessageAndFileDescriptor(connectionFd, &gChildPid, sizeof(gChildPid), 0);
     if (rc <= 0) {
         syslog(LOG_NOTICE, "send failed %s", strerror(errno));
@@ -127,14 +127,14 @@ static int SendFileDescriptorAndWait(int connectionFd) {
         return 0;
     }
 
-    syslog(LOG_DEBUG, "All done. Waiting for client to disconnect or child to die.");
+    syslog(LOG_NOTICE, "All done. Waiting for client to disconnect or child to die.");
     int fds[2] = { gPipe[0], connectionFd };
     int results[2];
     iTermSelect(fds, sizeof(fds) / sizeof(*fds), results);
-    syslog(LOG_DEBUG, "select returned. child dead=%d, connection closed=%d", results[0], results[1]);
+    syslog(LOG_NOTICE, "select returned. child dead=%d, connection closed=%d", results[0], results[1]);
     close(connectionFd);
 
-    syslog(LOG_DEBUG, "Connection closed.");
+    syslog(LOG_NOTICE, "Connection closed.");
     // If the pipe has been written to then results[0] will be nonzero. That
     // means the child process has died and we can terminate. The server's
     // termination signals the client that the child is dead.
@@ -170,10 +170,10 @@ int iTermFileDescriptorServerSocketBindListen(const char *path) {
         umask(oldMask);
         return -1;
     }
-    syslog(LOG_DEBUG, "bind() created %s", local.sun_path);
+    syslog(LOG_NOTICE, "bind() created %s", local.sun_path);
 
     if (listen(socketFd, kMaxConnections) == -1) {
-        syslog(LOG_DEBUG, "listen() failed: %s", strerror(errno));
+        syslog(LOG_NOTICE, "listen() failed: %s", strerror(errno));
         umask(oldMask);
         return -1;
     }
@@ -183,15 +183,15 @@ int iTermFileDescriptorServerSocketBindListen(const char *path) {
 
 static int Initialize(char *path, pid_t childPid) {
     openlog("iTerm2-Server", LOG_PID | LOG_NDELAY, LOG_USER);
-    setlogmask(LOG_UPTO(LOG_DEBUG));
+    setlogmask(LOG_UPTO(LOG_NOTICE));
     gPath = strdup(path);
     // We get this when iTerm2 crashes. Ignore it.
-    syslog(LOG_DEBUG, "Installing SIGHUP handler.");
+    syslog(LOG_NOTICE, "Installing SIGHUP handler.");
     signal(SIGHUP, SIG_IGN);
 
     pipe(gPipe);
 
-    syslog(LOG_DEBUG, "Installing SIGCHLD handler.");
+    syslog(LOG_NOTICE, "Installing SIGCHLD handler.");
     gChildPid = childPid;
     signal(SIGCHLD, SigChildHandler);
     signal(SIGUSR1, SigUsr1Handler);
@@ -200,7 +200,7 @@ static int Initialize(char *path, pid_t childPid) {
     sigset_t signal_set;
     sigemptyset(&signal_set);
     sigaddset(&signal_set, SIGCHLD);
-    syslog(LOG_DEBUG, "Unblocking SIGCHLD.");
+    syslog(LOG_NOTICE, "Unblocking SIGCHLD.");
     sigprocmask(SIG_UNBLOCK, &signal_set, NULL);
 
     return 0;
@@ -208,16 +208,16 @@ static int Initialize(char *path, pid_t childPid) {
 
 static void MainLoop(char *path) {
     // Listen on a Unix Domain Socket.
-    syslog(LOG_DEBUG, "Entering main loop.");
+    syslog(LOG_NOTICE, "Entering main loop.");
     int socketFd;
     do {
-        syslog(LOG_DEBUG, "Calling iTermFileDescriptorServerSocketBindListen.");
+        syslog(LOG_NOTICE, "Calling iTermFileDescriptorServerSocketBindListen.");
         socketFd = iTermFileDescriptorServerSocketBindListen(path);
         if (socketFd < 0) {
-            syslog(LOG_DEBUG, "iTermFileDescriptorServerSocketBindListen failed");
+            syslog(LOG_NOTICE, "iTermFileDescriptorServerSocketBindListen failed");
             return;
         }
-        syslog(LOG_DEBUG, "Calling PerformAcceptActivity");
+        syslog(LOG_NOTICE, "Calling PerformAcceptActivity");
     } while (!PerformAcceptActivity(socketFd));
 }
 
@@ -226,15 +226,15 @@ int iTermFileDescriptorServerRun(char *path, pid_t childPid, int connectionFd) {
     signal(SIGPIPE, SIG_IGN);
     int rc = Initialize(path, childPid);
     if (rc) {
-        syslog(LOG_DEBUG, "Initialize failed with code %d", rc);
+        syslog(LOG_NOTICE, "Initialize failed with code %d", rc);
     } else {
-        syslog(LOG_DEBUG, "Sending file descriptor and waiting on initial connection");
+        syslog(LOG_NOTICE, "Sending file descriptor and waiting on initial connection");
         if (!SendFileDescriptorAndWait(connectionFd)) {
             MainLoop(path);
         }
         // MainLoop never returns, except by dying on a signal.
     }
-    syslog(LOG_DEBUG, "Unlink %s", path);
+    syslog(LOG_NOTICE, "Unlink %s", path);
     unlink(path);
     return 1;
 }
