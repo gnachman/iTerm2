3b2e0a6d768c101271029e079cdd13728c16f597
George Nachman <gnachman@gmail.com>
2016-12-30 13:56:55 -0800

    Fix how the path gets set in after_failure

--- ci/after_failure.sh
+++ ci/after_failure.sh
@@ -4,11 +4,10 @@ set -x
 
 if ls /tmp/failed-* 1> /dev/null 2>&1; then
   cp tests/imgcat /tmp
-  export PATH=($PATH $PWD/tests)
+  export PATH=$PATH:$PWD/tests
   cd /tmp
   source /tmp/diffs > diffs.txt
   tar cvfz failed-images.tgz failed-*.png diffs.txt
-  whereis curl
   /usr/bin/curl -F "file=@failed-images.tgz" https://file.io
 fi
 
