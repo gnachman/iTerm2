gnachman/iTerm2

commit 392ab06855351e78558de9d6b626b8923efefeea

Fix a crash where selection/mark ranges has length 0 prior to trimming. Not sure what the cause was, but it's better to tolerate it and drop the subselection/mark than assert and die

sources/VT100Screen.m
@@ -4009,16 +4009,15 @@ static void SwapInt(int *a, int *b) {
     return result;
 }
 
-- (void)trimSelectionFromStart:(VT100GridCoord)start
+- (BOOL)trimSelectionFromStart:(VT100GridCoord)start
                            end:(VT100GridCoord)end
                       toStartX:(VT100GridCoord *)startPtr
-                        toEndX:(VT100GridCoord *)endPtr
-{
+                        toEndX:(VT100GridCoord *)endPtr {
     if (start.x < 0 || end.x < 0 ||
         start.y < 0 || end.y < 0) {
         *startPtr = start;
         *endPtr = end;
-        return;
+        return YES;
     }
 
     if (!XYIsBeforeXY(start.x, start.y, end.x, end.y)) {
@@ -4044,13 +4043,22 @@ static void SwapInt(int *a, int *b) {
     VT100GridRun run = VT100GridRunFromCoords(VT100GridCoordMake(startX, startY),
                                               VT100GridCoordMake(endX, endY),
                                               currentGrid_.size.width);
-    assert(run.length >= 0);
+    if (run.length == 0) {
+        DLog(@"Run has length 0 given start and end of %@ and %@", VT100GridCoordDescription(start),
+             VT100GridCoordDescription(end));
+        return NO;
+    }
     run = [self runByTrimmingNullsFromRun:run];
-    assert(run.length >= 0);
+    if (run.length == 0) {
+        DLog(@"After trimming, run has length 0 given start and end of %@ and %@", VT100GridCoordDescription(start),
+             VT100GridCoordDescription(end));
+        return NO;
+    }
     VT100GridCoord max = VT100GridRunMax(run, currentGrid_.size.width);
 
     *startPtr = run.origin;
     *endPtr = max;
+    return YES;
 }
 
 - (LineBufferPositionRange *)positionRangeForCoordRange:(VT100GridCoordRange)range
@@ -4089,10 +4097,13 @@ static void SwapInt(int *a, int *b) {
 
     VT100GridCoord trimmedStart;
     VT100GridCoord trimmedEnd;
-    [self trimSelectionFromStart:VT100GridCoordMake(range.start.x, range.start.y)
-                             end:VT100GridCoordMake(range.end.x, range.end.y)
-                        toStartX:&trimmedStart
-                          toEndX:&trimmedEnd];
+    BOOL ok = [self trimSelectionFromStart:VT100GridCoordMake(range.start.x, range.start.y)
+                                       end:VT100GridCoordMake(range.end.x, range.end.y)
+                                  toStartX:&trimmedStart
+                                    toEndX:&trimmedEnd];
+    if (!ok) {
+        return nil;
+    }
     if (VT100GridCoordOrder(trimmedStart, trimmedEnd) == NSOrderedDescending) {
         if (tolerateEmpty) {
             trimmedStart = trimmedEnd = range.start;

commit 55947c56f19d41983b6251b71948cf72428ebeaf

Fix a bug introduced by commit 392ab06 where a RemoteHost mark on the last line of all nulls would get removed because positionRangeForCoordRange:inLineBuffer:tolerateEmpty: incorrectly failed even when tolerateEmpty was true when trimSelectionFromStart:end:toStartX:toEndX: returned NO. Not sure why this used to work since it would have asserted.

sources/VT100Screen.m
@@ -4236,8 +4236,11 @@ static void SwapInt(int *a, int *b) {
                                        end:VT100GridCoordMake(range.end.x, range.end.y)
                                   toStartX:&trimmedStart
                                     toEndX:&trimmedEnd];
-    if (!ok) {
+    if (!ok && !tolerateEmpty) {
         return nil;
+    } else if (!ok && tolerateEmpty) {
+        trimmedStart = range.start;
+        trimmedEnd = range.start;
     }
     if (VT100GridCoordOrder(trimmedStart, trimmedEnd) == NSOrderedDescending) {
         if (tolerateEmpty) {