92cb73318e6f86699d50638354c4298eea6b67e9
George Nachman <gnachman@gmail.com>
2017-02-11 17:40:58 -0800

    Add a python example of API usage

--- tools/build_proto.sh
+++ tools/build_proto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SOURCE=proto
 GENFILES=sources/proto
-tools/protoc --proto_path="$SOURCE" --objc_out="$GENFILES" "$SOURCE"/api.proto
+tools/protoc --proto_path="$SOURCE" --objc_out="$GENFILES" --python_out=api/examples/python "$SOURCE"/api.proto
