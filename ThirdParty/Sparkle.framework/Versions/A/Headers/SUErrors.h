e46b11a08fd1a4479c876aab1086390210edb19a
George Nachman <gnachman@gmail.com>
2017-03-10 21:32:11 -0800

    Update to Sparkle 1.17 with my patch.

--- ThirdParty/Sparkle.framework/Versions/A/Headers/SUErrors.h
+++ ThirdParty/Sparkle.framework/Versions/A/Headers/SUErrors.h
@@ -9,7 +9,11 @@
 #ifndef SUERRORS_H
 #define SUERRORS_H
 
+#if __has_feature(modules)
+@import Foundation;
+#else
 #import <Foundation/Foundation.h>
+#endif
 #import "SUExport.h"
 
 /**
@@ -40,6 +44,7 @@ typedef NS_ENUM(OSStatus, SUError) {
     SURelaunchError = 4004,
     SUInstallationError = 4005,
     SUDowngradeError = 4006,
+    SUInstallationCancelledError = 4007,
     
     // System phase errors
     SUSystemPowerOffError = 5000
--- ThirdParty/Sparkle.framework/Versions/A/Headers/Sparkle.h
+++ ThirdParty/Sparkle.framework/Versions/A/Headers/Sparkle.h
@@ -9,8 +9,6 @@
 #ifndef SPARKLE_H
 #define SPARKLE_H
 
-#import <Cocoa/Cocoa.h>
-
 // This list should include the shared headers. It doesn't matter if some of them aren't shared (unless
 // there are name-space collisions) so we can list all of them to start with:
 
@@ -18,6 +16,7 @@
 #import "SUAppcastItem.h"
 #import "SUStandardVersionComparator.h"
 #import "SUUpdater.h"
+#import "SUUpdaterDelegate.h"
 #import "SUVersionComparisonProtocol.h"
 #import "SUVersionDisplayProtocol.h"
 #import "SUErrors.h"
