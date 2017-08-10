be33d4c8c6fa272504400cfc6c73a70a0c34ca6c
George Nachman <gnachman@gmail.com>
2017-08-09 21:54:06 -0700

    Make tab labels black and white when any tab has color. Make the outline always stark white (with very dark tabs it ended up the same color as the tab bar lines). Make uncolored tabs have a white outline.

--- ThirdParty/PSMTabBarControl/source/PSMYosemiteTabStyle.m
+++ ThirdParty/PSMTabBarControl/source/PSMYosemiteTabStyle.m
@@ -351,7 +351,7 @@
 
 - (NSColor *)textColorForCell:(PSMTabBarCell *)cell {
     NSColor *textColor;
-    if (cell.state == NSOnState) {
+    if (cell.state == NSOnState || [self anyTabHasColor]) {
         if (!cell.tabColor) {
             return [self textColorDefaultSelected:YES];
         } else if ([cell.tabColor brightnessComponent] > 0.2) {
@@ -540,7 +540,10 @@ static CGFloat PerceivedBrightness(CGFloat r, CGFloat g, CGFloat b) {
 }
 
 - (CGFloat)tabColorBrightness:(PSMTabBarCell *)cell {
-    NSColor *tabColor = cell.tabColor ?: [self backgroundColorSelected:cell.state == NSOnState highlightAmount:0];
+    if (!cell.tabColor) {
+        return 0.7;
+    }
+    NSColor *tabColor = cell.tabColor;
     NSColor *safeColor = [tabColor colorUsingColorSpaceName:NSCalibratedRGBColorSpace];
     CGFloat brightness = PerceivedBrightness([safeColor redComponent],
                                              [safeColor greenComponent],
@@ -548,11 +551,14 @@ static CGFloat PerceivedBrightness(CGFloat r, CGFloat g, CGFloat b) {
     return brightness;
 }
 
-- (void)drawPostHocDecorationsOnSelectedCell:(PSMTabBarCell *)cell {
-    const BOOL anyTabHasColor = [_tabBar.cells indexOfObjectPassingTest:^BOOL(PSMTabBarCell * _Nonnull cell, NSUInteger idx, BOOL * _Nonnull stop) {
+- (BOOL)anyTabHasColor {
+    return [_tabBar.cells indexOfObjectPassingTest:^BOOL(PSMTabBarCell * _Nonnull cell, NSUInteger idx, BOOL * _Nonnull stop) {
         return cell.tabColor != nil;
     }] != NSNotFound;
-    if (anyTabHasColor) {
+}
+
+- (void)drawPostHocDecorationsOnSelectedCell:(PSMTabBarCell *)cell {
+    if (self.anyTabHasColor) {
         const CGFloat brightness = [self tabColorBrightness:cell];
         [[NSColor colorWithWhite:MIN(1, brightness + 0.6) alpha:1] set];
         NSFrameRect(cell.frame);
