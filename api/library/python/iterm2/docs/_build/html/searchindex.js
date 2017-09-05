d679b5e501f69e44768c43dba9a94acfc0c185fc
George Nachman <gnachman@gmail.com>
2017-09-04 22:07:20 -0700

    Fresh docs folder

---
+++ api/library/python/iterm2/docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to iTerm2\u2019s documentation!"],titleterms:{document:0,indic:0,iterm2:0,tabl:0,welcom:0}})
--- api/library/python/iterm2/docs/modules.rst
+++
@@ -1,7 +0,0 @@
-iterm2
-======
-
-.. toctree::
-   :maxdepth: 4
-
-   iterm2
