0c843feedbae91824f425e1a00bc674d8902028c
George Nachman <gnachman@gmail.com>
2017-09-27 22:06:47 -0700

    Accept dead keys as hotkeys. Issue 6102.
    This fixes both deadkeys alone (e.g., ` on a DE keyboard) or deadkey+key (opt+` on DE to give '). I tested migration from 3.0 and it seems fine.

--- sources/iTermShortcut.m
+++ sources/iTermShortcut.m
@@ -187,11 +187,16 @@ const NSEventModifierFlags kHotKeyModifierMask = (NSCommandKeyMask |
 }
 
 - (NSString *)stringValue {
-    return self.charactersIgnoringModifiers.length > 0 ? [iTermKeyBindingMgr formatKeyCombination:self.identifier keyCode:self.keyCode] : @"";
+    // Dead keys can have characters without charactersIgnoringModifiers. For example, option+` on
+    // a German keyboard enters an apostrophe ('). The ` key is a dead key, so if you ignore modifiers
+    // it's nothing. So if there are either characters or characters ignoring modifiers, it's a
+    // formattable shortcut. If you press just the dead key then both characters and charactersIgnoringModifiers
+    // will be empty.
+    return (self.charactersIgnoringModifiers.length > 0 || self.characters.length > 0 || self.keyCode != 0) ? [iTermKeyBindingMgr formatKeyCombination:self.identifier keyCode:self.keyCode] : @"";
 }
 
 - (BOOL)isAssigned {
-    return self.charactersIgnoringModifiers.length > 0;
+    return self.charactersIgnoringModifiers.length > 0 || self.characters.length > 0 || self.keyCode != 0;
 }
 
 - (iTermHotKeyDescriptor *)descriptor {
