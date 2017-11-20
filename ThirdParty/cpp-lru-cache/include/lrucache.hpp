72662cc039761b378ff534e582643e5f5423bad3
George Nachman <gnachman@gmail.com>
2017-11-19 21:31:05 -0800

    Customize CPP-lru-cache for iTerm2.
    Don't throw when get fails. Return nullptr instead.
    Add peek() to get a key's value if it belongs to the cache without bumping it to mru.
    Add erase() to remove a key.
    Add exists() to check if a key exists.
    Add size() to return the number of entries. Replace tabs with spaces. Remove files we don't need.

--- ThirdParty/cpp-lru-cache/include/lrucache.hpp
+++ ThirdParty/cpp-lru-cache/include/lrucache.hpp
@@ -18,52 +18,76 @@ namespace cache {
 template<typename key_t, typename value_t>
 class lru_cache {
 public:
-	typedef typename std::pair<key_t, value_t> key_value_pair_t;
-	typedef typename std::list<key_value_pair_t>::iterator list_iterator_t;
+    typedef typename std::pair<key_t, value_t> key_value_pair_t;
+    typedef typename std::list<key_value_pair_t>::iterator list_iterator_t;
 
-	lru_cache(size_t max_size) :
-		_max_size(max_size) {
-	}
-	
-	void put(const key_t& key, const value_t& value) {
-		auto it = _cache_items_map.find(key);
-		_cache_items_list.push_front(key_value_pair_t(key, value));
-		if (it != _cache_items_map.end()) {
-			_cache_items_list.erase(it->second);
-			_cache_items_map.erase(it);
-		}
-		_cache_items_map[key] = _cache_items_list.begin();
-		
-		if (_cache_items_map.size() > _max_size) {
-			auto last = _cache_items_list.end();
-			last--;
-			_cache_items_map.erase(last->first);
-			_cache_items_list.pop_back();
-		}
-	}
-	
-	const value_t& get(const key_t& key) {
-		auto it = _cache_items_map.find(key);
-		if (it == _cache_items_map.end()) {
-			throw std::range_error("There is no such key in cache");
-		} else {
-			_cache_items_list.splice(_cache_items_list.begin(), _cache_items_list, it->second);
-			return it->second->second;
-		}
-	}
-	
-	bool exists(const key_t& key) const {
-		return _cache_items_map.find(key) != _cache_items_map.end();
-	}
-	
-	size_t size() const {
-		return _cache_items_map.size();
-	}
-	
+    lru_cache(size_t max_size) :
+    _max_size(max_size) {
+    }
+
+    inline void put(const key_t& key, const value_t& value) {
+        auto it = _cache_items_map.find(key);
+        _cache_items_list.push_front(key_value_pair_t(key, value));
+        if (it != _cache_items_map.end()) {
+            _cache_items_list.erase(it->second);
+            _cache_items_map.erase(it);
+        }
+        _cache_items_map[key] = _cache_items_list.begin();
+
+        if (_cache_items_map.size() > _max_size) {
+            auto last = _cache_items_list.end();
+            last--;
+            _cache_items_map.erase(last->first);
+            _cache_items_list.pop_back();
+        }
+    }
+
+    // Returns the key-value pair of the least-recently used key and its associated value.
+    key_value_pair_t get_lru(void) const {
+        auto last = _cache_items_list.end();
+        last--;
+        return *last;
+    }
+
+    inline const value_t *get(const key_t& key) {
+        auto it = _cache_items_map.find(key);
+        if (it == _cache_items_map.end()) {
+            return nullptr;
+        } else {
+            _cache_items_list.splice(_cache_items_list.begin(), _cache_items_list, it->second);
+            return &it->second->second;
+        }
+    }
+
+    inline const value_t *peek(const key_t& key) {
+        auto it = _cache_items_map.find(key);
+        if (it == _cache_items_map.end()) {
+            return nullptr;
+        } else {
+            return &it->second->second;
+        }
+    }
+
+    inline void erase(const key_t &key) {
+        auto it = _cache_items_map.find(key);
+        if (it != _cache_items_map.end()) {
+            _cache_items_list.erase(it->second);
+            _cache_items_map.erase(it);
+        }
+    }
+
+    inline bool exists(const key_t& key) const {
+        return _cache_items_map.find(key) != _cache_items_map.end();
+    }
+
+    inline size_t size() const {
+        return _cache_items_map.size();
+    }
+
 private:
-	std::list<key_value_pair_t> _cache_items_list;
-	std::unordered_map<key_t, list_iterator_t> _cache_items_map;
-	size_t _max_size;
+    std::list<key_value_pair_t> _cache_items_list;
+    std::unordered_map<key_t, list_iterator_t> _cache_items_map;
+    size_t _max_size;
 };
 
 } // namespace cache
