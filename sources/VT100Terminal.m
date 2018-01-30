573fc172b1cad35cd465537cfe884672ee630c0b
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2018-01-30 13:44:15 +0100

    Fix regression when fixing the setupterm memory leak

--- sources/VT100Terminal.m
+++ sources/VT100Terminal.m
@@ -264,29 +264,28 @@ static const int kMaxScreenRows = 4096;
 - (void)setTermType:(NSString *)termtype
 {
     static char *myterm = NULL;
-    [_termType autorelease];
-   // _termType = [termtype copy];
 
-    self.allowKeypadMode = [_termType rangeOfString:@"xterm"].location != NSNotFound;
+    self.allowKeypadMode = [termtype rangeOfString:@"xterm"].location != NSNotFound;
     int r;
 
     // NOTE: This seems to cause a memory leak. The setter for termTypeIsValid (below) has the
     // side effect of copying various curses strings, and it depends on this. When I redo output,
     // fix this disaster.
-    const char *t = [_termType UTF8String];
+    const char *t = [termtype UTF8String];
     if (t) {
         free (myterm);
         myterm = strdup (t);
     }
     setupterm(myterm, fileno(stdout), &r);
     if (r != 1) {
-        NSLog(@"Terminal type %s is not defined.", [_termType UTF8String]);
+        NSLog(@"Terminal type %s is not defined.", t);
     }
     _output.termTypeIsValid = (r == 1);
 
-    self.isAnsi = [_termType rangeOfString:@"ANSI"
+    self.isAnsi = [termtype rangeOfString:@"ANSI"
                                    options:NSCaseInsensitiveSearch | NSAnchoredSearch ].location !=  NSNotFound;
     [delegate_ terminalTypeDidChange];
+    [termtype autorelease];
 }
 
 - (void)setAnswerBackString:(NSString *)s {
