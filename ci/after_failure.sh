ba95f8580bf55728c570075ed69bae6e9e7119df
George Nachman <gnachman@gmail.com>
2018-02-11 20:39:07 -0800

    Fix path to dropbox uploader

--- ci/after_failure.sh
+++ ci/after_failure.sh
@@ -6,9 +6,10 @@ if ls /tmp/failed-* 1> /dev/null 2>&1; then
   cp tests/imgcat /tmp
   cp ci/accept.sh /tmp
   export PATH=$PATH:$PWD/tests
+  export IT2BASE="$PWD"
   cd /tmp
   source /tmp/diffs > diffs.txt
   tar cvfz travis-fail-$TRAVIS_JOB_NUMBER.tgz failed-*.png diffs.txt accept.sh
-  tools/dropbox_uploader.sh upload travis-fail-$TRAVIS_JOB_NUMBER.tgz 
+  $IT2BASE/tools/dropbox_uploader.sh upload travis-fail-$TRAVIS_JOB_NUMBER.tgz 
 fi
 
