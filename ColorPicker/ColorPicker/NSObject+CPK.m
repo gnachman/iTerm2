9ed6d3fcfde7a03a2fe22e35237ba5cbb5b73443
George Nachman <gnachman@gmail.com>
2018-03-10 15:58:53 -0800

    Adhoc build 3.2.20180310_155800

--- ColorPicker/ColorPicker/NSObject+CPK.m
+++ ColorPicker/ColorPicker/NSObject+CPK.m
@@ -5,13 +5,37 @@
 - (NSImage *)cpk_imageNamed:(NSString *)name {
     NSString *path = [[NSBundle bundleForClass:[self class]] pathForResource:name ofType:@"tiff"];
     NSImage *image = [[NSImage alloc] initWithContentsOfFile:path];
+    NSString *mainBundle = [[[NSBundle mainBundle] bundleURL] path];
+    NSString *suffix = @"Contents/Frameworks/ColorPicker.framework/Resources";
+    NSURL *url = [NSURL fileURLWithPath:[[[mainBundle stringByAppendingPathComponent:suffix] stringByAppendingPathComponent:name] stringByAppendingPathExtension:@"tiff"]];
 
-    NSString *log = [NSString stringWithFormat:@"Loading %@ (%@). Image is %@\n", path, name, image];
+    if (!image) {
+        NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+        image = [bundle imageForResource:name];
+
+        if (!image) {
+            image = [[NSImage alloc] initWithContentsOfURL:url];
+        }
+    }
+
+    NSString *log = [NSString stringWithFormat:@"Loading %@ (%@). Image is %@. Bundle for %@ is %@. Main bundle is %@. Constructed url is %@\n", path, name, image, [self class],
+                     [NSBundle bundleForClass:[self class]], [NSBundle mainBundle], url];
     NSLog(@"%@",log);
-    NSString *contents = [NSString stringWithContentsOfFile:@"/tmp/colorlog.txt"] ?: @"";
-    contents = [contents stringByAppendingString:log];
+
+    NSMutableString *contents = [([NSString stringWithContentsOfFile:@"/tmp/colorlog.txt"] ?: @"") mutableCopy];
+
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [contents appendFormat:@"BEGIN LIST OF BUNDLES\n"];
+        for (NSBundle *bundle in [NSBundle allBundles]) {
+            [contents appendFormat:@"%@\n", bundle];
+        }
+        [contents appendFormat:@"END LIST OF BUNDLES\n\n"];
+    });
+
+    [contents appendString:log];
     [contents writeToFile:@"/tmp/colorlog.txt" atomically:YES encoding: NSUTF8StringEncoding error:NULL];
-    
+
     return image;
 }
 
