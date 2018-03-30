b2f1cdf8d46904fd858ae590aab88a33a1df82a0
George Nachman <gnachman@gmail.com>
2018-03-29 19:41:05 -0700

    Move CPKScreenshot to its own file

--- ColorPicker/ColorPicker/CPKEyedropperWindow.m
+++ ColorPicker/ColorPicker/CPKEyedropperWindow.m
@@ -1,5 +1,6 @@
 #import "CPKEyedropperWindow.h"
 #import "CPKEyedropperView.h"
+#import "CPKScreenshot.h"
 #import "NSColor+CPK.h"
 
 // Size in points of one edge of the window. It is square.
@@ -7,98 +8,6 @@ const CGFloat kSize = 200;
 
 // How often to update the view for the current mouse coordinates
 const NSTimeInterval kUpdateInterval = 1.0 / 60.0;
-
-@interface CPKScreenshot : NSObject
-@property (nonatomic, strong) NSData *data;
-@property (nonatomic) CGSize size;
-
-+ (instancetype)grabFromScreen:(NSScreen *)screen;
-+ (instancetype)screenshotFromCGImage:(CGImageRef)image;
-- (NSColor *)colorAtX:(int)x y:(int)y;
-
-@end
-
-@implementation CPKScreenshot
-
-+ (instancetype)grabFromScreen:(NSScreen *)screen {
-    NSDictionary *dict = screen.deviceDescription;
-    CGDirectDisplayID displayId = [dict[@"NSScreenNumber"] unsignedIntValue];
-    CGImageRef cgImage = CGDisplayCreateImage(displayId);
-
-    NSSize size = screen.frame.size;
-    size.width *= screen.backingScaleFactor;
-    size.height *= screen.backingScaleFactor;
-
-    CPKScreenshot *screenshot = [CPKScreenshot screenshotFromCGImage:cgImage];
-    CFRelease(cgImage);
-
-    return screenshot;
-}
-
-+ (instancetype)screenshotFromCGImage:(CGImageRef)inImage; {
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateWithName(kCGColorSpaceSRGB);
-    if (colorSpace == NULL) {
-        return nil;
-    }
-
-    // Create the bitmap context
-    NSMutableData *storage = [NSMutableData data];
-    CGContextRef cgctx = [self newBitmapContextForImage:inImage
-                                             colorSpace:colorSpace
-                                                storage:storage];
-    if (cgctx == NULL) {
-        CGColorSpaceRelease(colorSpace);
-        return nil;
-    }
-
-    CGRect rect = CGRectMake(0,
-                             0,
-                             CGImageGetWidth(inImage),
-                             CGImageGetHeight(inImage));
-    CGContextDrawImage(cgctx, rect, inImage);
-
-    CPKScreenshot *result = [[CPKScreenshot alloc] init];
-    result.data = [NSData dataWithBytes:CGBitmapContextGetData(cgctx)
-                                 length:CGBitmapContextGetBytesPerRow(cgctx) * CGBitmapContextGetHeight(cgctx)];
-    result.size = rect.size;
-
-    CGContextRelease(cgctx);
-    CGColorSpaceRelease(colorSpace);
-
-    return result;
-}
-
-+ (CGContextRef)newBitmapContextForImage:(CGImageRef)inImage
-                              colorSpace:(CGColorSpaceRef)colorSpace
-                                 storage:(NSMutableData *)storage {
-    const int pixelsWide = (int)CGImageGetWidth(inImage);
-    const int pixelsHigh = (int)CGImageGetHeight(inImage);
-
-    const int bitmapBytesPerRow = pixelsWide * 4;
-    const int bitmapByteCount     = (bitmapBytesPerRow * pixelsHigh);
-
-    storage.length = bitmapByteCount;
-    CGContextRef context = CGBitmapContextCreate (storage.mutableBytes,
-                                                  pixelsWide,
-                                                  pixelsHigh,
-                                                  8,      // bits per component
-                                                  bitmapBytesPerRow,
-                                                  colorSpace,
-                                                  kCGImageAlphaPremultipliedFirst);
-    return context;
-}
-
-- (NSColor *)colorAtX:(int)x y:(int)y {
-    unsigned char *b = (unsigned char *)_data.bytes;
-    b += y * (int)_size.width * 4;
-    b += x * 4;
-    return [NSColor colorWithSRGBRed:b[1] / 255.0
-                               green:b[2] / 255.0
-                                blue:b[3] / 255.0
-                               alpha:b[0] / 255.0];
-}
-
-@end
 
 @interface CPKEyedropperWindow ()
 @property(nonatomic) NSMutableArray<CPKScreenshot *> *screenshots;
