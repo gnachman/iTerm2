02ba435ec3fe73e7f4c008431d9a51ef424b6df3
George Nachman <gnachman@gmail.com>
2018-04-22 11:14:58 -0700

    Rewrite remote_control.py using the new library

--- api/examples/python/remote_control.py
+++ api/examples/python/remote_control.py
@@ -1,103 +1,38 @@
-#!/usr/bin/python
-# This is python 2.7 on macOS 10.12.
+#!/usr/bin/env python3.6
 
 from __future__ import print_function
 
-import api_pb2
+import asyncio
+import iterm2
 import sys
-import thread
 import time
-import websocket
+import traceback
 
-callbacks = []
-DEBUG=0
+future = None
 
-def SendRPC(ws, message, callback):
-    if DEBUG > 0:
-      print(">>> " + str(message))
-    ws.send(message.SerializeToString(), opcode=websocket.ABNF.OPCODE_BINARY)
-    callbacks.append(callback)
+async def on_custom_esc(connection, notif):
+  global future
+  print(notif)
+  future.set_result(notif)
 
-def handle_notification(ws, notification):
-    def handle_custom_escape_sequence_notification(custom_escape_sequence_notification):
-        # -- Your logic goes here --
-        print(custom_escape_sequence_notification.sender_identity + " sends message " + custom_escape_sequence_notification.payload)
-
-    def handle_new_session_notification(new_session_notification):
-        subscribe_to_custom_escape_sequence(ws, new_session_notification.uniqueIdentifier)
-
-    if notification.HasField('custom_escape_sequence_notification'):
-      handle_custom_escape_sequence_notification(notification.custom_escape_sequence_notification)
-    elif notification.HasField('new_session_notification'):
-      handle_new_session_notification(notification.new_session_notification)
-
-def handle_notification_response(response):
-  if not response.HasField('notification_response'):
-    print("Malformed notification response")
-    print(str(response))
-    return
-  if response.notification_response.status != api_pb2.NotificationResponse.OK:
-    print("Bad status in notification response")
-    print(str(response))
-    return
-
-def subscribe_to_new_sessions(ws):
-    request = api_pb2.Request()
-    request.notification_request.subscribe = True
-    request.notification_request.notification_type = api_pb2.NOTIFY_ON_NEW_SESSION
-    SendRPC(ws, request, handle_notification_response)
-
-def subscribe_to_custom_escape_sequence(ws, session):
-    request = api_pb2.Request()
-    request.notification_request.subscribe = True
-    request.notification_request.session = session
-    request.notification_request.notification_type = api_pb2.NOTIFY_ON_CUSTOM_ESCAPE_SEQUENCE
-    SendRPC(ws, request, handle_notification_response)
+async def program(connection):
+  try:
+    h = await iterm2.hierarchy.Hierarchy.construct(connection)
+    for s in h.windows[0].tabs[0].get_sessions():
+      await iterm2.notifications.subscribe_to_custom_escape_sequence_notification(connection, on_custom_esc, s.session_id)
+    while True:
+      global future
+      future = asyncio.Future()
+      await connection.dispatch_until_future(future)
+  except Exception as e:
+    print(traceback.format_exc())
 
 def main(argv):
-    def on_message(ws, message):
-        response = api_pb2.Response()
-        response.ParseFromString(message)
-        if DEBUG > 0:
-          print("<<< " + str(response))
-        if response.HasField('notification'):
-          handle_notification(ws, response.notification)
-        else:
-          global callbacks
-          callback = callbacks[0]
-          del callbacks[0]
-          callback(response)
-
-    def on_error(ws, error):
-        print("Error: " + str(error))
-
-    def on_close(ws):
-        print("Connection closed")
-
-    def on_open(ws):
-        def list_sessions(ws):
-            def callback(response):
-              for window in response.list_sessions_response.windows:
-                  for tab in window.tabs:
-                      for session in tab.sessions:
-                          subscribe_to_custom_escape_sequence(ws, session.uniqueIdentifier)
-            request = api_pb2.Request()
-            request.list_sessions_request.SetInParent()
-            SendRPC(ws, request, callback)
-
-        subscribe_to_new_sessions(ws)
-        list_sessions(ws)
-
-    #websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://localhost:1912/",
-                              on_message = on_message,
-                              on_error = on_error,
-                              on_close = on_close,
-                              subprotocols = [ 'api.iterm2.com' ])
-    ws.on_open = on_open
-    ws.run_forever()
+    c = iterm2.connection.Connection()
+    c.run(program)
 
 if __name__ == "__main__":
+  try:
     main(sys.argv)
-
-
+  except:
+    sys.exit(1)
