28ef4fcb20405d5ca606835056e33048c235d3ee
George Nachman <gnachman@gmail.com>
2018-04-22 11:11:34 -0700

    Only have one api_pb2.py. It's the one in the python library.

--- tools/build_proto.sh
+++ tools/build_proto.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 SOURCE=proto
 GENFILES=sources/proto
-tools/protoc --proto_path="$SOURCE" --objc_out="$GENFILES" --python_out=api/examples/python "$SOURCE"/api.proto
-tools/protoc --proto_path="$SOURCE" --objc_out="$GENFILES" --python_out=api/library/python "$SOURCE"/api.proto
+tools/protoc --proto_path="$SOURCE" --objc_out="$GENFILES" --python_out=api/library/python/iterm2/iterm2 "$SOURCE"/api.proto
