c810e9a0c38f805af51fe12f8e40edda45402286
George Nachman <gnachman@gmail.com>
2018-04-30 09:13:28 -0700

    Remove sphinx build directory

--- api/library/python/iterm2/sphinx/_build/html/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '0.26',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
-};
