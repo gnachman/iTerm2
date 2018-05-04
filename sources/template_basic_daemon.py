8046f15e34472d3ec2b8065a0cf48cbd09d6e92a
George Nachman <gnachman@gmail.com>
2018-05-03 19:26:42 -0700

    Update templates for the new namespacing

--- sources/template_basic_daemon.py
+++ sources/template_basic_daemon.py
@@ -4,23 +4,21 @@ import asyncio
 import iterm2
 import sys
 # This script was created with the "basic" environment which does not support adding dependencies
-# outside of those that ship with Python.
-
-_APP = None
-
-# This is an example of a callback function. In this template, on_custom_esc is called when a
-# custom escape sequence is received. You can send a custom escape sequence with this command:
-#
-# printf "\033]1337;Custom=id=%s:%s\a" "shared-secret" "create-window"
-async def on_custom_esc(connection, notification):
-    print("Received a custom escape sequence")
-    if notification.sender_identity == "shared-secret":
-        if notification.payload == "create-window":
-            await _APP.async_create_window()
+# with pip.
 
 async def main(connection, argv):
-    global _APP
-    _APP = await iterm2.app.async_get_app(connection)
+    app = await iterm2.async_get_app(connection)
+
+    # This is an example of a callback function. In this template, on_custom_esc is called when a
+    # custom escape sequence is received. You can send a custom escape sequence with this command:
+    #
+    # printf "\033]1337;Custom=id=%s:%s\a" "shared-secret" "create-window"
+    async def on_custom_esc(connection, notification):
+        print("Received a custom escape sequence")
+        if notification.sender_identity == "shared-secret":
+            if notification.payload == "create-window":
+                await app.async_create_window()
+
     # Your program should register for notifications it wants to receive here. This example
     # watches for custom escape sequences.
     await iterm2.notifications.async_subscribe_to_custom_escape_sequence_notification(connection, on_custom_esc)
--- sources/template_basic_simple.py
+++ sources/template_basic_simple.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
-import asyncio
 import iterm2
 import sys
 # This script was created with the "basic" environment which does not support adding dependencies
-# outside of those that ship with Python.
+# with pip.
 
 async def main(connection, argv):
     # Your code goes here. Here's a bit of example code that adds a tab to the current window:
-    a = await iterm2.app.async_get_app(connection)
-    w = await a.async_get_key_window()
-    if w is not None:
-        await w.async_create_tab()
+    app = await iterm2.async_get_app(connection)
+    window = app.key_window
+    if window is not None:
+        await window.async_create_tab()
     else:
         # You can view this message in the script console. Open the script console before running
         # the script.
