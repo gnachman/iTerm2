2fa2dcce4b2893e7d50aa576347ef15dd1f4a4e3
George Nachman <gnachman@gmail.com>
2018-06-19 09:07:13 -0700

    Don't show tmux in title preview in prefs

--- sources/ProfilesGeneralPreferencesViewController.m
+++ sources/ProfilesGeneralPreferencesViewController.m
@@ -619,7 +619,7 @@ static NSString *const iTermProfilePreferencesUpdateSessionName = @"iTermProfile
                                                                      tty:@"TTY"
                                                                     user:@"User"
                                                                     host:@"Host"
-                                                                    tmux:@"Tmux"
+                                                                    tmux:nil
                                                               components:value];
 
     const CGFloat maxWidth = NSMinX(_customTitleHelp.frame) - NSMinX(_titleSettings.frame) - 5;
