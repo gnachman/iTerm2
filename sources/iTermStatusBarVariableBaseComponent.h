70bf388bbe20039eb648a18c65d4dd28ea215ef0
George Nachman <gnachman@gmail.com>
2018-07-01 22:21:10 -0700

    Add some easy variable-based components

---
+++ sources/iTermStatusBarVariableBaseComponent.h
@@ -0,0 +1,43 @@
+//
+//  iTermStatusBarVariableBaseComponent.h
+//  iTerm2SharedARC
+//
+//  Created by George Nachman on 7/1/18.
+//
+
+#import "iTermStatusBarTextComponent.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface iTermStatusBarVariableBaseComponent : iTermStatusBarTextComponent
+
+- (instancetype)initWithPath:(NSString *)path NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithConfiguration:(NSDictionary<iTermStatusBarComponentConfigurationKey, id> *)configuration NS_UNAVAILABLE;
+
+@end
+
+@interface iTermStatusBarHostnameComponent : iTermStatusBarVariableBaseComponent
+
+- (instancetype)initWithConfiguration:(NSDictionary<iTermStatusBarComponentConfigurationKey, id> *)configuration;
+- (instancetype)init NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithPath:(NSString *)path NS_UNAVAILABLE;
+
+@end
+
+@interface iTermStatusBarUsernameComponent : iTermStatusBarVariableBaseComponent
+
+- (instancetype)initWithConfiguration:(NSDictionary<iTermStatusBarComponentConfigurationKey, id> *)configuration;
+- (instancetype)init NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithPath:(NSString *)path NS_UNAVAILABLE;
+
+@end
+
+@interface iTermStatusBarWorkingDirectoryComponent : iTermStatusBarVariableBaseComponent
+
+- (instancetype)initWithConfiguration:(NSDictionary<iTermStatusBarComponentConfigurationKey, id> *)configuration;
+- (instancetype)init NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithPath:(NSString *)path NS_UNAVAILABLE;
+
+@end
+
+NS_ASSUME_NONNULL_END
