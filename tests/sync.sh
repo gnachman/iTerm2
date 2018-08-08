1eac8bd31e3e7ea3433aff1e739b6410260ef068
George Nachman <gnachman@gmail.com>
2018-08-08 16:48:10 -0700

    Add test of synchronous updates

---
+++ tests/sync.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo Hello world should appear at once.
+echo '[?25l'
+sleep 0.1
+echo P=s\\
+echo ']4;-1;rgb:ff/00/00'
+echo hello
+sleep .5
+echo world
+
+
+echo P=s\\
+sleep .5
+echo world
+
+echo '[?25h'
+echo P=s\\
+sleep .5
+echo world
+
+echo P=s\\
+sleep .5
+echo world
+
+echo P=s\\
+sleep .5
+echo world
+
+echo P=s\\
+sleep .5
+echo world
+
+echo P=\\
