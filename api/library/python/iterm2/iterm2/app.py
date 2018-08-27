937b1539afbb60373ee98dbba5de2fec1b377112
George Nachman <gnachman@gmail.com>
2018-08-26 22:42:46 -0700

    [python] Move window arrangement code to its own file

--- api/library/python/iterm2/iterm2/app.py
+++ api/library/python/iterm2/iterm2/app.py
@@ -25,10 +25,6 @@ class CreateWindowException(Exception):
     """A problem was encountered while creating a window."""
     pass
 
-class SavedArrangementException(Exception):
-    """A problem was encountered while saving or restoring an arrangement."""
-    pass
-
 class MenuItemException(Exception):
     """A problem was encountered while selecting a menu item."""
     pass
@@ -139,36 +135,6 @@ class App:
             False,
             False,
             activate_app_opts=opts)
-
-    async def async_save_window_arrangement(self, name):
-        """Save all windows as a new arrangement.
-
-        Replaces the arrangement with the given name if it already exists.
-
-        :param name: The name of the arrangement.
-
-        :throws: SavedArrangementException
-        """
-        result = await iterm2.rpc.async_save_arrangement(self.connection, name)
-        status = result.saved_arrangement_response.status
-        if status != iterm2.api_pb2.CreateTabResponse.Status.Value("OK"):
-            raise SavedArrangementException(
-                iterm2.api_pb2.SavedArrangementResponse.Status.Name(
-                    result.saved_arrangement_response.status))
-
-    async def async_restore_window_arrangement(self, name):
-        """Restore a saved window arrangement.
-
-        :param name: The name of the arrangement to restore.
-
-        :throws: SavedArrangementException
-        """
-        result = await iterm2.rpc.async_restore_arrangement(self.connection, name)
-        status = result.saved_arrangement_response.status
-        if status != iterm2.api_pb2.CreateTabResponse.Status.Value("OK"):
-            raise SavedArrangementException(
-                iterm2.api_pb2.SavedArrangementResponse.Status.Name(
-                    result.saved_arrangement_response.status))
 
     @staticmethod
     def _windows_from_list_sessions_response(connection, response):
