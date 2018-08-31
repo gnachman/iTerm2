69b4c086a3f78d982d82d6213f4738be9b632290
George Nachman <gln@whatsapp.com>
2018-08-30 23:48:04 -0700

    Don't listen to carbon notification about the menu bar hiding. It does not tell you which screen it's talking about and if there are two you will be confused. You could confuse it by changing the fullscreen status of a window on a screen without an iTerm2 window. Also, fix how menu bar frames are computed because they're flipped but screen frames are not flipped. I'm not sure what will break by removing the menu bar hidden test, but let's see. Issue 6985

--- sources/iTermMenuBarObserver.m
+++ sources/iTermMenuBarObserver.m
@@ -11,22 +11,10 @@
 #import "NSArray+iTerm.h"
 #import <Carbon/Carbon.h>
 
-static const NSTimeInterval iTermMenuBarObserverDelay = 0.01;
 static const CGFloat iTermMenuBarHeight = 22;
-
-@interface iTermMenuBarObserver()
-- (void)menuBarVisibilityDidChangeWithEvent:(EventRef)event;
-@end
-
-OSErr menuBarVisibilityChangedCallback(EventHandlerCallRef inHandlerRef, EventRef inEvent, void *userData) {
-    iTermMenuBarObserver *observer = (__bridge iTermMenuBarObserver *)userData;
-    [observer menuBarVisibilityDidChangeWithEvent:inEvent];
-    return noErr;
-}
 
 @implementation iTermMenuBarObserver {
     BOOL _fullscreenMode;
-    BOOL _menuBarVisible;  // This is YES when in a desktop with a full screen app that isn't us.
 }
 
 + (instancetype)sharedInstance {
@@ -41,65 +29,17 @@ OSErr menuBarVisibilityChangedCallback(EventHandlerCallRef inHandlerRef, EventRe
 - (id)init {
     self = [super init];
     if (self) {
-        EventTypeSpec events[] = {
-            { kEventClassMenu, kEventMenuBarShown },
-            { kEventClassMenu, kEventMenuBarHidden }
-        };
-        _menuBarVisible = [NSMenu menuBarVisible];
-        InstallEventHandler(GetEventDispatcherTarget(),
-                            NewEventHandlerUPP((EventHandlerProcPtr)menuBarVisibilityChangedCallback),
-                            2,
-                            events,
-                            (__bridge void *)(self), nil);
-
-        [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
-                                                               selector:@selector(activeSpaceDidChangeNotification:)
-                                                                   name:NSWorkspaceActiveSpaceDidChangeNotification
-                                                                 object:nil];
+        DLog(@"Initialize menu bar observer");
     }
 
     return self;
 }
 
-- (BOOL)menuBarVisible {
-    return _menuBarVisible && !self.currentDesktopHasFullScreenWindow;
+- (BOOL)menuBarVisibleOnScreen:(NSScreen *)screen {
+    return [[self screensWithMenuBars] containsObject:screen];
 }
 
 #pragma mark - Private
-
-- (void)scheduleCheck {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(iTermMenuBarObserverDelay * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self checkForFullScreenChange:nil];
-    });
-}
-
-- (void)activeSpaceDidChangeNotification:(NSNotification *)notification {
-    [self scheduleCheck];
-    DLog(@"Activate space changed. set menu bar visible to %@", @([NSMenu menuBarVisible]));
-    _menuBarVisible = [NSMenu menuBarVisible];
-}
-
-- (void)menuBarVisibilityDidChangeWithEvent:(EventRef)event {
-    _menuBarVisible = (GetEventKind(event) != kEventMenuBarHidden);
-    DLog(@"Menu bar visibility did change with event. set menu bar visible to %@", @(GetEventKind(event) != kEventMenuBarHidden));
-    [self scheduleCheck];
-}
-
-- (void)checkForFullScreenChange:(NSTimer *)timer {
-    if (([self menuBarOffScreen] && [self isMenuBarAttachedToMainScreen])) {
-        _currentDesktopHasFullScreenWindow = YES;
-    } else {
-        _currentDesktopHasFullScreenWindow = NO;
-    }
-}
-
-- (BOOL)isMenuBarAttachedToMainScreen {
-    return [[NSScreen mainScreen] isEqualTo:self.screenWithMenuBar];
-}
-
-- (NSScreen *)screenWithMenuBar {
-    return [[NSScreen screens] firstObject];
-}
 
 - (NSArray<NSDictionary *> *)allWindowInfoDictionaries {
     const CGWindowListOption options = (kCGWindowListExcludeDesktopElements |
@@ -109,13 +49,14 @@ OSErr menuBarVisibilityChangedCallback(EventHandlerCallRef inHandlerRef, EventRe
     return windowInfos;
 }
 
-- (NSDictionary *)menuBarWindowInfoDictionary {
+- (NSArray<NSDictionary *> *)menuBarWindowInfoDictionaries {
     NSArray<NSDictionary *> *windowInfos = self.allWindowInfoDictionaries;
-    NSDictionary *menuBarWindowInfo = [windowInfos objectPassingTest:^BOOL(NSDictionary *info, NSUInteger index, BOOL *stop) {
+    NSArray<NSDictionary *> *menuBarInfos = [windowInfos filteredArrayUsingBlock:^BOOL(NSDictionary *info) {
         NSString *windowName = info[(id)kCGWindowName];
         return [windowName isEqualToString:@"Menubar"];
     }];
-    return menuBarWindowInfo;
+    NSLog(@"menu bar infos:\n%@", menuBarInfos);
+    return menuBarInfos;
 }
 
 - (CGRect)windowBoundsRectFromWindowInfoDictionary:(NSDictionary *)dictionary {
@@ -131,22 +72,38 @@ OSErr menuBarVisibilityChangedCallback(EventHandlerCallRef inHandlerRef, EventRe
     return rect;
 }
 
-- (CGRect)actualMenuBarFrame {
-    NSDictionary *menuBarWindowInfo = [self menuBarWindowInfoDictionary];
-    CGRect windowBounds =  [self windowBoundsRectFromWindowInfoDictionary:menuBarWindowInfo];
-    return windowBounds;
+- (NSArray<NSValue *> *)actualMenuBarFrames {
+    NSArray<NSDictionary *> *menuBarWindowInfos = [self menuBarWindowInfoDictionaries];
+    return [menuBarWindowInfos mapWithBlock:^id(NSDictionary *info) {
+        CGRect windowBounds =  [self windowBoundsRectFromWindowInfoDictionary:info];
+        return [NSValue valueWithRect:windowBounds];
+    }];
 }
 
-- (CGRect)expectedFrameOfVisibleMenuBar {
-    CGRect menuBarFrame = [self.screenWithMenuBar frame];
+- (CGRect)expectedFrameOfVisibleMenuBarOnScreen:(NSScreen *)screenWithMenuBar {
+    CGRect menuBarFrame = screenWithMenuBar.frame;
+    CGRect firstScreenFrame = [[[NSScreen screens] firstObject] frame];
+    // Menu bar frames are flipped versus screen frames, and are relative to the first screen.
+    // A menu bar y coordinate of 0 equals the top of the first screen.
+    // A screen y coordinate of 0 equals the bottom of the first screen.
+    menuBarFrame.origin.y = firstScreenFrame.size.height - screenWithMenuBar.frame.size.height - screenWithMenuBar.frame.origin.y;
     menuBarFrame.size.height = iTermMenuBarHeight;
     return menuBarFrame;
 }
 
-- (BOOL)menuBarOffScreen {
-    const BOOL menuBarOnScreen = CGRectContainsPoint(self.expectedFrameOfVisibleMenuBar,
-                                                     self.actualMenuBarFrame.origin);
-    return !menuBarOnScreen;
+- (NSArray<NSScreen *> *)screensWithMenuBars {
+    NSArray<NSValue *> *actualFrames = [self actualMenuBarFrames];
+    NSArray<NSScreen *> *screens = [NSScreen screens];
+    NSArray<NSScreen *> *screensWithMenuBars = [screens filteredArrayUsingBlock:^BOOL(NSScreen *screen) {
+        CGRect expectedFrame = [self expectedFrameOfVisibleMenuBarOnScreen:screen];
+        return [actualFrames anyWithBlock:^BOOL(NSValue *value) {
+            const NSRect actualMenuBarFrame = value.rectValue;
+            return CGRectContainsPoint(expectedFrame,
+                                       actualMenuBarFrame.origin);
+        }];
+    }];
+    DLog(@"Screens with menu bars are %@ from all screens %@", screensWithMenuBars, screens);
+    return screensWithMenuBars;
 }
 
 @end
