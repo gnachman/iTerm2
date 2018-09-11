899ebe83c718edb1254b2a000fc362ec70ba1037
George Nachman <gnachman@gmail.com>
2018-09-10 23:50:08 -0700

    Add support for python 3.7 to copy_api.sh

--- tools/copy_api.sh
+++ tools/copy_api.sh
@@ -1,1 +1,2 @@
 cp api/library/python/iterm2/iterm2/* ~/Library/ApplicationSupport/iTerm2/iterm2env/versions/3.6.5/lib/python3.6/site-packages/iterm2/
+cp api/library/python/iterm2/iterm2/* ~/Library/ApplicationSupport/iTerm2/iterm2env/versions/3.7.0/lib/python3.7/site-packages/iterm2/
