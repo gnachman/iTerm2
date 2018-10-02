417494218029e7a253926d6d1aca41c6e2abaeb2
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2018-10-02 17:40:22 +0200

    Do not for whiteBackground in Preferences->Profiles->List (Mojave rules)

--- sources/ProfileListView.m
+++ sources/ProfileListView.m
@@ -138,7 +138,7 @@ const CGFloat kDefaultTagsWidth = 80;
         [tableView_ setAllowsEmptySelection:YES];
         [tableView_ setAllowsMultipleSelection:NO];
         [tableView_ setAllowsTypeSelect:NO];
-        [tableView_ setBackgroundColor:[NSColor whiteColor]];
+      //  [tableView_ setBackgroundColor:[NSColor whiteColor]];
 
         tableColumn_ =
             [[NSTableColumn alloc] initWithIdentifier:@"name"];
