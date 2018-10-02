7e1f68c48cfc5cec959347f1910141ff45993af2
Josh Soref <jsoref@users.noreply.github.com>
2018-10-02 18:09:43 -0400

    spelling: assignment

--- sources/iTermHotkeyPreferencesWindowController.h
+++ sources/iTermHotkeyPreferencesWindowController.h
@@ -8,7 +8,7 @@
 
 @interface iTermHotkeyPreferencesWindowController : NSWindowController
 
-// Assign to this before using it. UI will be updated on assignemnt. Model will be updated when
+// Assign to this before using it. UI will be updated on assignment. Model will be updated when
 // the user interacts with the UI.
 @property(nonatomic, retain) iTermHotkeyPreferencesModel *model;
 @property(nonatomic, copy) NSArray<iTermHotKeyDescriptor *> *descriptorsInUseByOtherProfiles;
