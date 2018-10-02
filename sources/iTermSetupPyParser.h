53b1364e9a122cec7d58c46c60d0937c362a7b8b
Josh Soref <jsoref@users.noreply.github.com>
2018-10-02 18:30:50 -0400

    spelling: executable

--- sources/iTermSetupPyParser.h
+++ sources/iTermSetupPyParser.h
@@ -7,7 +7,7 @@
 
 #import <Foundation/Foundation.h>
 
-// This thing is a hack to avoid running excutable code while installing a script. The user has to
+// This thing is a hack to avoid running executable code while installing a script. The user has to
 // have a chance to inspect what they installed before it has the chance to do any damage.
 // It wants a install_requires=[...] all one one line containing a list of strings quoted with '
 // or " and delimited by commas. Version numbers aren't supported, but that could be added later.
