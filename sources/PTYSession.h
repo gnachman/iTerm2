1f5c202db78223102e082a02bb0e20be1374c77c
Josh Soref <jsoref@users.noreply.github.com>
2018-10-02 22:15:18 -0400

    spelling: mustn't

--- sources/PTYSession.h
+++ sources/PTYSession.h
@@ -369,7 +369,7 @@ typedef enum {
 // You should usually not assign to this; instead use divorceAddressBookEntryFromPreferences.
 @property(nonatomic, assign) BOOL isDivorced;
 
-// Ignore resize notifications. This would be set because the session's size musn't be changed
+// Ignore resize notifications. This would be set because the session's size mustn't be changed
 // due to temporary changes in the window size, as code later on may need to know the session's
 // size to set the window size properly.
 @property(nonatomic, assign) BOOL ignoreResizeNotifications;
