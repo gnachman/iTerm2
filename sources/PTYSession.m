e726ceed524501c3c1457fce40fa0989f153d040
Stuart Carnie <stuart.carnie@gmail.com>
2018-10-16 15:14:04 -0700

    fix: Use autorelease to ensure objects are freed

--- sources/PTYSession.m
+++ sources/PTYSession.m
@@ -7490,7 +7490,7 @@ ITERM_WEAKLY_REFERENCEABLE
     iTermVariableScope *scope = [[iTermVariableScope alloc] init];
     [scope addVariables:self.variables toScopeNamed:nil];
     [scope addVariables:[iTermVariables globalInstance] toScopeNamed:iTermVariableKeyGlobalScopeName];
-    return scope;
+    return [scope autorelease];
 }
 
 - (BOOL)textViewSuppressingAllOutput {
