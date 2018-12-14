gnachman/iTerm2

commit 5062f7f7337ac8c3ecdf68c6fe3cbff26446821f

Pinned, visible hotkey windows participate in window cycle by default. Issue 7374

sources/PseudoTerminal.m
@@ -828,24 +828,43 @@ static NSRect iTermRectCenteredVerticallyWithinRect(NSRect frameToCenter, NSRect
     return self.isHotKeyWindow && [[[iTermHotKeyController sharedInstance] profileHotKeyForWindowController:self] floats];
 }
 
-- (void)setHotkeyWindowType:(iTermHotkeyWindowType)hotkeyWindowType {
-    _hotkeyWindowType = hotkeyWindowType;
-    switch (hotkeyWindowType) {
+- (NSWindowCollectionBehavior)desiredWindowCollectionBehavior {
+    NSWindowCollectionBehavior result = self.window.collectionBehavior;
+    if (_spaceSetting == iTermProfileJoinsAllSpaces) {
+        result |= NSWindowCollectionBehaviorCanJoinAllSpaces;
+    }
+    switch (_hotkeyWindowType) {
         case iTermHotkeyWindowTypeNone:
             // This allows the window to enter Lion fullscreen.
-            [[self window] setCollectionBehavior:[[self window] collectionBehavior] | NSWindowCollectionBehaviorFullScreenPrimary];
-            break;
+            result |= NSWindowCollectionBehaviorFullScreenPrimary;
+            return result;
 
         case iTermHotkeyWindowTypeRegular:
         case iTermHotkeyWindowTypeFloatingPanel:
-        case iTermHotkeyWindowTypeFloatingWindow:
-            [[self window] setCollectionBehavior:[[self window] collectionBehavior] | NSWindowCollectionBehaviorFullScreenAuxiliary];
-            if (![iTermAdvancedSettingsModel hotkeyWindowsExcludedFromCycling]) {
-                [[self window] setCollectionBehavior:[[self window] collectionBehavior] | NSWindowCollectionBehaviorIgnoresCycle];
-                [[self window] setCollectionBehavior:[[self window] collectionBehavior] & ~NSWindowCollectionBehaviorParticipatesInCycle];
+        case iTermHotkeyWindowTypeFloatingWindow: {
+            NSWindowCollectionBehavior result = self.window.collectionBehavior;
+            result |= NSWindowCollectionBehaviorFullScreenAuxiliary;
+            BOOL excludeFromCycling = [iTermAdvancedSettingsModel hotkeyWindowsExcludedFromCycling];
+            if (!excludeFromCycling) {
+                iTermProfileHotKey *profileHotKey = [[iTermHotKeyController sharedInstance] profileHotKeyForWindowController:self];
+                excludeFromCycling = !profileHotKey.isHotKeyWindowOpen;
+            }
+            if (excludeFromCycling) {
+                result |= NSWindowCollectionBehaviorIgnoresCycle;
+                result &= ~NSWindowCollectionBehaviorParticipatesInCycle;
+            } else {
+                result &= ~NSWindowCollectionBehaviorIgnoresCycle;
+                result |= NSWindowCollectionBehaviorParticipatesInCycle;
             }
             break;
+        }
     }
+    return result;
+}
+
+- (void)setHotkeyWindowType:(iTermHotkeyWindowType)hotkeyWindowType {
+    _hotkeyWindowType = hotkeyWindowType;
+    self.window.collectionBehavior = self.desiredWindowCollectionBehavior;
 }
 
 - (void)finishToolbeltInitialization {
@@ -2586,10 +2605,12 @@ ITERM_WEAKLY_REFERENCEABLE
     }
 
     Profile* addressbookEntry = [[[[[self tabs] objectAtIndex:0] sessions] objectAtIndex:0] profile];
-    if ([addressbookEntry[KEY_SPACE] intValue] == iTermProfileJoinsAllSpaces) {
-        [[self window] setCollectionBehavior:[[self window] collectionBehavior] | NSWindowCollectionBehaviorCanJoinAllSpaces];
-    } else if ([addressbookEntry[KEY_SPACE] intValue] == iTermProfileOpenInCurrentSpace) {
-        _openInCurrentSpace = YES;
+    _spaceSetting = [addressbookEntry[KEY_SPACE] intValue];
+    switch ([addressbookEntry[KEY_SPACE] intValue]) {
+        case iTermProfileJoinsAllSpaces:
+            self.window.collectionBehavior = [self desiredWindowCollectionBehavior];
+        case iTermProfileOpenInCurrentSpace:
+            break;
     }
     if ([arrangement objectForKey:TERMINAL_GUID] &&
         [[arrangement objectForKey:TERMINAL_GUID] isKindOfClass:[NSString class]]) {
@@ -8632,11 +8653,15 @@ ITERM_WEAKLY_REFERENCEABLE
                                              completion:nil];
 
     // On Lion, a window that can join all spaces can't go fullscreen.
-    if ([self numberOfTabs] == 1 &&
-        [profile[KEY_SPACE] intValue] == iTermProfileJoinsAllSpaces) {
-        [[self window] setCollectionBehavior:[[self window] collectionBehavior] | NSWindowCollectionBehaviorCanJoinAllSpaces];
-    } else if ([profile[KEY_SPACE] intValue] == iTermProfileOpenInCurrentSpace) {
-        _openInCurrentSpace = YES;
+    if ([self numberOfTabs] == 1) {
+        _spaceSetting = [profile[KEY_SPACE] intValue];
+        switch (_spaceSetting) {
+            case iTermProfileJoinsAllSpaces:
+                self.window.collectionBehavior = [self desiredWindowCollectionBehavior];
+            case iTermProfileOpenInCurrentSpace:
+            default:
+                break;
+        }
     }
 
     return aSession;

sources/ITAddressBookMgr.h
@@ -120,7 +120,7 @@
 #define KEY_USE_CUSTOM_WINDOW_TITLE           @"Use Custom Window Title"
 #define KEY_CUSTOM_WINDOW_TITLE               @"Custom Window Title"
 #define KEY_SCREEN                 @"Screen"
-#define KEY_SPACE                  @"Space"
+#define KEY_SPACE                  @"Space"  // integer, iTermProfileSpaceSetting
 #define KEY_NORMAL_FONT            @"Normal Font"
 #define KEY_NON_ASCII_FONT         @"Non Ascii Font"
 #define KEY_HORIZONTAL_SPACING     @"Horizontal Spacing"
@@ -242,8 +242,10 @@ extern NSString *const iTermUnicodeVersionDidChangeNotification;
 extern const NSTimeInterval kMinimumAntiIdlePeriod;
 
 // Special values for KEY_SPACE.
-extern const NSInteger iTermProfileJoinsAllSpaces;
-extern const NSInteger iTermProfileOpenInCurrentSpace;
+typedef NS_ENUM(NSInteger, iTermProfileSpaceSetting) {
+    iTermProfileJoinsAllSpaces = -1,
+    iTermProfileOpenInCurrentSpace = 0
+};
 
 // The numerical values for each enum matter because they are used in
 // the UI as "tag" values for each select list item. They are also

sources/ITAddressBookMgr.m
@@ -46,8 +46,6 @@
 NSString *const iTermUnicodeVersionDidChangeNotification = @"iTermUnicodeVersionDidChangeNotification";
 
 const NSTimeInterval kMinimumAntiIdlePeriod = 1.0;
-const NSInteger iTermProfileJoinsAllSpaces = -1;
-const NSInteger iTermProfileOpenInCurrentSpace = 0;
 
 static NSMutableArray<NSNotification *> *sDelayedNotifications;
 

sources/PseudoTerminal.h
@@ -2,6 +2,7 @@
 
 #import "Autocomplete.h"
 #import "FutureMethods.h"
+#import "ITAddressBookMgr.h"
 #import "iTermBroadcastInputHelper.h"
 #import "iTermController.h"
 #import "iTermInstantReplayWindowController.h"
@@ -85,7 +86,7 @@ extern NSString *const iTermSelectedTabDidChange;
 @property(nonatomic) BOOL restorableStateDecodePending;
 
 // Used only by hotkey windows. Indicate if it should move to the active space when opening.
-@property(nonatomic, readonly) BOOL openInCurrentSpace;
+@property (nonatomic, readonly) iTermProfileSpaceSetting spaceSetting;
 
 @property(nonatomic, readonly) BOOL hasBeenKeySinceActivation;
 
@@ -93,6 +94,7 @@ extern NSString *const iTermSelectedTabDidChange;
 @property(nonatomic, readonly) int number;
 @property(nonatomic, readonly) Profile *initialProfile;
 @property(nonatomic, readonly) iTermVariableScope *scope;
+@property(nonatomic, readonly) NSWindowCollectionBehavior desiredWindowCollectionBehavior;
 
 // Draws a mock-up of a window arrangement into the current graphics context.
 // |frames| gives an array of NSValue's having NSRect values for each screen,

sources/iTermAdvancedSettingsModel.m
@@ -310,7 +310,7 @@ DEFINE_STRING(pathToTelnet, @"telnet", SECTION_GENERAL @"Path to telnet for open
 DEFINE_STRING(fallbackLCCType, @"", SECTION_GENERAL @"Value to set LC_CTYPE to if the machine‘s combination of country and language are not supported.\nIf unset, the encoding (e.g., UTF-8) will be used.");
 // See issue 6994
 DEFINE_BOOL(useVirtualKeyCodesForDetectingDigits, NO, SECTION_GENERAL @"Treat the top row of keys like number keys on an English keyboard for the purposes of switching panes, tabs, and windows with modifier+number.\nFor example, AZERTY requires you to hold down Shift to enter a number. To switch tabs with ⌘+Number on an AZERTY keyboard, you must enable this setting. Then, for example, ⌘-& switches to tab 1. When this setting is enabled, some user-defined shortcuts may become unavailable because the tab/window/pane switching behavior takes precedence.");
-DEFINE_BOOL(hotkeyWindowsExcludedFromCycling, YES, SECTION_GENERAL @"Hotkey windows are excluded from Cycle Through Windows.");
+DEFINE_BOOL(hotkeyWindowsExcludedFromCycling, NO, SECTION_GENERAL @"Hotkey windows are excluded from Cycle Through Windows.");
 
 #pragma mark - Drawing
 

sources/iTermProfileHotKey.m
@@ -631,7 +631,8 @@ static NSString *const kArrangement = @"Arrangement";
     if (self.windowController.weaklyReferencedObject) {
         DLog(@"already have a hotkey window created");
         if (self.windowController.window.alphaValue == 1) {
-            if (self.windowController.openInCurrentSpace && !self.windowController.window.isOnActiveSpace) {
+            if (self.windowController.spaceSetting == iTermProfileOpenInCurrentSpace &&
+                !self.windowController.window.isOnActiveSpace) {
                 DLog(@"Move already-open hotkey window to current space");
                 NSWindow *window = self.windowController.window;
                 // I tested this on 10.12 and it's sufficient to move the window. Maybe not in older OS versions?
@@ -728,6 +729,7 @@ static NSString *const kArrangement = @"Arrangement";
     [self.windowController.window makeKeyAndOrderFront:nil];
     [self.windowController.window makeFirstResponder:self.windowController.currentSession.textview];
     [[self.windowController currentTab] recheckBlur];
+    self.windowController.window.collectionBehavior = self.windowController.desiredWindowCollectionBehavior;
 }
 
 - (void)didFinishRollingOut {
@@ -746,6 +748,7 @@ static NSString *const kArrangement = @"Arrangement";
     } else {
         [self orderOut];
     }
+    self.windowController.window.collectionBehavior = self.windowController.desiredWindowCollectionBehavior;
 }
 
 - (void)orderOut {

commit 78b76fb8572dd65276ba2fba93e9e0aeab924275

Fix a bug where we set the wrong window collection behavior because a variable was shadowed. Caused by 5062f7f7337ac8c3ecdf68c6fe3cbff26446821f. Issue 7416.

sources/PseudoTerminal.m
@@ -842,7 +842,6 @@ static NSRect iTermRectCenteredVerticallyWithinRect(NSRect frameToCenter, NSRect
         case iTermHotkeyWindowTypeRegular:
         case iTermHotkeyWindowTypeFloatingPanel:
         case iTermHotkeyWindowTypeFloatingWindow: {
-            NSWindowCollectionBehavior result = self.window.collectionBehavior;
             result |= NSWindowCollectionBehaviorFullScreenAuxiliary;
             BOOL excludeFromCycling = [iTermAdvancedSettingsModel hotkeyWindowsExcludedFromCycling];
             if (!excludeFromCycling) {