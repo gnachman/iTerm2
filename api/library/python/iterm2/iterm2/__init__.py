cb708c932f16e205ce20373fe6eefdb5ce68e1d3
George Nachman <gnachman@gmail.com>
2018-12-22 11:41:32 -0800

    [python] Remove async_subscribe_to_focus_change_notification from the public api

--- api/library/python/iterm2/iterm2/__init__.py
+++ api/library/python/iterm2/iterm2/__init__.py
@@ -19,7 +19,7 @@ from iterm2.lifecycle import SessionTerminationMonitor, LayoutChangeMonitor, New
 
 from iterm2.mainmenu import MenuItemState, MainMenu, MenuItemException
 
-from iterm2.notifications import async_unsubscribe, async_subscribe_to_focus_change_notification, RPC_ROLE_GENERIC, RPC_ROLE_SESSION_TITLE
+from iterm2.notifications import async_unsubscribe, RPC_ROLE_GENERIC, RPC_ROLE_SESSION_TITLE
 
 from iterm2.keyboard import Modifier, Keycode, Keystroke, KeystrokePattern, KeystrokeMonitor, KeystrokeFilter
 
