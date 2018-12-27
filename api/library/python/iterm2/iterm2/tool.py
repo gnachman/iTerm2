db727ea0d60ab977477faca3f1b59829ce18af29
George Nachman <gnachman@gmail.com>
2018-12-27 12:07:30 -0800

    [python] Add type hints to tool

--- api/library/python/iterm2/iterm2/tool.py
+++ api/library/python/iterm2/iterm2/tool.py
@@ -1,25 +1,24 @@
 """Provides functions to register toolbelt webivew tools."""
 
 import iterm2.rpc
+import iterm2
 
-async def async_register_web_view_tool(connection,
-                                       display_name,
-                                       identifier,
-                                       reveal_if_already_registered,
-                                       url):
+async def async_register_web_view_tool(
+        connection: iterm2.Connection,
+        display_name: str,
+        identifier: str,
+        reveal_if_already_registered: bool,
+        url: str) -> None:
     """
     Registers a toolbelt tool that shows a webview.
 
-    :param connection: A connected iterm2.connection.Connection.
+    :param connection: The connection to iTerm2.
     :param display_name: The name of the tool. User-visible.
-    :param identifier: A unique ID that prevents duplicate registration.
-    :param reveal_if_already_registered: Bool. If True, shows the tool on a duplicate registration
-        attempt.
+    :param identifier: A unique ID for this tool. Only one tool with a given identifier may be registered at a time.
+    :param reveal_if_already_registered: If `True`, shows the tool on a duplicate registration attempt.
     :param url: The URL to show in the webview.
 
-    :returns: iterm2.api_pb2.RegisterToolResponse on success
-
-    :raises: iterm2.rpc.RPCException if something goes wrong
+    :raises: :class:`iterm2.RPCException` if something goes wrong
     """
     result = await iterm2.rpc.async_register_web_view_tool(
         connection,
@@ -29,6 +28,6 @@ async def async_register_web_view_tool(connection,
         url)
     status = result.register_tool_response.status
     if status == iterm2.api_pb2.RegisterToolResponse.Status.Value("OK"):
-        return result
+        return None
     else:
         raise iterm2.rpc.RPCException(result.register_tool_response)
