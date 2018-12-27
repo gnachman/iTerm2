855f9a1187257f6a1be612123847f3eeb48e803c
George Nachman <gnachman@gmail.com>
2018-12-26 22:11:59 -0800

    [python] Add type annotations to color presets

--- api/library/python/iterm2/docs/colorpresets.rst
+++ api/library/python/iterm2/docs/colorpresets.rst
@@ -3,6 +3,8 @@ Color Presets
 .. automodule:: iterm2.colorpresets
 .. autoclass:: iterm2.ColorPreset
    :members: async_get_list, async_get, values
+.. autoclass:: iterm2.ColorPreset.Color
+   :members: key
 .. autoclass:: iterm2.ListPresetsException
 .. autoclass:: iterm2.GetPresetException
 
--- api/library/python/iterm2/iterm2/colorpresets.py
+++ api/library/python/iterm2/iterm2/colorpresets.py
@@ -1,7 +1,9 @@
 """Provides access to color presets."""
 
+import iterm2.connection
 import iterm2.color
 import iterm2.rpc
+import typing
 
 class ListPresetsException(Exception):
     """Something went wrong listing presets."""
@@ -13,12 +15,16 @@ class GetPresetException(Exception):
 
 class ColorPreset:
     class Color(iterm2.color.Color):
+        """Derives from :class:`iterm2.Color`.
+
+        Note this is `iterm2.ColorPreset.Color`, despite how Sphinx chooses to format this documentation."""
         def __init__(self, r, g, b, a, color_space, key):
             super().__init__(r, g, b, a, color_space)
             self.__key = key
 
         @property
-        def key(self):
+        def key(self) -> str:
+            """Describes which property this color affects."""
             return self.__key
 
         def __repr__(self):
@@ -31,12 +37,12 @@ class ColorPreset:
                 self.color_space)
 
     @staticmethod
-    async def async_get_list(connection):
+    async def async_get_list(connection: iterm2.connection.Connection) -> typing.List[str]:
         """Fetches a list of color presets.
 
         :param connection: An :class:`iterm2.Connection`.
 
-        :returns: A list of names as strings.
+        :returns: Names of the color presets.
         """
         result = await iterm2.rpc.async_list_color_presets(connection)
         if result.color_preset_response.status == iterm2.api_pb2.ColorPresetResponse.Status.Value("OK"):
@@ -45,13 +51,13 @@ class ColorPreset:
             raise GetPresetException(iterm2.api_pb2.ColorPresetResponse.Status.Name(result.color_preset_response.status))
 
     @staticmethod
-    async def async_get(connection, name):
+    async def async_get(connection: iterm2.connection.Connection, name: str) -> typing.Union[None, 'ColorPreset']:
         """Fetches a color preset with the given name.
 
-        :param connection: An :class:`iterm2.Connection`.
+        :param connection: The connection to iTerm2.
         :param name: The name of the preset to fetch.
 
-        :returns: Either a new :class:`ColorPreset` or None."""
+        :returns: Either a new preset or `None`."""
         result = await iterm2.rpc.async_get_color_preset(connection, name)
         if result.color_preset_response.status == iterm2.api_pb2.ColorPresetResponse.Status.Value("OK"):
             return ColorPreset(result.color_preset_response.get_preset.color_settings)
@@ -71,10 +77,10 @@ class ColorPreset:
                 setting.key))
 
     @property
-    def values(self):
+    def values(self) -> typing.List['ColorPreset.Color']:
         """Returns a list of color settings.
 
-        :returns: A list of :class:`Color` objects.
+        :returns: The colors belonging to the preset.
         """
         return self.__values
 
