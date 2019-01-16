8e5562edb31a09bd243f3bf5a014077862a969d3
George Nachman <gnachman@gmail.com>
2019-01-15 23:40:36 -0800

    Fixes a bug where underline offsets were computed incorrectly. The INSANELY confusing NSFontAttributeName and NSFontNameAttribute got me. F7U12

--- sources/PTYFontInfo.m
+++ sources/PTYFontInfo.m
@@ -173,7 +173,7 @@
     NSLayoutManager *layoutManager = [[[NSLayoutManager alloc] init] autorelease];
     NSTextContainer *textContainer = [[[NSTextContainer alloc] init] autorelease];
     [layoutManager addTextContainer:textContainer];
-    NSDictionary *attributes = @{ NSFontNameAttribute: font_,
+    NSDictionary *attributes = @{ NSFontAttributeName: font_,
                                   NSUnderlineStyleAttributeName: @(NSUnderlineStyleSingle) };
     NSAttributedString *attributedString = [[[NSAttributedString alloc] initWithString:@"M" attributes:attributes] autorelease];
     NSTextStorage *textStorage = [[[NSTextStorage alloc] initWithAttributedString:attributedString] autorelease];
