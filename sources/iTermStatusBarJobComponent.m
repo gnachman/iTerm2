da284bc9d0ddb0efa7ffdcebcd215a32c07493bb
George Nachman <gnachman@gmail.com>
2019-01-23 20:57:26 -0800

    [statusbar] Disable jobs component popover in 10.13 because it looks bad. The text views and disclosure triangles have a 50% alpha gray background. It looks like a compositing bug to me because modifying the textfield's alpha value does not fade it out but hiding the textfield hides the background. And besides it is also on the disclosure triangles so even if I beat the textfield into submission I'd also have to make my own disclosure triangle and honestly just F it Apple gives me nothing and I'm tired of fighting

--- sources/iTermStatusBarJobComponent.m
+++ sources/iTermStatusBarJobComponent.m
@@ -128,7 +128,11 @@ NS_ASSUME_NONNULL_BEGIN
 }
 
 - (BOOL)statusBarComponentHandlesClicks {
-    return YES;
+    if (@available(macOS 10.14, *)) {
+        return YES;
+    }
+    // The outline view looks awful on 10.13 for no good reason.
+    return NO;
 }
 
 - (void)statusBarComponentMouseDownWithView:(NSView *)view {
