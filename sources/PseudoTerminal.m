c295531e7ff1b4ce72964abceca152488909324e
George Nachman <gnachman@gmail.com>
2019-01-28 22:54:45 -0800

    Make title bar transparent for compact and non-title-bar windows. Issue 7493

--- sources/PseudoTerminal.m
+++ sources/PseudoTerminal.m
@@ -5811,7 +5811,30 @@ ITERM_WEAKLY_REFERENCEABLE
             break;
     }
     self.window.backgroundColor = self.anyPaneIsTransparent ? [NSColor clearColor] : [NSColor windowBackgroundColor];
-    self.window.titlebarAppearsTransparent = NO;  // Keep it from showing content from other windows behind it. Issue 7108.
+    self.window.titlebarAppearsTransparent = [self titleBarShouldAppearTransparent];  // Keep it from showing content from other windows behind it. Issue 7108.
+}
+
+- (BOOL)titleBarShouldAppearTransparent {
+    switch (windowType_) {
+        case WINDOW_TYPE_TOP:
+        case WINDOW_TYPE_LEFT:
+        case WINDOW_TYPE_RIGHT:
+        case WINDOW_TYPE_BOTTOM:
+        case WINDOW_TYPE_NORMAL:
+        case WINDOW_TYPE_ACCESSORY:
+        case WINDOW_TYPE_TOP_PARTIAL:
+        case WINDOW_TYPE_LEFT_PARTIAL:
+        case WINDOW_TYPE_RIGHT_PARTIAL:
+        case WINDOW_TYPE_BOTTOM_PARTIAL:
+        case WINDOW_TYPE_LION_FULL_SCREEN:
+        case WINDOW_TYPE_TRADITIONAL_FULL_SCREEN:
+            break;
+
+        case WINDOW_TYPE_NO_TITLE_BAR:
+        case WINDOW_TYPE_COMPACT:
+            return YES;
+    }
+    return NO;
 }
 
 - (void)setLegacyBackgroundColor:(nullable NSColor *)backgroundColor {
