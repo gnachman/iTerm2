80751a06315e2a6999c3b81ebf05fb5f95d39df5
George Nachman <gnachman@gmail.com>
2019-02-16 14:48:40 -0800

    Disable window shadow with transparency prior to mojave by default because I got a second report of the issue in 7542

--- sources/iTermAdvancedSettingsModel.m
+++ sources/iTermAdvancedSettingsModel.m
@@ -396,7 +396,7 @@ DEFINE_BOOL(serializeOpeningMultipleFullScreenWindows, YES, SECTION_WINDOWS @"Wh
 DEFINE_BOOL(trackingRunloopForLiveResize, YES, SECTION_WINDOWS @"Use a tracking runloop for live resizing.\nThis allows the terminal to redraw during a resizing drag.");
 DEFINE_FLOAT(invalidateShadowTimesPerSecond, 15, SECTION_WINDOWS @"How many times per second to update the shadow of transparent windows to prevent ghosting.\nThis works around a macOS Mojave bug that leaves a ghost of past window contents behind in transparent windows. It hurts performance to do it frequently, especially in large windows. Set to 0 to disable.");
 DEFINE_BOOL(disableWindowShadowWhenTransparencyOnMojave, YES, SECTION_WINDOWS @"Disable the window shadow on Mojave when the window has a transparent session to improve performance.");
-DEFINE_BOOL(disableWindowShadowWhenTransparencyPreMojave, NO, SECTION_WINDOWS @"Disable the window shadow on High Sierra and earlier when the window has a transparent session to prevent text shadows.");
+DEFINE_BOOL(disableWindowShadowWhenTransparencyPreMojave, YES, SECTION_WINDOWS @"Disable the window shadow on High Sierra and earlier when the window has a transparent session to prevent text shadows.");
 
 #pragma mark tmux
 
