81e6d874726470faa696c5a64bfb5ff98e8aab08
George Nachman <gnachman@gmail.com>
2019-02-28 20:42:41 -0800

    Make copy_api a little more verbose and less error prone

--- tools/copy_api.sh
+++ tools/copy_api.sh
@@ -1,5 +1,8 @@
 #!/bin/tcsh
 foreach x (~/Library/ApplicationSupport/iTerm2/iterm2env*/versions/*/lib/python3.7/site-packages/iterm2/)
+    echo copy to $x
 cp api/library/python/iterm2/iterm2/* $x
 end
-cp api/library/python/iterm2/iterm2/* /usr/local/lib/python3.7/site-packages/iterm2
+
+test -d /usr/local/lib/python3.7/site-packages/iterm2 && \
+echo cp api/library/python/iterm2/iterm2/* /usr/local/lib/python3.7/site-packages/iterm2
