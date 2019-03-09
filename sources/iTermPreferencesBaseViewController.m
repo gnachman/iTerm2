41a9b2fe320fbc377242360e1a48d73f72a79607
George Nachman <gnachman@gmail.com>
2019-03-09 13:01:26 -0800

    Tweak bottom margin in prefs to be equal to the top margin.

--- sources/iTermPreferencesBaseViewController.m
+++ sources/iTermPreferencesBaseViewController.m
@@ -628,8 +628,9 @@ NSString *const kPreferenceDidChangeFromOtherPanelKeyUserInfoKey = @"key";
 - (void)resizeWindowForView:(NSView *)theView tabView:(NSTabView *)tabView animated:(BOOL)animated {
     const CGFloat kTabViewMinWidth = self.minimumWidth;
     const CGFloat inset = NSWidth(tabView.bounds) - NSWidth(theView.superview.bounds);
+    const CGFloat bottomMargin = 36;
     CGSize tabViewSize = NSMakeSize(MAX(kTabViewMinWidth, theView.bounds.size.width) + inset,
-                                    theView.bounds.size.height + 50);
+                                    theView.bounds.size.height + bottomMargin);
     NSRect frame = [self windowFrameForTabViewSize:tabViewSize tabView:tabView];
     if (NSEqualRects(_desiredFrame, frame)) {
         return;
