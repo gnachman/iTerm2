60ecaa882954d9b01a9333a0c7826e847596c342
George Nachman <gnachman@gmail.com>
2019-03-11 19:44:04 -0700

    Revert "Ask for accessibility permissions after failing to create an event tap"
    This reverts commit 4a94d5a6f2d1219a0b60a1e6aaad7480ed46ef06.
    
    I was getting two prompts concurrently.

--- sources/iTermEventTap.m
+++ sources/iTermEventTap.m
@@ -210,7 +210,6 @@ static CGEventRef iTermEventTapCallback(CGEventTapProxy proxy,
     if (!_machPort) {
         XLog(@"CGEventTapCreate failed");
         AppendPinnedDebugLogMessage(@"EventTap", @"CGEventTapCreate failed");
-        AXIsProcessTrustedWithOptions((__bridge CFDictionaryRef)@{(id)kAXTrustedCheckOptionPrompt: @YES});
         goto error;
     }
 
