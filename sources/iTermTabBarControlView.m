399ea7267170a66223e6143b11235fa92f27d2b8
George Nachman <gnachman@gmail.com>
2019-03-26 22:34:35 -0700

    Use performWindowDragWithEvent: instead of using my own tracking loop so that drags that go to the edge of the display will move to the next space. Issue 7628

--- sources/iTermTabBarControlView.m
+++ sources/iTermTabBarControlView.m
@@ -269,40 +269,11 @@ typedef NS_ENUM(NSInteger, iTermTabBarFlashState) {
                              hitView == self &&
                              ![self.itermTabBarDelegate iTermTabBarWindowIsFullScreen]);
     if (handleDrag) {
-        [self trackClickForWindowMove:event];
+        [self.window performWindowDragWithEvent:event];
         return;
     }
     
     [super mouseDown:event];
-}
-
-- (void)trackClickForWindowMove:(NSEvent*)event {
-    NSWindow *window = self.window;
-    NSPoint origin = [window frame].origin;
-    NSPoint lastPointInScreenCoords = [NSEvent mouseLocation];
-    const NSEventMask eventMask = (NSEventMaskLeftMouseDown |
-                                   NSEventMaskLeftMouseDragged |
-                                   NSEventMaskLeftMouseUp);
-    event = [NSApp nextEventMatchingMask:eventMask
-                               untilDate:[NSDate distantFuture]
-                                  inMode:NSEventTrackingRunLoopMode
-                                 dequeue:YES];
-    while (event && event.type != NSEventTypeLeftMouseUp) {
-        @autoreleasepool {
-            NSPoint currentPointInScreenCoords = [NSEvent mouseLocation];
-            
-            origin.x += currentPointInScreenCoords.x - lastPointInScreenCoords.x;
-            origin.y += currentPointInScreenCoords.y - lastPointInScreenCoords.y;
-            lastPointInScreenCoords = currentPointInScreenCoords;
-            
-            [window setFrameOrigin:origin];
-            
-            event = [NSApp nextEventMatchingMask:eventMask
-                                       untilDate:[NSDate distantFuture]
-                                          inMode:NSEventTrackingRunLoopMode
-                                         dequeue:YES];
-        }
-    }
 }
 
 @end
