gnachman/iTerm2

commit daa6fb4a123d04de581c6beee8697b525ef7ee7d

Add some performance improvements to the legacy renderer for box drawing. See email to list with subject line 'Major performance hit with truecolor'

sources/iTermBoxDrawingBezierCurveFactory.h
@@ -15,8 +15,9 @@
                         cellSize:(NSSize)cellSize
                            scale:(CGFloat)scale
                           offset:(CGPoint)offset
-                           color:(NSColor *)color
+                           color:(NSColor *)color  // only needed for powerline glyphs
         useNativePowerlineGlyphs:(BOOL)useNativePowerlineGlyphs;
 
++ (BOOL)isPowerlineGlyph:(unichar)code;
 
 @end

sources/iTermBoxDrawingBezierCurveFactory.m
@@ -330,28 +330,37 @@
                    hints:nil];
 }
 
++ (BOOL)isPowerlineGlyph:(unichar)code {
+    switch (code) {
+        case 0xE0A0:  // Version control branch
+        case 0xE0A1:  // LN (line) symbol
+        case 0xE0A2:  // Closed padlock
+        case 0xE0B0:  // Rightward black arrowhead
+        case 0xE0B1:  // Rightwards arrowhead
+        case 0xE0B2:  // Leftwards black arrowhead
+        case 0xE0B3:  // Leftwards arrowhead
+            return YES;
+    }
+    return NO;
+}
+
 + (void)drawCodeInCurrentContext:(unichar)code
                         cellSize:(NSSize)cellSize
                            scale:(CGFloat)scale
                           offset:(CGPoint)offset
                            color:(NSColor *)color
         useNativePowerlineGlyphs:(BOOL)useNativePowerlineGlyphs {
-    if (useNativePowerlineGlyphs) {
-        switch (code) {
-            case 0xE0A0:  // Version control branch
-            case 0xE0A1:  // LN (line) symbol
-            case 0xE0A2:  // Closed padlock
-            case 0xE0B0:  // Rightward black arrowhead
-            case 0xE0B1:  // Rightwards arrowhead
-            case 0xE0B2:  // Leftwards black arrowhead
-            case 0xE0B3:  // Leftwards arrowhead
-                [self drawPowerlineCode:code
-                               cellSize:cellSize
-                                  color:color];
-                return;
-        }
+    if (useNativePowerlineGlyphs && [self isPowerlineGlyph:code]) {
+        [self drawPowerlineCode:code
+                       cellSize:cellSize
+                          color:color];
+        return;
     }
-    
+    if (code == iTermFullBlock) {
+        // Fast path
+        NSRectFill(NSMakeRect(0, 0, cellSize.width, cellSize.height));
+        return;
+    }
     BOOL solid = NO;
     NSArray<NSBezierPath *> *paths = [iTermBoxDrawingBezierCurveFactory bezierPathsForBoxDrawingCode:code
                                                                                             cellSize:cellSize

sources/iTermTextDrawingHelper.m
@@ -1153,8 +1153,16 @@ typedef struct iTermTextColorContext {
     [transform translateXBy:pos.x yBy:pos.y];
     [transform concat];
 
-    NSColor *color = [NSColor colorWithCGColor:(CGColorRef)attributes[(NSString *)kCTForegroundColorAttributeName]];
-    [color set];
+    NSColor *color = nil;
+    if (self.useNativePowerlineGlyphs &&
+        [iTermBoxDrawingBezierCurveFactory isPowerlineGlyph:theCharacter]) {
+        color = [NSColor colorWithCGColor:(CGColorRef)attributes[(NSString *)kCTForegroundColorAttributeName]];
+        [color set];
+    } else {
+        // Fast path
+        CGContextSetFillColorWithColor(ctx.CGContext,
+                                       (CGColorRef)attributes[(NSString *)kCTForegroundColorAttributeName]);
+    }
     [iTermBoxDrawingBezierCurveFactory drawCodeInCurrentContext:theCharacter
                                                        cellSize:_cellSize
                                                           scale:1
@@ -1228,6 +1236,18 @@ typedef struct iTermTextColorContext {
                 positions:(CGFloat *)positions
                 inContext:(CGContextRef)ctx
           backgroundColor:(NSColor *)backgroundColor {
+    if ([cheapString.attributes[iTermIsBoxDrawingAttribute] boolValue]) {
+        // Special box-drawing cells don't use the font so they look prettier.
+        unichar *chars = (unichar *)cheapString.characters;
+        for (NSUInteger i = 0; i < cheapString.length; i++) {
+            unichar c = chars[i];
+            NSPoint p = NSMakePoint(point.x + positions[i], point.y);
+            [self drawBoxDrawingCharacter:c
+                           withAttributes:cheapString.attributes
+                                       at:p];
+        }
+        return cheapString.length;
+    }
     int result = [self drawFastPathStringWithoutUnderline:cheapString
                                                   atPoint:point
                                                    origin:origin
@@ -2243,9 +2263,6 @@ static BOOL iTermTextDrawingHelperShouldAntiAlias(screen_char_t *c,
                     orCharacter:code
                       positions:positions
                          offset:(i - indexRange.location) * _cellSize.width];
-            if (characterAttributes.boxDrawing) {
-                [builder disableFastPath];
-            }
         }
     }
     if (builder.length) {

sources/iTermMutableAttributedStringBuilder.m
@@ -189,7 +189,7 @@
         _canUseFastPath &= iTermCharacterSupportsFastPath(code, _asciiLigaturesAvailable);
     }
     if (!_characterData) {
-        _characterData = [[NSMutableData alloc] init];
+        _characterData = [[NSMutableData alloc] initWithCapacity:20];
     }
     [_characterData appendBytes:&code length:sizeof(unichar)];
 }

commit 754249556ebe40bbe452805ff56fc066e0c2ce6f

Fix box drawing broken by daa6fb4a1

sources/Metal/Support/iTermCharacterSource.m
@@ -684,7 +684,7 @@ static const CGFloat iTermCharacterSourceAliasedFakeBoldShiftPoints = 1;
                                                                            _descriptor.cellSize.height * _descriptor.scale)
                                                            scale:_descriptor.scale
                                                           offset:offset
-                                                          color:[self textColorForIteration:iteration]
+                                                          color:[[self textColorForIteration:iteration] CGColor]
                                                            useNativePowerlineGlyphs:_useNativePowerlineGlyphs];
 }
 

sources/iTermBoxDrawingBezierCurveFactory.h
@@ -15,7 +15,7 @@
                         cellSize:(NSSize)cellSize
                            scale:(CGFloat)scale
                           offset:(CGPoint)offset
-                           color:(NSColor *)color  // only needed for powerline glyphs
+                           color:(CGColorRef)color
         useNativePowerlineGlyphs:(BOOL)useNativePowerlineGlyphs;
 
 + (BOOL)isPowerlineGlyph:(unichar)code;

sources/iTermBoxDrawingBezierCurveFactory.m
@@ -194,7 +194,7 @@
     [[NSGraphicsContext currentContext] setImageInterpolation:saved];
 }
 
-+ (void)drawPowerlineCode:(unichar)code cellSize:(NSSize)cellSize color:(NSColor *)color {
++ (void)drawPowerlineCode:(unichar)code cellSize:(NSSize)cellSize color:(CGColorRef)color {
     switch (code) {
         case 0xE0A0:
             [self drawPDFWithName:@"PowerlineVersionControlBranch" cellSize:cellSize stretch:NO color:color antialiased:YES];
@@ -235,12 +235,13 @@
 + (NSImage *)imageForPDFNamed:(NSString *)pdfName
                      cellSize:(NSSize)cellSize
                   antialiased:(BOOL)antialiased
-                        color:(NSColor *)color {
+                        color:(CGColorRef)colorRef {
     static iTermImageCache *cache;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         cache = [[iTermImageCache alloc] initWithByteLimit:1024 * 1024];
     });
+    NSColor *color = [NSColor colorWithCGColor:colorRef];
     NSImage *image = [cache imageWithName:pdfName size:cellSize color:color];
     if (image) {
         return image;
@@ -311,7 +312,7 @@
 + (void)drawPDFWithName:(NSString *)pdfName
                cellSize:(NSSize)cellSize
                 stretch:(BOOL)stretch
-                  color:(NSColor *)color
+                  color:(CGColorRef)color
             antialiased:(BOOL)antialiased {
    
     NSImage *image = [self imageForPDFNamed:pdfName
@@ -348,17 +349,19 @@
                         cellSize:(NSSize)cellSize
                            scale:(CGFloat)scale
                           offset:(CGPoint)offset
-                           color:(NSColor *)color
+                           color:(CGColorRef)colorRef
         useNativePowerlineGlyphs:(BOOL)useNativePowerlineGlyphs {
     if (useNativePowerlineGlyphs && [self isPowerlineGlyph:code]) {
         [self drawPowerlineCode:code
                        cellSize:cellSize
-                          color:color];
+                          color:colorRef];
         return;
     }
     if (code == iTermFullBlock) {
         // Fast path
-        NSRectFill(NSMakeRect(0, 0, cellSize.width, cellSize.height));
+        CGContextRef context = [[NSGraphicsContext currentContext] CGContext];
+        CGContextSetFillColorWithColor(context, colorRef);
+        CGContextFillRect(context, CGRectMake(0, 0, cellSize.width, cellSize.height));
         return;
     }
     BOOL solid = NO;
@@ -367,6 +370,8 @@
                                                                                                scale:scale
                                                                                               offset:offset
                                                                                                solid:&solid];
+    NSColor *color = [NSColor colorWithCGColor:colorRef];
+    [color set];
     for (NSBezierPath *path in paths) {
         if (solid) {
             [path fill];

sources/iTermTextDrawingHelper.m
@@ -1153,16 +1153,7 @@ typedef struct iTermTextColorContext {
     [transform translateXBy:pos.x yBy:pos.y];
     [transform concat];
 
-    NSColor *color = nil;
-    if (self.useNativePowerlineGlyphs &&
-        [iTermBoxDrawingBezierCurveFactory isPowerlineGlyph:theCharacter]) {
-        color = [NSColor colorWithCGColor:(CGColorRef)attributes[(NSString *)kCTForegroundColorAttributeName]];
-        [color set];
-    } else {
-        // Fast path
-        CGContextSetFillColorWithColor(ctx.CGContext,
-                                       (CGColorRef)attributes[(NSString *)kCTForegroundColorAttributeName]);
-    }
+    CGColorRef color = (CGColorRef)attributes[(NSString *)kCTForegroundColorAttributeName];
     [iTermBoxDrawingBezierCurveFactory drawCodeInCurrentContext:theCharacter
                                                        cellSize:_cellSize
                                                           scale:1