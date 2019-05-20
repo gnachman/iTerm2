d8efd10163a68359580cd792c657a5320f0e4363
George Nachman <gnachman@gmail.com>
2019-05-19 20:42:56 -0700

    Fix copy to control

--- tools/release_beta.sh
+++ tools/release_beta.sh
@@ -100,9 +100,9 @@ function Build {
   SparkleSign ${SPARKLE_PREFIX}testing3_modern.xml ${SPARKLE_PREFIX}template3_modern.xml
 
   # Copy experiment to control
-  cp ${SPARKLE_PREFIX}testing3.xml ${SPARKLE_PREFIX}template.xml
-  cp ${SPARKLE_PREFIX}testing3_new.xml ${SPARKLE_PREFIX}template_new.xml
-  cp ${SPARKLE_PREFIX}testing3_modern.xml ${SPARKLE_PREFIX}template_modern.xml
+  cp $SVNDIR/source/appcasts/${SPARKLE_PREFIX}testing3.xml        $SVNDIR/source/appcasts/${SPARKLE_PREFIX}template.xml
+  cp $SVNDIR/source/appcasts/${SPARKLE_PREFIX}testing3_new.xml    $SVNDIR/source/appcasts/${SPARKLE_PREFIX}template_new.xml
+  cp $SVNDIR/source/appcasts/${SPARKLE_PREFIX}testing3_modern.xml $SVNDIR/source/appcasts/${SPARKLE_PREFIX}template_modern.xml
 
   popd
 }
