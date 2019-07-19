b24b81bf15cf9ad3412f127421e82e15e4bfba0c
George Nachman <gnachman@gmail.com>
2019-07-18 22:44:18 -0700

    [python] Remove stray print statement

--- api/library/python/iterm2/iterm2/capabilities.py
+++ api/library/python/iterm2/iterm2/capabilities.py
@@ -8,6 +8,5 @@ def ge(a, b):
 
 def supports_multiple_set_profile_properties(connection):
     min_ver = (0, 69)
-    print(f'Capabilities check between {min_ver} and {connection.iterm2_protocol_version} is {ge(min_ver, connection.iterm2_protocol_version)}')
     return ge(connection.iterm2_protocol_version, min_ver)
 
