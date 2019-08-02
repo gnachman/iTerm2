0a1eb6cfac24fc0f989d1fd5cba42f6cdf5ecd6c
George Nachman <gnachman@gmail.com>
2019-08-01 21:43:39 -0700

    Activate the app before showing a modal sheet/alert. This doesn't cover all of the many modal alerts because it's just too much to test right after a release. It fixes issue 7977 - opening the set tab title panel in a floating hotkey window when another app is active causes the window to disappear.

--- sources/NSAlert+iTerm.m
+++ sources/NSAlert+iTerm.m
@@ -10,6 +10,7 @@
 @implementation NSAlert (iTerm)
 
 - (NSInteger)runSheetModalForWindow:(NSWindow *)window {
+    [NSApp activateIgnoringOtherApps:YES];
     [self beginSheetModalForWindow:window completionHandler:^(NSModalResponse returnCode) {
         [NSApp stopModalWithCode:returnCode];
     }];
