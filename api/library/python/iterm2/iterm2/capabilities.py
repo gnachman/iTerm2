aeb0bbeaf52ad926b3ee3332d9e75116adbe10ca
George Nachman <gnachman@gmail.com>
2019-08-05 10:31:59 -0700

    Add support for navigating to a split pane in a direction. Issue 8014.
    Also bumps the lib version *and* protocol version to 1.0.

--- api/library/python/iterm2/iterm2/capabilities.py
+++ api/library/python/iterm2/iterm2/capabilities.py
@@ -1,3 +1,7 @@
+class AppVersionTooOld(Exception):
+    """Raised when you need a newer version of iTerm2."""
+    pass
+
 def ge(a, b):
     """Is a >= b?"""
     if a[0] > b[0]:
@@ -10,3 +14,6 @@ def supports_multiple_set_profile_properties(connection):
     min_ver = (0, 69)
     return ge(connection.iterm2_protocol_version, min_ver)
 
+def supports_select_pane_in_direction(connection):
+    min_ver = (1, 0)
+    return ge(connection.iterm2_protocol_version, min_ver)
