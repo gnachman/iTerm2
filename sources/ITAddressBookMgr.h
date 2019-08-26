ebda75785857b69c4e26fc07276b030574037eda
Daniel Fiori <fiori.daniel@gmail.com>
2019-08-26 15:15:32 -0400

    Add a "Maximized" window type that is always maximized
    This differs from the full-screen type since it does not hide the
    dock or menu bars.
    
    https://github.com/gnachman/iTerm2/pull/398
    
    Originally by Daniel Fiori <fiori.daniel@gmail.com> with minor style tweaks by
    George.

--- sources/ITAddressBookMgr.h
+++ sources/ITAddressBookMgr.h
@@ -302,7 +302,10 @@ typedef enum {
 
     WINDOW_TYPE_NO_TITLE_BAR = 12,
     WINDOW_TYPE_COMPACT = 13,  // May be converted to normal depending on theme
-    WINDOW_TYPE_ACCESSORY = 14
+    WINDOW_TYPE_ACCESSORY = 14,
+
+    WINDOW_TYPE_MAXIMIZED = 15,
+    WINDOW_TYPE_COMPACT_MAXIMIZED = 16
 } iTermWindowType;
 
 iTermWindowType iTermWindowDefaultType(void);
