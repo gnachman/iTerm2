52990d96851ac5b71e0c0a4769da8ac427834d75
George Nachman <gnachman@gmail.com>
2019-09-12 22:28:00 -0700

    Improve legibility of git errors

--- sources/iterm2_git_wrapper.sh
+++ sources/iterm2_git_wrapper.sh
@@ -10,7 +10,7 @@ echo ""
 git "$@" && exit 0
 
 echo $'\e]1337;Disinter\e\\'
-echo -n $'\e[30;41m'
+echo -n $'\e[7m'
 echo -n "An error occurred while running 'git " "$@" "'."
 echo $'\e[m'
 echo "You may safely close this window."
