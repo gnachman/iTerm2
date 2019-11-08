966d10120c21ee3a47d466e5eea313926057e497
George Nachman <gnachman@gmail.com>
2019-11-07 22:42:58 -0800

    Enable dragging the window by the add-tab button. Issue 8046

--- ThirdParty/PSMTabBarControl/source/PSMRolloverButton.m
+++ ThirdParty/PSMTabBarControl/source/PSMRolloverButton.m
@@ -128,10 +128,24 @@ static const CGFloat PSMRolloverButtonMaxAlpha = 0.25;
     self.layer.backgroundColor = [[NSColor colorWithWhite:0.5 alpha:_alpha] CGColor];
 }
 
+// Must override mouseDown and mouseUp and not call super for mouseDragged: to work.
 - (void)mouseDown:(NSEvent *)theEvent {
     // eliminates drawing artifact
-    [[NSRunLoop currentRunLoop] performSelector:@selector(display) target:[self superview] argument:nil order:1 modes:[NSArray arrayWithObjects:@"NSEventTrackingRunLoopMode", @"NSDefaultRunLoopMode", nil]];
-    [super mouseDown:theEvent];
+    [[NSRunLoop currentRunLoop] performSelector:@selector(display)
+                                         target:[self superview]
+                                       argument:nil
+                                          order:1
+                                          modes:@[ NSEventTrackingRunLoopMode, NSDefaultRunLoopMode ]];
+}
+
+- (void)mouseUp:(NSEvent *)event {
+    if (event.clickCount == 1) {
+        [self performClick:self];
+    }
+}
+
+- (void)mouseDragged:(NSEvent *)event {
+    [self.window performWindowDragWithEvent:event];
 }
 
 - (void)resetCursorRects {
