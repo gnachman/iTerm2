gnachman/iTerm2

commit 2067c5e19b3b2ab6aabc8a0d71d7aad3ad73f7df

Fix gray bar that appeared when in native full screen with one tab and no tab bar. Also appears when changing to minimal theme in native full screen. This happened because the style mask was not full-size content view. Issue 8257.
Remove and re-add the tabbar titlebar accessory view when changing its height from 0 (one tab, hidden, minimal theme) to 38 (two tabs, visible tabbar, minimal theme) because otherwise the OS gets confused about how high it should be.

Update the tab bar control's height when updating its style so that the transition from regular to minimal makes it grow. Outside full screen this happens as a side-effect of changing the window style or in -[iTermRootTerminalView layoutSubviews], but the former doesn't happen for obvious reasons and the latter because it doesn't consider itself the owner of the tabbar (it's on loan).

sources/PseudoTerminal.m
@@ -4613,9 +4613,14 @@ ITERM_WEAKLY_REFERENCEABLE
 }
 
 - (NSUInteger)styleMask {
-    return [PseudoTerminal styleMaskForWindowType:self.windowType
-                                  savedWindowType:self.savedWindowType
-                                 hotkeyWindowType:_hotkeyWindowType];
+    NSUInteger styleMask = [PseudoTerminal styleMaskForWindowType:self.windowType
+                                                  savedWindowType:self.savedWindowType
+                                                 hotkeyWindowType:_hotkeyWindowType];
+    if (self.lionFullScreen) {
+        styleMask |= NSWindowStyleMaskFullScreen;
+    }
+    DLog(@"Returning style mask of %@", @(styleMask));
+    return styleMask;
 }
 
 // This is a hack to fix the problem of exiting a fullscreen window that as never not-fullscreen.
@@ -5261,15 +5266,22 @@ ITERM_WEAKLY_REFERENCEABLE
 - (void)updateTabBarControlIsTitlebarAccessoryAssumingFullScreen:(BOOL)fullScreen NS_AVAILABLE_MAC(10_14) {
     const NSInteger index = [self.window.it_titlebarAccessoryViewControllers indexOfObject:_lionFullScreenTabBarViewController];
     if (fullScreen && [self shouldMoveTabBarToTitlebarAccessoryInLionFullScreen]) {
+        NSRect frame = _lionFullScreenTabBarViewController.view.superview.bounds;
+        if (index != NSNotFound) {
+            DLog(@"Removing title bar accessory view for tabbar. Will re-add it shortly");
+            [self.window removeTitlebarAccessoryViewControllerAtIndex:index];
+        }
         NSTitlebarAccessoryViewController *viewController = [self lionFullScreenTabBarViewController];
-        if ([self shouldShowPermanentFullScreenTabBar]) {
-            [viewController setFullScreenMinHeight:_contentView.tabBarControl.frame.size.height];
-        } else {
-            [viewController setFullScreenMinHeight:0];
+        const CGFloat tabBarHeight = self.shouldShowPermanentFullScreenTabBar ? self.desiredTabBarHeight : 0;
+        viewController.fullScreenMinHeight = tabBarHeight;
+
+        if (index != NSNotFound) {
+            frame.size.height = tabBarHeight;
+            DLog(@"Set frame of tabbar as accessory to %@", NSStringFromRect(frame));
+            viewController.view.frame = frame;
         }
-        if (index == NSNotFound) {
-            [self.window addTitlebarAccessoryViewController:viewController];
-        }
+        DLog(@"Adding title bar accessory view for %@", self);
+        [self.window addTitlebarAccessoryViewController:viewController];
     } else if (_contentView.tabBarControlOnLoan) {
         assert(index != NSNotFound);
         [self.window removeTitlebarAccessoryViewControllerAtIndex:index];
@@ -8429,6 +8441,7 @@ static CGFloat iTermDimmingAmount(PSMTabBarControl *tabView) {
     if (self.windowType != _windowType) {
         [self updateWindowType];
     }
+    self.window.styleMask = self.styleMask;
     [self updateTabBarStyle];
     [self updateProxyIcon];
 
@@ -8593,12 +8606,16 @@ static CGFloat iTermDimmingAmount(PSMTabBarControl *tabView) {
     return YES;
 }
 
-- (CGFloat)rootTerminalViewHeightOfTabBar:(iTermRootTerminalView *)sender {
+- (CGFloat)desiredTabBarHeight {
     if ([self shouldHaveTallTabBar]) {
         return [iTermAdvancedSettingsModel compactMinimalTabBarHeight];
     } else {
         return [iTermAdvancedSettingsModel defaultTabBarHeight];
     }
+}
+
+- (CGFloat)rootTerminalViewHeightOfTabBar:(iTermRootTerminalView *)sender {
+    return [self desiredTabBarHeight];
 }
 
 - (CGFloat)rootTerminalViewStoplightButtonsOffset:(iTermRootTerminalView *)sender {
@@ -8753,6 +8770,7 @@ static CGFloat iTermDimmingAmount(PSMTabBarControl *tabView) {
                                                           effectiveAppearance:self.window.effectiveAppearance];
     [_contentView.tabBarControl setStyle:style];
     [_contentView.tabBarControl setTabsHaveCloseButtons:[iTermPreferences boolForKey:kPreferenceKeyTabsHaveCloseButton]];
+    _contentView.tabBarControl.height = [self desiredTabBarHeight];
 
     [self updateTabColors];
     [self updateToolbeltAppearance];

commit c486f1a8e36958334741967c5d2e53f9185d815a

Fix bug introduced by commit 2067c5e19b3b2ab6aabc8a0d71d7aad3ad73f7df that caused the tab bar not to be visible when revealing the menu bar in native full screen, which was noticed in issue 8603 (1/8 comment by nicm). A gray bar does appear when switching from regular to minimal theme, but that's a better bug to live with.

sources/PseudoTerminal.m
@@ -5314,21 +5314,19 @@ ITERM_WEAKLY_REFERENCEABLE
     const NSInteger index = [self.window.it_titlebarAccessoryViewControllers indexOfObject:_lionFullScreenTabBarViewController];
     if (fullScreen && [self shouldMoveTabBarToTitlebarAccessoryInLionFullScreen]) {
         NSRect frame = _lionFullScreenTabBarViewController.view.superview.bounds;
-        if (index != NSNotFound) {
-            DLog(@"Removing title bar accessory view for tabbar. Will re-add it shortly");
-            [self.window removeTitlebarAccessoryViewControllerAtIndex:index];
-        }
         NSTitlebarAccessoryViewController *viewController = [self lionFullScreenTabBarViewController];
         const CGFloat tabBarHeight = self.shouldShowPermanentFullScreenTabBar ? self.desiredTabBarHeight : 0;
         viewController.fullScreenMinHeight = tabBarHeight;
 
         if (index != NSNotFound) {
-            frame.size.height = tabBarHeight;
+            frame.size.height = self.desiredTabBarHeight;
             DLog(@"Set frame of tabbar as accessory to %@", NSStringFromRect(frame));
             viewController.view.frame = frame;
         }
         DLog(@"Adding title bar accessory view for %@", self);
-        [self.window addTitlebarAccessoryViewController:viewController];
+        if (index == NSNotFound) {
+            [self.window addTitlebarAccessoryViewController:viewController];
+        }
     } else if (_contentView.tabBarControlOnLoan) {
         assert(index != NSNotFound);
         [self.window removeTitlebarAccessoryViewControllerAtIndex:index];