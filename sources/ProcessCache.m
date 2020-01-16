9709b8efe5ae3a5e8d4aa75ba0dbdf102edc36da
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2020-01-16 12:56:51 +0100

    Fix autorelease pool abuse that was causing a segfault somehow

--- sources/ProcessCache.m
+++ sources/ProcessCache.m
@@ -313,12 +313,12 @@ NSString *PID_INFO_NAME = @"name";
 - (void)_run
 {
     while (1) {
-        NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
         [self _update];
 
         // Rate limit this while loop to avoid eating too much CPU when we're very busy.
         [NSThread sleepForTimeInterval:0.1];
 
+        NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
         // As long as there's no output, don't update the process cache. Otherwise the CPU usage
         // appears to spike periodically.
         while (![self testAndClearNewOutput]) {
