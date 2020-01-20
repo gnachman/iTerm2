e820d3586e470ad20e111304307e78f09f460a99
George Nachman <gnachman@gmail.com>
2020-01-19 17:16:46 -0800

    Add notarization to adhoc

--- tools/adhoc.sh
+++ tools/adhoc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo Enter the notarization password
+read -s NOTPASS
 COMPACTDATE=$(date +"%Y%m%d_%H%M%S")
 VERSION=$(cat version.txt | sed -e "s/%(extra)s/$COMPACTDATE/")
 NAME=$(echo $VERSION | sed -e "s/\\./_/g")-adhoc
@@ -7,6 +9,23 @@ make release
 rm -rf build/Deployment/iTerm.app
 mv build/Deployment/iTerm2.app build/Deployment/iTerm.app
 pushd build/Deployment
+
+# - notarize -
+PRENOTARIZED_ZIP=iTerm2-${NAME}-prenotarized.zip
+zip -ry $PRENOTARIZED_ZIP iTerm.app
+xcrun altool --notarize-app --primary-bundle-id "com.googlecode.iterm2" --username "apple@georgester.com" --password "$NOTPASS" --file $PRENOTARIZED_ZIP > /tmp/upload.out 2>&1 || die "Notarization failed"
+UUID=$(grep RequestUUID /tmp/upload.out | sed -e 's/RequestUUID = //')
+echo "uuid is $UUID"
+xcrun altool --notarization-info $UUID -u "apple@georgester.com" -p "$NOTPASS"
+sleep 1
+while xcrun altool --notarization-info $UUID -u "apple@georgester.com" -p "$NOTPASS" 2>&1 | egrep -i "in progress|Could not find the RequestUUID":
+do
+    echo "Trying again"
+    sleep 1
+done
+xcrun stapler staple iTerm.app
+# - end notarize - 
+
 zip -ry iTerm2-${NAME}.zip iTerm.app
 chmod a+r iTerm2-${NAME}.zip
 scp iTerm2-${NAME}.zip gnachman@iterm2.com:iterm2.com/adhocbuilds/ || \
