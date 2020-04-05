a5e2aadf389ecd77db4c5f116b8bb459bdcd9531
George Nachman <gnachman@gmail.com>
2020-04-05 13:50:27 -0700

    Add an Applescript call to request a cookie.

---
+++ sources/iTermRequestCookieCommand.h
@@ -0,0 +1,16 @@
+//
+//  iTermRequestCookieCommand.h
+//  iTerm2SharedARC
+//
+//  Created by George Nachman on 4/5/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface iTermRequestCookieCommand : NSScriptCommand
+
+@end
+
+NS_ASSUME_NONNULL_END
