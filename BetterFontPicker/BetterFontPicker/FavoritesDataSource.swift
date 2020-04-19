40b888f1129124a41ee0d2c4b86e1204efc8e72a
George Nachman <gnachman@gmail.com>
2020-04-18 17:15:25 -0700

    Don't forget favorites when toggling a star while searching. Issue 8835

--- BetterFontPicker/BetterFontPicker/FavoritesDataSource.swift
+++ BetterFontPicker/BetterFontPicker/FavoritesDataSource.swift
@@ -21,67 +21,77 @@ class FavoritesDataSource: NSObject, FontListDataSource {
     private let userDefaultsKey = "NoSyncBFPFavorites"
     weak var delegate: FavoritesDataSourceDelegate?
     var names: [String] {
-        return favoriteNames
-    }
-    override init() {
-        super.init()
+        return filteredFavoriteNames
     }
     var isSeparator: Bool {
         return false
     }
-    lazy var favoriteNames: Array<String> = {
-        return loadNames()
-    }()
-    private var internalFilter = ""
-    var filter: String {
-        get {
-            return internalFilter
-        }
-        set {
-            internalFilter = newValue
-            favoriteNames = loadNames()
+    private var filteredFavoriteNames: Array<String> {
+        let queryTokens = filter.normalizedTokens
+        return persistentFavoriteNames.filter {
+            return $0.matchesTableViewSearchQueryTokens(queryTokens) && fontFamilyExists($0)
         }
     }
+    private var cachedNames: [String]? = nil
+    private var persistentFavoriteNames: [String] {
+        set {
+            UserDefaults.standard.set(newValue, forKey: userDefaultsKey)
+            cachedNames = newValue
+        }
+        get {
+            if let cachedNames = cachedNames {
+                return cachedNames
+            }
+            let names = UserDefaults.standard.object(forKey: userDefaultsKey) as? [String] ?? []
+            cachedNames = names
+            return names
+        }
+    }
+    private var unfilteredFavoriteNames: [String] {
+        get {
+            return persistentFavoriteNames
+        }
+        set {
+            persistentFavoriteNames = newValue
+        }
+    }
+    var filter: String = ""
 
     func makeFavorite(_ name: String) {
-        if !favoriteNames.contains(name) {
-            let index = favoriteNames.count
-            favoriteNames.append(name)
-            UserDefaults.standard.set(favoriteNames, forKey: userDefaultsKey)
-            delegate?.favoritesDataSource(self, didInsertRowAtIndex: index)
+        guard !unfilteredFavoriteNames.contains(name) else {
+            return
         }
+        persistentFavoriteNames = persistentFavoriteNames + [name]
+        guard let index = filteredFavoriteNames.firstIndex(of: name) else {
+            return
+        }
+        delegate?.favoritesDataSource(self, didInsertRowAtIndex: index)
     }
 
     func removeFavorite(_ name: String) {
-        if let index = favoriteNames.firstIndex(of: name) {
-            favoriteNames.remove(at: index)
-            UserDefaults.standard.set(favoriteNames, forKey: userDefaultsKey)
-            delegate?.favoritesDataSource(self, didDeleteRowAtIndex: index, name: name)
+        let maybeTableIndex = filteredFavoriteNames.firstIndex(of: name)
+        guard let index = unfilteredFavoriteNames.firstIndex(of: name) else {
+            return
         }
+        var temp = unfilteredFavoriteNames
+        temp.remove(at: index)
+        unfilteredFavoriteNames = temp
+        guard let tableIndex = maybeTableIndex else {
+            return
+        }
+        delegate?.favoritesDataSource(self, didDeleteRowAtIndex: tableIndex, name: name)
     }
 
     func toggleFavorite(_ name: String) {
-        if favoriteNames.contains(name) {
+        if unfilteredFavoriteNames.contains(name) {
             removeFavorite(name)
         } else {
             makeFavorite(name)
         }
     }
 
-    private func loadNames() -> [String] {
-        let userDefault = UserDefaults.standard.object(forKey: userDefaultsKey)
-        if let array = userDefault as? [String] {
-            let queryTokens = filter.normalizedTokens
-            return array.filter({ (name) -> Bool in
-                return name.matchesTableViewSearchQueryTokens(queryTokens) && fontFamilyExists(name)
-            })
-        } else {
-            return []
-        }
-    }
-
     func reload() {
-        favoriteNames = loadNames()
+        cachedNames = nil
     }
 
     private func fontFamilyExists(_ name: String) -> Bool {
