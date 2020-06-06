982b949410a4f984346f51078c3692741285334e
George Nachman <gnachman@gmail.com>
2020-06-05 23:00:10 -0700

    Revert "Deal with spacelessApplicationSupport that's empty or contains spaces. Issue 8792"
    This reverts commit bf0bb5944907f5269fa267c0d6244ec7d7ffc06b.
    
    See issue 8919. People want to be able not to have this symlink at all.

--- sources/NSFileManager+iTerm.m
+++ sources/NSFileManager+iTerm.m
@@ -28,7 +28,6 @@
 #import "DebugLogging.h"
 #import "iTermAdvancedSettingsModel.h"
 #import "iTermAutoMasterParser.h"
-#import "RegexKitLite.h"
 #include <sys/param.h>
 #include <sys/mount.h>
 
@@ -150,11 +149,6 @@ NSString * const DirectoryLocationDomain = @"DirectoryLocationDomain";
                                        appendPathComponent:nil
                                                      error:&error];
     NSString *linkName = [iTermAdvancedSettingsModel spacelessApplicationSupport];
-    linkName = [linkName stringByReplacingOccurrencesOfRegex:@"\\s" withString:@""];
-    if (!linkName.length) {
-        linkName = @"ApplicationSupport";
-    }
-
     NSString *nospaces = [realAppSupport stringByReplacingOccurrencesOfString:@"Application Support"
                                                                    withString:linkName];
     if (linkName.length) {
