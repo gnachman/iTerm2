9f7b39c0ce1f02e68755ac24d7b97e0f84e87b41
George Nachman <gnachman@gmail.com>
2020-09-18 00:33:20 -0700

    Fix a bug where we would open two untitled windows at startup for some users. Issue 9146

--- sources/iTermApplicationDelegate.m
+++ sources/iTermApplicationDelegate.m
@@ -1385,13 +1385,16 @@ static BOOL hasBecomeActive = NO;
     } else if (!ranAutoLaunchScripts &&
                [iTermAdvancedSettingsModel openNewWindowAtStartup] &&
                ![iTermPreferences boolForKey:kPreferenceKeyOpenNoWindowsAtStartup] &&
-               ![PseudoTerminalRestorer willOpenWindows] &&
                [[[iTermController sharedInstance] terminals] count] == 0 &&
                ![self isAppleScriptTestApp] &&
                [[[iTermHotKeyController sharedInstance] profileHotKeys] count] == 0 &&
                [[[iTermBuriedSessions sharedInstance] buriedSessions] count] == 0 &&
                ![[NSApplication sharedApplication] isRunningUnitTests]) {
-        [self newWindow:nil];
+        // Over time logic has shifted into -applicationOpenUntitledFile:, and for most users I
+        // beleive this is a no-op. However, it is complex enough that there might be a baby in this
+        // bathwater so I'm disinclined to remove it until I understand when it would be used. For
+        // now I will leave it in to avoid adding risk to 3.4.0.
+        [_untitledWindowStateMachine maybeOpenUntitledFile];
     }
     [_untitledWindowStateMachine didFinishInitialization];
 
