68a68087ad9cbc03588b6caa37a35414e5b27d1a
George Nachman <gnachman@gmail.com>
2020-10-04 12:08:49 -0700

    Drop max frames in flight to 1

--- sources/Metal/Infrastructure/iTermMetalRenderer.m
+++ sources/Metal/Infrastructure/iTermMetalRenderer.m
@@ -11,7 +11,7 @@
 #import "NSImage+iTerm.h"
 #import <simd/simd.h>
 
-const NSInteger iTermMetalDriverMaximumNumberOfFramesInFlight = 3;
+const NSInteger iTermMetalDriverMaximumNumberOfFramesInFlight = 1;
 
 @implementation iTermMetalBlending
 
