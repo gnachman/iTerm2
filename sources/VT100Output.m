28b5cae3f397434fa914788993972fc3fa701f43
George Nachman <gnachman@gmail.com>
2020-10-25 11:49:18 -0700

    Only respect application keypad/application cursor mode for unmodified keypresses. See analysis in issue 9161

--- sources/VT100Output.m
+++ sources/VT100Output.m
@@ -614,58 +614,60 @@ typedef enum {
 
 #pragma mark - Private
 
+- (int)cursorModifierParamForEventModifierFlags:(NSEventModifierFlags)modflag {
+    // Normal mode
+    static int metaModifierValues[] = {
+        0,  // Nothing
+        2,  // Shift
+        5,  // Control
+        6,  // Control Shift
+        9,  // Meta
+        10, // Meta Shift
+        13, // Meta Control
+        14  // Meta Control Shift
+    };
+    static int altModifierValues[] = {
+        0,  // Nothing
+        2,  // Shift
+        5,  // Control
+        6,  // Control Shift
+        3,  // Alt
+        4,  // Alt Shift
+        7,  // Alt Control
+        8   // Alt Control Shift
+    };
+
+
+    int theIndex = 0;
+    if (modflag & NSEventModifierFlagOption) {
+        theIndex |= 4;
+    }
+    if (modflag & NSEventModifierFlagControl) {
+        theIndex |= 2;
+    }
+    if (modflag & NSEventModifierFlagShift) {
+        theIndex |= 1;
+    }
+    int *modValues = _optionIsMetaForSpecialKeys ? metaModifierValues : altModifierValues;
+    return modValues[theIndex];
+}
+
 - (NSData *)specialKey:(int)terminfo
-             cursorMod:(char*)cursorMod
-             cursorSet:(char*)cursorSet
-           cursorReset:(char*)cursorReset
+             cursorMod:(char *)cursorMod
+             cursorSet:(char *)cursorSet
+           cursorReset:(char *)cursorReset
                modflag:(unsigned int)modflag
               isCursor:(BOOL)isCursor {
     NSData* prefix = nil;
     NSData* theSuffix;
-    if (_keyStrings[terminfo] && (isCursor ? self.cursorMode : self.keypadMode)) {
+    const int mod = [self cursorModifierParamForEventModifierFlags:modflag];
+    if (_keyStrings[terminfo] && mod == 0 && (isCursor ? self.cursorMode : self.keypadMode)) {
         // Application keypad mode or application cursor mode
         theSuffix = [NSData dataWithBytes:_keyStrings[terminfo]
                                    length:strlen(_keyStrings[terminfo])];
     } else {
-        // Normal mode
-        int mod = 0;
-        static char buf[20];
-        static int metaModifierValues[] = {
-            0,  // Nothing
-            2,  // Shift
-            5,  // Control
-            6,  // Control Shift
-            9,  // Meta
-            10, // Meta Shift
-            13, // Meta Control
-            14  // Meta Control Shift
-        };
-        static int altModifierValues[] = {
-            0,  // Nothing
-            2,  // Shift
-            5,  // Control
-            6,  // Control Shift
-            3,  // Alt
-            4,  // Alt Shift
-            7,  // Alt Control
-            8   // Alt Control Shift
-        };
-
-
-        int theIndex = 0;
-        if (modflag & NSEventModifierFlagOption) {
-            theIndex |= 4;
-        }
-        if (modflag & NSEventModifierFlagControl) {
-            theIndex |= 2;
-        }
-        if (modflag & NSEventModifierFlagShift) {
-            theIndex |= 1;
-        }
-        int *modValues = _optionIsMetaForSpecialKeys ? metaModifierValues : altModifierValues;
-        mod = modValues[theIndex];
-
         if (mod) {
+            char buf[20];
             sprintf(buf, cursorMod, mod);
             theSuffix = [NSData dataWithBytes:buf length:strlen(buf)];
         } else {
