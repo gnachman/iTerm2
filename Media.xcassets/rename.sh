5c6bc096d4b4e32fc4c41e79362eec1e0eaa9880
George Nachman <gnachman@gmail.com>
2020-10-25 21:02:53 -0700

    Add script to help rename iconsets appropriately. Use in conjunction with Image2Icon

---
+++ Media.xcassets/rename.sh
@@ -0,0 +1,8 @@
+mv icon_16x16.png      $1-16.png      
+mv icon_32x32.png      $1-32.png      
+mv icon_32x32@2x.png   $1-64.png   
+mv icon_128x128.png    $1-128.png    
+mv icon_256x256.png    $1-256.png    
+mv icon_512x512.png    $1-512.png    
+mv icon_512x512@2x.png $1-1024.png 
+rm icon_*.png
