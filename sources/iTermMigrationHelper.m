d647186e307a1e5e00129f904ccd99ed84bdab55
George Nachman <gnachman@gmail.com>
2020-11-14 09:53:24 -0800

    Apply hack from preceding commit to runtime downloader and migration helper

--- sources/iTermMigrationHelper.m
+++ sources/iTermMigrationHelper.m
@@ -106,6 +106,15 @@
         accessory.textView.selectable = YES;
         accessory.requestLayout = ^{
             [alert layout];
+            if (@available(macOS 10.16, *)) {
+                // FB8897296:
+                // Prior to Big Sur, you could call [NSAlert layout] on an already-visible NSAlert
+                // to have it change its size to accommodate an accessory view controller whose
+                // frame changed.
+                //
+                // On Big Sur, it no longer works. Instead, you must call NSAlert.layout *twice*.
+                [alert layout];
+            }
         };
         alert.accessoryView = accessory;
 
