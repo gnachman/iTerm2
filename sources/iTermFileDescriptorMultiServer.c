23f4c1ad2763e84f5a03b2f0df1f58cf72e84eb8
George Nachman <gnachman@gmail.com>
2020-11-19 15:11:47 -0800

    Don't daemonize iTermServer.
    It doesn't seem to be necessary. I tested logging out and back in, fast user switching, and applescript. I was focused on not regressing on issue 9000 (activity monitor shows process not responding; that was because we connected to the window server but didn't run an event loop) and issue 8986 (applescript doesn't work, I think also because of the window server connection).
    
    This should fix issue 9276 where BBEdit wants to look at the parent process of the shell to find the cocoa app that owns it.
    
    The new behavior is similar to what the monoserver did, so I can't think of why it wouldn't work. I look forward to being surprised.

--- sources/iTermFileDescriptorMultiServer.c
+++ sources/iTermFileDescriptorMultiServer.c
@@ -985,38 +985,6 @@ static int iTermFileDescriptorMultiServerDaemonize(void) {
     return 0;
 }
 
-static void iTermFileDescriptorMultiServerConnectToWindowServer(void) {
-    // Use GetCurrentProcess() to force a connection to the window server. This gets us killed on
-    // log out. Child process become broken because their Aqua namespace session has disappeared.
-    // For example, `whoami` will print a number instead of a name. Better to die than live less
-    // than your best life.
-    //
-    // For more on these mysteries see:
-    // Technical Note TN2083 - Daemons and Agents
-    //   http://mirror.informatimago.com/next/developer.apple.com/technotes/tn2005/tn2083.html
-    //
-    // There is also an informative comment in shell_launcher.c.
-    //
-    // The approach taken in shell_launcher.c, to move the process from the Aqua per-session
-    // namespace to the per-user namespace was clever but had a lot of unintended consequences.
-    // For example, it broke PAM hacks that let you use touch ID for sudo. It broke launching
-    // Cocoa apps from the command line (sometimes! Not for me, but for the guy next to me at work).
-    // The costs of "random things don't work sometimes" is higher than the benefit of sessions
-    // surviving log out-log in.
-    //
-    // GetCurrentProcess() is deprecated, and Apple wants you to use
-    // [NSRunningApplication currentApplication], instead. I don't want to take on the overhead of
-    // the obj-c runtime, so I'll keep using this deprecated API until it actually breaks.
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-    ProcessSerialNumber psn;
-    GetCurrentProcess(&psn);
-#pragma clang diagnostic pop
-
-    // Do this to remove the dock icon.
-    TransformProcessType(&psn, kProcessTransformToUIElementApplication);
-}
-
 static void CleanUp(void) {
     FDLog(LOG_DEBUG, "Cleaning up to exit");
     if (!gPath) {
@@ -1028,14 +996,9 @@ static void CleanUp(void) {
 }
 
 static int iTermFileDescriptorMultiServerRun(char *path, int socketFd, int writeFD, int readFD) {
-    // I have turned off connectToWindowServer because it seems to be a bad idea. For one thing,
-    // Activity Monitor shows an iTerm2 process that is not responding. It also might be responsible
-    // for breaking Applescript (issue 9000, 8986).
-    const int connectToWindowServer = 0;
+    const int daemonize = 0;
 
-    if (connectToWindowServer) {
-        iTermFileDescriptorMultiServerConnectToWindowServer();
-    } else {
+    if (daemonize) {
         iTermFileDescriptorMultiServerDaemonize();
     }
 
