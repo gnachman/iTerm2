9c6e00844e33b89d119a5db3c157b8f0edf90ef9
George Nachman <gnachman@gmail.com>
2020-11-26 13:36:25 -0800

    Turn off enabling modifyOtherKeys=2 by control sequence support by default.

--- sources/iTermProfilePreferences.m
+++ sources/iTermProfilePreferences.m
@@ -421,7 +421,7 @@ NSString *const kProfilePreferenceInitialDirectoryAdvancedValue = @"Advanced";
                   KEY_OPTION_KEY_SENDS: @(OPT_NORMAL),
                   KEY_RIGHT_OPTION_KEY_SENDS: @(OPT_NORMAL),
                   KEY_APPLICATION_KEYPAD_ALLOWED: @NO,
-                  KEY_ALLOW_MODIFY_OTHER_KEYS: @YES,
+                  KEY_ALLOW_MODIFY_OTHER_KEYS: @NO,
                   KEY_USE_LIBTICKIT_PROTOCOL: @NO,
                   KEY_PLACE_PROMPT_AT_FIRST_COLUMN: @YES,
                   KEY_SHOW_MARK_INDICATORS: @YES,
