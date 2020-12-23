2943d587cd35d5ed525c3a230c75d9555dd4a94d
Benedek Kozma <cyberbeni@gmail.com>
2020-12-23 20:10:01 +0100

    cleanup autogenerated code

--- sources/iTerm2SandboxedWorkerProtocol.h
+++ sources/iTerm2SandboxedWorkerProtocol.h
@@ -26,22 +26,3 @@
 - (void)decodeImageFromData:(NSData * _Nonnull)imageData withReply:(void (^_Nonnull)(iTermImage * _Nullable))reply;
     
 @end
-
-/*
- To use the service from an application or other process, use NSXPCConnection to establish a connection to the service by doing something like this:
-
-     _connectionToService = [[NSXPCConnection alloc] initWithServiceName:@"hu.cyberbeni.iTerm2SandboxedWorker"];
-     _connectionToService.remoteObjectInterface = [NSXPCInterface interfaceWithProtocol:@protocol(iTerm2SandboxedWorkerProtocol)];
-     [_connectionToService resume];
-
-Once you have a connection to the service, you can use it like this:
-
-     [[_connectionToService remoteObjectProxy] upperCaseString:@"hello" withReply:^(NSString *aString) {
-         // We have received a response. Update our text field, but do it on the main thread.
-         NSLog(@"Result string was: %@", aString);
-     }];
-
- And, when you are finished with the service, clean up the connection like this:
-
-     [_connectionToService invalidate];
-*/
