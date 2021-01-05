gnachman/iTerm2

commit daed41c33dffbcf6520f8d4429a0f3e6e2576af7

New tmux windows created outside iterm2 (e.g. with tmux new-window) become tabs in the current window. Issue 6400

sources/TmuxWindowOpener.m
@@ -450,6 +450,15 @@ static int OctalValue(const char *bytes) {
             initialTabs = term.tabs.count;
             DLog(@"Use original window %@", term);
         }
+        if (!term && [iTermAdvancedSettingsModel anonymousTmuxWindowsOpenInCurrentWindow]) {
+            PseudoTerminal *candidate = [[iTermController sharedInstance] currentTerminal];
+            if ([[candidate uniqueTmuxControllers] count] == 0 ||
+                [[candidate uniqueTmuxControllers] containsObject:controller_]) {
+                term = candidate;
+                initialTabs = term.tabs.count;
+                DLog(@"Use current window %@", term);
+            }
+        }
         if (!term) {
             term = [[iTermController sharedInstance] openTmuxIntegrationWindowUsingProfile:self.profile];
             if (self.newWindowBlock) {

sources/iTermAdvancedSettingsModel.h
@@ -49,6 +49,7 @@ extern NSString *const iTermAdvancedSettingsDidChange;
 + (NSString *)alternateMouseScrollStringForUp;
 + (BOOL)alwaysAcceptFirstMouse;
 + (int)alwaysWarnBeforePastingOverSize;
++ (BOOL)anonymousTmuxWindowsOpenInCurrentWindow;
 + (BOOL)appendToExistingDebugLog;
 + (BOOL)autoLockSessionNameOnEdit;
 + (int)autocompleteMaxOptions;

sources/iTermAdvancedSettingsModel.m
@@ -485,6 +485,7 @@ DEFINE_BOOL(useBlackFillerColorForTmuxInFullScreen, NO, SECTION_TMUX @"Use black
 DEFINE_SETTABLE_OPTIONAL_BOOL(tmuxWindowsShouldCloseAfterDetach, TmuxWindowsShouldCloseAfterDetach, nil, SECTION_TMUX @"Close tmux windows after detaching?\nThis only takes effect when “Prefs > Profiles > Session > After a session ends” is set to “No Action”.");
 DEFINE_BOOL(disableTmuxWindowPositionRestoration, NO, SECTION_TMUX @"Disable window position restoration in tmux integration.");
 DEFINE_BOOL(disableTmuxWindowResizing, YES, SECTION_TMUX @"Don't automatically resize tmux windows");
+DEFINE_BOOL(anonymousTmuxWindowsOpenInCurrentWindow, YES, SECTION_TMUX @"Should new tmux windows not created by iTerm2 open in the current window?\nIf set to No, they will open in new windows.");
 
 #pragma mark Warnings
 

commit ef2ab6d371163ee862085b4d41c09408179d6ba0

Commit daed41c33dffbcf6520f8d4429a0f3e6e2576af7 introduced a bug because it treated initial windows the same as anonymous windows, causing them to always open in the attaching tab. Adding to the confusion is that it didn't take commit 8d92dd6e05 in to account. This commit explicitly labels windows as anonymous and uses the advanced pref added daed41c in for them while respecting the user's setting for the initial attach. Issue 9403

sources/TmuxController.m
@@ -314,7 +314,8 @@ static NSDictionary *iTermTmuxControllerDefaultFontOverridesFromProfile(Profile 
     windowOpener.manuallyOpened = _manualOpenRequested;
     windowOpener.tabColors = _tabColors;
     windowOpener.profile = profile;
-    windowOpener.initial = initial || !_pendingWindows[@(windowIndex)];
+    windowOpener.initial = initial;
+    windowOpener.anonymous = (_pendingWindows[@(windowIndex)] == nil);
     windowOpener.completion = _pendingWindows[@(windowIndex)];
     windowOpener.minimumServerVersion = self.gateway.minimumServerVersion;
     windowOpener.tabIndex = tabIndex;

sources/TmuxWindowOpener.h
@@ -42,7 +42,13 @@ extern NSString *const kTmuxWindowOpenerWindowOptionStyleValueFullScreen;
 @property (nonatomic, assign) BOOL manuallyOpened;
 @property (nonatomic, copy) NSDictionary<NSNumber *, NSString *> *tabColors;
 @property (nonatomic, copy) Profile *profile;
+
+// Are we just attaching to a tmux session initially? If false, the initial window restoration has completed.
 @property (nonatomic, assign) BOOL initial;
+
+// If true, we did not originate creation of this window. Coulda been `tmux new-window`.
+@property (nonatomic, assign) BOOL anonymous;
+
 @property (nonatomic, copy) void (^completion)(int windowIndex);
 @property (nonatomic, assign) NSDecimalNumber *minimumServerVersion;
 @property (nonatomic, readonly) NSInteger errorCount;

sources/TmuxWindowOpener.m
@@ -450,7 +450,10 @@ static int OctalValue(const char *bytes) {
             initialTabs = term.tabs.count;
             DLog(@"Use original window %@", term);
         }
-        if (!term && [iTermAdvancedSettingsModel anonymousTmuxWindowsOpenInCurrentWindow]) {
+        if (!term &&
+            !self.initial &&
+            self.anonymous &&
+            [iTermAdvancedSettingsModel anonymousTmuxWindowsOpenInCurrentWindow]) {
             PseudoTerminal *candidate = [[iTermController sharedInstance] currentTerminal];
             if ([[candidate uniqueTmuxControllers] count] == 0 ||
                 [[candidate uniqueTmuxControllers] containsObject:controller_]) {