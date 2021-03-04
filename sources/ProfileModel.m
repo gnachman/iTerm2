8f82c1af41eb1444746d47145a3bbf5ee7d7c27b
George Nachman <gnachman@gmail.com>
2021-03-04 13:04:41 -0800

    When creating the tmux profile, disable its hotkey. Issue 9527

--- sources/ProfileModel.m
+++ sources/ProfileModel.m
@@ -144,6 +144,7 @@ static NSMutableArray<NSString *> *_combinedLog;
     if (!profile) {
         Profile *defaultBookmark = [self defaultBookmark];
         NSMutableDictionary *tmuxProfile = [[defaultBookmark mutableCopy] autorelease];
+        tmuxProfile[KEY_HAS_HOTKEY] = @NO;
         [tmuxProfile setObject:@"tmux" forKey:KEY_NAME];
         [tmuxProfile setObject:[ProfileModel freshGuid] forKey:KEY_GUID];
         [tmuxProfile setObject:[NSNumber numberWithInt:1000]
