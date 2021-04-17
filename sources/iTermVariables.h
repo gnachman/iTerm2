51166728b268f5564185e2ef47108693b620df50
George Nachman <gnachman@gmail.com>
2021-04-17 15:32:55 -0700

    Add comments about tmux window/pane titles

--- sources/iTermVariables.h
+++ sources/iTermVariables.h
@@ -21,7 +21,7 @@ extern NSString *const iTermVariableKeyTabTitleOverride;
 extern NSString *const iTermVariableKeyTabTitleOverrideFormat;
 extern NSString *const iTermVariableKeyTabCurrentSession;
 extern NSString *const iTermVariableKeyTabID;
-extern NSString *const iTermVariableKeyTabTmuxWindowTitle;
+extern NSString *const iTermVariableKeyTabTmuxWindowTitle;  // #{T:set-titles-string} for a particular tmux window
 extern NSString *const iTermVariableKeyTabTmuxWindowName;
 extern NSString *const iTermVariableKeyTabWindow;
 extern NSString *const iTermVariableKeyTabTitle;
@@ -52,7 +52,7 @@ extern NSString *const iTermVariableKeySessionJob;  // job name, not affected by
 extern NSString *const iTermVariableKeySessionProcessTitle;  // process title, affected by modifying argv[0]. see issue 4214 for details.
 extern NSString *const iTermVariableKeySessionCommandLine;  // Current foreground job with arguments
 extern NSString *const iTermVariableKeySessionPresentationName;  // What's shown in the session title view
-extern NSString *const iTermVariableKeySessionTmuxPaneTitle;  // Evaluation of 'show-options -v -g set-titles-string'
+extern NSString *const iTermVariableKeySessionTmuxPaneTitle;  // #{pane_title} for a particular session
 extern NSString *const iTermVariableKeySessionTmuxRole;  // Unset (normal session), "gateway" (where you ran tmux -CC), or "client".
 extern NSString *const iTermVariableKeySessionTmuxClientName;  // Set on tmux gateways. Gives a name for the tmux session.
 extern NSString *const iTermVariableKeySessionTmuxWindowPane;  // NSNumber. Window pane number. Set if the session is a tmux session;
