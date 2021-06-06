gnachman/iTerm2

commit 5cffd254b75643cb33b8d1d3f6533522a9bd58b3

Revert "Add various exceptions to CSI u support for backward compatibility."
This reverts commit 2de95135556a38498b1c8e76c80b392c2b1049a2.

See issue 9052

sources/iTermTermkeyKeyMapper.m
@@ -418,49 +418,10 @@
 }
 
 // Only control pressed
-- (NSString *)modifiedUnicodeStringForControlCharacter:(unichar)codePoint
-                                          shiftPressed:(BOOL)shiftPressed {
+- (NSString *)modifiedUnicodeStringForControlCharacter:(unichar)codePoint {
     switch (codePoint) {
-        case 'i':
-        case 'm':
-            return nil;
-
-        case '[':
-            // Intentional deviation from the CSI u spec because of the stupid touch bar.
-            if (shiftPressed) {
-                return nil;
-            }
-            return [self stringWithCharacter:27];
-
         case ' ':
-            if (shiftPressed) {
-                return nil;
-            }
             return [self stringWithCharacter:0];
-
-        case '2':
-        case '@':  // Intentional deviation from the CSI u spec because control+number changes desktops.
-            return [self stringWithCharacter:0];
-
-        case '\\':
-            if (shiftPressed) {
-                return nil;
-            }
-            return [self stringWithCharacter:28];
-
-        case ']':
-            if (shiftPressed) {
-                return nil;
-            }
-            return [self stringWithCharacter:29];
-
-        case '^':  // Intentional deviation from the CSI u spec because control+number changes desktops.
-        case '6':
-            return [self stringWithCharacter:30];
-
-        case '-':
-        case '_':  // Intentional deviation from the CSI u spec for emacs users.
-            return [self stringWithCharacter:31];
     }
 
     if (codePoint < 'a') {
@@ -510,17 +471,17 @@ static BOOL CodePointInPrivateUseArea(unichar c) {
     }
 
     // Modified unicode - control
-    const NSEventModifierFlags allEventModifierFlagsExShift = (NSEventModifierFlagControl |
-                                                               NSEventModifierFlagOption |
-                                                               maybeFunction);
-    if ((eventModifiers & allEventModifierFlagsExShift) == NSEventModifierFlagControl) {
-        NSString *string = [self modifiedUnicodeStringForControlCharacter:codePoint shiftPressed:!!(eventModifiers & NSEventModifierFlagShift)];
+    if ((eventModifiers & allEventModifierFlags) == NSEventModifierFlagControl) {
+        NSString *string = [self modifiedUnicodeStringForControlCharacter:codePoint];
         if (string) {
             return string;
         }
     }
 
     // Modified Unicode - option
+    const NSEventModifierFlags allEventModifierFlagsExShift = (NSEventModifierFlagControl |
+                                                               NSEventModifierFlagOption |
+                                                               maybeFunction);
     if ((eventModifiers & allEventModifierFlagsExShift) == NSEventModifierFlagOption) {
         // Legacy code path: option-letter, for the "simplest form of these keys." Not sure what
         // he meant exactly, but anything that's not a function key seems simple to me. ¯\_(ツ)_/¯

iTerm2XCTests/iTermTermkeyKeyMapperTest.m
@@ -142,53 +142,7 @@ charactersIgnoringModifiers:(NSString *)charactersIgnoringModifiers
 }
 
 - (void)testCtrlOpenBracket {
-    // This is an intentional deviation because the touch bar makes pressing esc hard.
-    [self verifyCharacters:[self character:0x1b] charactersIgnoringModifiers:@"[" modifiers:0x42100 keycode:33 expected:[self character:27]];
-}
-
-- (void)testCtrlAtSign {
-    // This is an intentional deviation from the spec because control-2 changes desktops.
-    [self verifyCharacters:[self character:0] charactersIgnoringModifiers:@"@" modifiers:0x62104 keycode:19 expected:[self character:0]];
-}
-
-- (void)testControlCaret {
-    [self verifyCharacters:[self character:0x1e] charactersIgnoringModifiers:@"^" modifiers:0x62104 keycode:22 expected:[self character:0x1e]];
-}
-
-- (void)testControlHyphen {
-    [self verifyCharacters:[self character:0x1f] charactersIgnoringModifiers:@"-" modifiers:0x42100 keycode:27 expected:[self character:0x1f]];
-}
-
-- (void)testControlUnderscore {
-    [self verifyCharacters:[self character:0x1f] charactersIgnoringModifiers:@"_" modifiers:0x62102 keycode:27 expected:[self character:0x1f]];
-}
-
-- (void)testCtrl2 {
-    [self verifyCharacters:[self character:'2'] charactersIgnoringModifiers:@"2" modifiers:0x42100 keycode:19 expected:[self character:0]];
-}
-
-- (void)testControlOpenBracket {
-    [self verifyCharacters:[self character:0x1b] charactersIgnoringModifiers:@"[" modifiers:0x42100 keycode:33 expected:[self character:27]];
-}
-
-- (void)testControlShiftOpenBracket {
-    [self verifyCharacters:[self character:0x1b] charactersIgnoringModifiers:@"{" modifiers:0x62102 keycode:33 expected:[self csiUWithCodepoint:'{' modifier:5]];
-}
-
-- (void)testControlSlash {
-    [self verifyCharacters:[self character:'/'] charactersIgnoringModifiers:@"?" modifiers:NSEventModifierFlagControl | NSEventModifierFlagShift keycode:44 expected:[self character:0x7f]];
-}
-
-- (void)testControlQuestionMark {
-    [self verifyCharacters:[self character:'?'] charactersIgnoringModifiers:@"/" modifiers:0x60103 keycode:44 expected:[self csiUWithCodepoint:'/' modifier:5]];
-}
-
-- (void)testControlCloseBracket {
-    [self verifyCharacters:[self character:0x1d] charactersIgnoringModifiers:@"]" modifiers:0x42100 keycode:30 expected:[self character:0x1d]];
-}
-
-- (void)testControlShiftCloseBracket {
-    [self verifyCharacters:[self character:0x1d] charactersIgnoringModifiers:@"}" modifiers:0x62102 keycode:30 expected:[self csiUWithCodepoint:'}' modifier:5]];
+    [self verifyCharacters:[self character:0x1b] charactersIgnoringModifiers:@"[" modifiers:0x42100 keycode:33 expected:[self csiUWithCodepoint:91 modifier:5]];
 }
 
 - (void)testCtrlShiftI {

commit da53da0c18eb33cc889279c56d8979d3d4d9b2cd

Fix CSI u mode C-i and C-m, broken by 5cffd254b75643cb33b8d1d3f6533522a9bd58b3

sources/iTermTermkeyKeyMapper.m
@@ -423,6 +423,9 @@
     switch (codePoint) {
         case ' ':
             return [self stringWithCharacter:0];
+        case 'i':
+        case 'm':
+            return nil;
     }
 
     if (codePoint < 'a') {