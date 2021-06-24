17748577fca963c14c71be179c1ce50fa51e57b9
George Nachman <gnachman@gmail.com>
2021-06-23 21:42:07 -0700

    Make it possible to use Swift.

---
+++ sources/HelloWorld.swift
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.swift
+//  iTerm2SharedARC
+//
+//  Created by George Nachman on 6/23/21.
+//
+// This is a placeholder Swift file. Eventually I'll add some actual swift code and this can be
+// deleted.
+
+import Foundation
+
+@objc public class HelloWorld: NSObject {
+    @objc public func Hello() { print("Hello") }
+}
---
+++ sources/Phony.swift
@@ -0,0 +1,10 @@
+//
+//  Phony.swift
+//  iTerm2
+//
+//  Created by George Nachman on 6/23/21.
+//
+// The only purpose of this file is to trick xcode into linking the proper Swift dylibs into the
+// binary targets.
+
+import Foundation
