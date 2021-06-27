20e3320e94f2a0e468a737dc11c1cc04031f7fa9
George Nachman <gnachman@gmail.com>
2021-06-27 10:05:38 -0700

    Add iTerm2-shell-integration submodule. Update copy_shell_integration.sh to pull from it.
    I decided to keep a separate copy in the iTerm2 repo because
    it needs a good bit of processing to be useful and I want to
    keep the main repo dependency-free. Submodules already
    require manual work to update, anyway, so the number of
    steps remains the same.

--- tools/copy_shell_integration.sh
+++ tools/copy_shell_integration.sh
@@ -14,21 +14,22 @@ branch() {
 
 set -x
 
-WEBSITE=~/iterm2-website
-test -d $WEBSITE || die No $WEBSITE directory
-pushd $WEBSITE
+git submodule update --init --remote -- submodules/iTerm2-shell-integration
+SUBMODULE=$PWD/submodules/iTerm2-shell-integration
+test -d $SUBMODULE || die No $SUBMODULE directory
+pushd $SUBMODULE
 if [ $(branch) != master ]; then
   die "Not on master"
 fi
 popd
 
-cp $WEBSITE/source/shell_integration/bash Resources/shell_integration/iterm2_shell_integration.bash
-cp $WEBSITE/source/shell_integration/fish Resources/shell_integration/iterm2_shell_integration.fish
-cp $WEBSITE/source/shell_integration/tcsh Resources/shell_integration/iterm2_shell_integration.tcsh
-cp $WEBSITE/source/shell_integration/zsh  Resources/shell_integration/iterm2_shell_integration.zsh
+cp $SUBMODULE/source/shell_integration/bash Resources/shell_integration/iterm2_shell_integration.bash
+cp $SUBMODULE/source/shell_integration/fish Resources/shell_integration/iterm2_shell_integration.fish
+cp $SUBMODULE/source/shell_integration/tcsh Resources/shell_integration/iterm2_shell_integration.tcsh
+cp $SUBMODULE/source/shell_integration/zsh  Resources/shell_integration/iterm2_shell_integration.zsh
 DEST=$PWD/Resources/utilities
 
-pushd $WEBSITE/source/utilities
+pushd $SUBMODULE/source/utilities
 files=$(find . -type f)
 tar cvfz $DEST/utilities.tgz *
 echo * > $DEST/utilities-manifest.txt
