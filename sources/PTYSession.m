gnachman/iTerm2

commit 72b2f8dd29253d19d982a4e949a598d81bbc0d99

Improve metal enablement behavior because of strange behavior that surfaced in a comment in issue 6902.
Do not keep trying to render a metal frame after a session has exited. This prevents infinitely retrying to draw posthumously. This was done in both renderTwoMetalFramesAndShowMetalView and drawFrameAndRemoveTemporarilyDisablementOfMetalForToken.

Remove sessionViewHideMetalViewUntilNextFrame, which was unnecessary because the caller already gets a temporary disablement token and releases it after a spin.

When drawFrameAndRemoveTemporarilyDisablementOfMetalForToken eventually finishes its async draw, change how it shows the metal view: instead of just turning on the wrapper, turning of PTYTextView's darwing, and setting the metal view's alpha to 1, also cause it to redraw the metal view immediately.

sources/PTYSession.m
@@ -5173,7 +5173,7 @@ ITERM_WEAKLY_REFERENCEABLE
         // up a new metal driver. This frame will never be seen since PTYTextView is still visible.
         DLog(@"Begin async draw for %@", self);
         [_view.driver drawAsynchronouslyInView:_view.metalView completion:^(BOOL ok) {
-            if (!_useMetal) {
+            if (!_useMetal || _exited) {
                 return;
             }
 
@@ -5212,7 +5212,10 @@ ITERM_WEAKLY_REFERENCEABLE
              self, _textview.dataSource, @(_screen.width), @(_screen.height));
         return;
     }
+    [self reallyShowMetalViewImmediately];
+}
 
+- (void)reallyShowMetalViewImmediately {
     [_view setNeedsDisplay:YES];
     [self showMetalAndStopDrawingTextView];
     _view.metalView.enableSetNeedsDisplay = YES;
@@ -9926,16 +9929,6 @@ ITERM_WEAKLY_REFERENCEABLE
     [self.delegate sessionUpdateMetalAllowed];
 }
 
-- (void)sessionViewHideMetalViewUntilNextFrame {
-    if (@available(macOS 10.11, *)) {
-        if (!_useMetal) {
-            return;
-        }
-        id token = [self temporarilyDisableMetal];
-        [self drawFrameAndRemoveTemporarilyDisablementOfMetalForToken:token];
-    }
-}
-
 - (id)temporarilyDisableMetal NS_AVAILABLE_MAC(10_11) {
     assert(_useMetal);
     _wrapper.useMetal = NO;
@@ -9970,6 +9963,10 @@ ITERM_WEAKLY_REFERENCEABLE
             DLog(@"Token %@ is gone, not proceeding.", token);
             return;
         }
+        if (_exited) {
+            DLog(@"Returning because the session is dead");
+            return;
+        }
         if (!_useMetal) {
             DLog(@"Returning because useMetal is off");
             return;
@@ -9994,12 +9991,11 @@ ITERM_WEAKLY_REFERENCEABLE
         [_metalDisabledTokens removeObject:token];
         DLog(@"Remove temporarily disablement. Tokens are now %@", _metalDisabledTokens);
         if (_metalDisabledTokens.count == 0 && _useMetal) {
-            _wrapper.useMetal = YES;
-            _textview.suppressDrawing = YES;
-            _view.metalView.alphaValue = 1;
+            [self reallyShowMetalViewImmediately];
         }
     }];
 }
+
 
 - (void)sessionViewNeedsMetalFrameUpdate {
     if (@available(macOS 10.11, *)) {

sources/SessionView.h
@@ -123,8 +123,6 @@
 // Announcement shown, changed, or removed.
 - (void)sessionViewAnnouncementDidChange:(SessionView *)sessionView;
 
-- (void)sessionViewHideMetalViewUntilNextFrame;
-
 - (void)sessionViewUserScrollDidChange:(BOOL)userScroll;
 
 - (void)sessionViewDidChangeHoverURLVisible:(BOOL)visible;

sources/SessionView.m
@@ -520,7 +520,6 @@ static NSDate* lastResizeDate_;
 }
 
 - (void)reallyUpdateMetalViewFrame {
-    [_delegate sessionViewHideMetalViewUntilNextFrame];
     NSRect frame = _scrollview.contentView.frame;
     if (self.showBottomStatusBar) {
         frame.origin.y += iTermStatusBarHeight;

commit 381d3c5f8018264a96d6b9015f8b0033eeaf120f

Allow metal to be enabled on terminated sessions. Fixes a bug where scrolling does not update the view. The bug was introduced by 72b2f8dd29253d19d982a4e949a598d81bbc0d99 in an attempt to reduce unecessary drawing of a terminated session. I'm not able to reproduce that now and it was obviously wrong. This commit changes it to test for a missing window instead, which is almost certainly never going to happen. Issue 9836.

sources/PTYSession.m
@@ -13316,8 +13316,8 @@ preferredEscaping:(iTermSendTextEscaping)preferredEscaping {
             DLog(@"drawFrameAndRemoveTemporarilyDisablementOfMetal: Token %@ is gone, not proceeding.", token);
             return;
         }
-        if (_exited) {
-            DLog(@"drawFrameAndRemoveTemporarilyDisablementOfMetal: Returning because the session is dead");
+        if (!_view.window) {
+            DLog(@"drawFrameAndRemoveTemporarilyDisablementOfMetal: Returning because the view has no window");
             return;
         }
         if (!_useMetal) {