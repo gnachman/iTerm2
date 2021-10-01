9c6b8d4e9972769f8aac11866a1e83ce2feeaa7d
George Nachman <gnachman@gmail.com>
2021-10-01 15:42:13 -0700

    Rebuild terminfo with old tic

--- tools/build_terminfo.sh
+++ tools/build_terminfo.sh
@@ -7,18 +7,7 @@
 rm -rf Resources/terminfo
 mkdir Resources/terminfo
 
-currentver="$(tic -V | cut -d' ' -f2)"
-minver="6.1"
-if [ "$(printf '%s\n' "$minver" "$currentver" | sort -V | tail -n1)" = "$minver" ]; then
-    # Older versions of tic suffer from integer overflow for values at least
-    # 2^16 which is a problem for xterm-256color's pairs#65536.
-    echo "tic 6.1 or later is needed. You have " $(tic -V) at $(which tic) with PATH $PATH
-    echo "You probably want to do this:"
-    echo 'set path=(/usr/local/Cellar/ncurses/6.2/bin $path)'
-    exit 1
-fi
-
-tic -x -o Resources/terminfo Resources/xterm-terminfo
+/usr/bin/tic -x -o Resources/terminfo Resources/xterm-terminfo
 export TERMINFO_DIRS=$(pwd)/Resources/terminfo
 infocmp xterm
 infocmp xterm-new
