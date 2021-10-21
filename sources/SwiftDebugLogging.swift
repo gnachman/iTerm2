05eb149a9637ea622585f9f2082d652d6a976025
George Nachman <gnachman@gmail.com>
2021-10-20 17:37:00 -0700

    Improve performance of DLog in swift. Issue 9941

--- sources/SwiftDebugLogging.swift
+++ sources/SwiftDebugLogging.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
-func DLog(_ message: String, file: String = #file, line: Int = #line, function: String = #function) {
+func DLog(_ messageBlock: @autoclosure () -> String, file: String = #file, line: Int = #line, function: String = #function) {
     guard gDebugLogging.boolValue else {
         return
     }
+    let message = messageBlock()
     // print("\(file):\(line) \(function): \(message)")
-
     DebugLogImpl(file.cString(using: .utf8), Int32(line), function.cString(using: .utf8), message)
 }
 
