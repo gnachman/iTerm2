7926adff32aa3a8b774636e264e7653c182294ac
George Nachman <gnachman@gmail.com>
2022-01-02 11:33:55 -0800

    Make iTermExpectation possible to use in a world with a mutation thread. The mutation thread will use a copy and reflect changes back to the main thread.

--- sources/iTermExpect.h
+++ sources/iTermExpect.h
@@ -7,6 +7,25 @@
 
 #import <Foundation/Foundation.h>
 
+// Usage;
+//
+// /* Main thread */
+// iTermExpect *e = [[iTermExpect alloc] init];
+// iTermExpectation *x1 = [e expectRegularExpression:@"…" after:nil deadline:nil willExpect:…  completion:…];
+// iTermExpectation *x2 = [e expectRegularExpression:@"…" after:x1 deadline:nil willExpect:… completion:…];
+//
+// /* Time to sync with mutation thread. This happens in a mutex so neither `e` nor a pre-existing
+//    `_mutableState.expect` gets modified during -copy. */
+// _mutableState.expect = [e copy];
+//
+// /* Mutation thread */
+// [_mutableState.expect.expectations.firstObject didMatchWithCaptureGroups:…];
+//
+// /* This is allowed concurrently on the main thread */
+// [e cancelExpectation:x1];
+// iTermExpectation *x3 = [e expectRegularExpression:@"…" after:nil deadline:nil willExpect:… completion:…];
+
+
 NS_ASSUME_NONNULL_BEGIN
 
 @interface iTermExpectation: NSObject
@@ -14,24 +33,24 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, readonly) BOOL hasCompleted;
 @property (nullable, nonatomic, strong, readonly) iTermExpectation *successor;
 @property (nonatomic, readonly) iTermExpectation *lastExpectation;  // self or successor
+@property (nullable, nonatomic, readonly) NSDate *deadline;
 
+// This is to be called on the mutation thread.
 - (void)didMatchWithCaptureGroups:(NSArray<NSString *> *)captureGroups;
 
 @end
 
-@interface iTermExpect : NSObject
+@interface iTermExpect : NSObject<NSCopying>
 @property (nonatomic, readonly) NSArray<iTermExpectation *> *expectations;
 
 - (iTermExpectation *)expectRegularExpression:(NSString *)regex
                                         after:(nullable iTermExpectation *)precedecessor
+                                     deadline:(nullable NSDate *)deadline
                                    willExpect:(void (^ _Nullable)(void))willExpect
                                    completion:(void (^ _Nullable)(NSArray<NSString *> *captureGroups))completion;
 
-- (iTermExpectation *)expectRegularExpression:(NSString *)regex
-                                   completion:(void (^)(NSArray<NSString *> *captureGroups))completion;
-
+// This may only be called on the main thread. The mutation thread cannot cancel expectations without creating races.
 - (void)cancelExpectation:(iTermExpectation *)expectation;
-- (void)setTimeout:(NSTimeInterval)timeout forExpectation:(iTermExpectation *)expectation;
 
 @end
 
