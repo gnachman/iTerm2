508adbf822667b195e66e438148fb8be2f593b86
George Nachman <gnachman@gmail.com>
2022-01-16 09:46:24 -0800

    Remove mutAddNoteWithText:inAbsoluteRnage:

--- sources/VT100Screen+Mutation.h
+++ sources/VT100Screen+Mutation.h
@@ -121,7 +121,6 @@ basedAtAbsoluteLineNumber:(long long)absoluteLineNumber
                          colors:(NSDictionary *)colors;
 - (void)mutAddTokens:(CVector)vector length:(int)length highPriority:(BOOL)highPriority;
 - (void)mutScheduleTokenExecution;
-- (PTYAnnotation *)mutAddNoteWithText:(NSString *)text inAbsoluteRange:(VT100GridAbsCoordRange)absRange;
 - (void)mutInjectData:(NSData *)data;
 - (void)mutPerformPeriodicTriggerCheck;
 - (void)mutForceCheckTriggers;
