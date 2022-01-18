5dd51aa559840047e105c970f8f6bfb42154404d
George Nachman <gnachman@gmail.com>
2022-01-18 11:44:30 -0800

    Move periodic trigger checks (partial line) into sync

--- sources/VT100Screen+Mutation.h
+++ sources/VT100Screen+Mutation.h
@@ -23,8 +23,6 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic, readonly) LineBuffer *mutableLineBuffer;
 
 - (void)mutResetDirty;
-- (void)mutPerformPeriodicTriggerCheck;
-- (void)mutForceCheckTriggers;
 
 @end
 
