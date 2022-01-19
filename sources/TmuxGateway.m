d58911f740e6799c925bb4f4436e054d92fc1a6d
Lonny Wong <lonnywong@qq.com>
2022-01-19 19:46:21 +0800

    Improve input speed for tmux integration

--- sources/TmuxGateway.m
+++ sources/TmuxGateway.m
@@ -856,13 +856,48 @@ error:
     }
 
     // Send multiple small send-keys commands because commands longer than 1024 bytes crash tmux 1.8.
+    const NSUInteger maxLiteralCharacters = 1000;  // 1024 - len('send -lt %123456789 ') = 1004
+    const NSUInteger maxHexCharacters = maxLiteralCharacters / 8;  // len(' C-Space') = 8
+
+    BOOL asLiteralCharacters = NO;
     NSMutableArray *commands = [NSMutableArray array];
-    const NSUInteger stride = 80;
-    for (NSUInteger start = 0; start < codePoints.count; start += stride) {
-        NSUInteger length = MIN(stride, codePoints.count - start);
-        NSRange range = NSMakeRange(start, length);
-        NSArray *subarray = [codePoints subarrayWithRange:range];
-        [commands addObject:[self dictionaryForSendKeysCommandWithCodePoints:subarray windowPane:windowPane]];
+    NSMutableArray *subarray = [NSMutableArray arrayWithCapacity:MIN(codePoints.count, maxLiteralCharacters)];
+
+    for (NSNumber *number in codePoints) {
+        BOOL currentAsLiteralCharacter = [self canSendAsLiteralCharacter:number];
+        if (asLiteralCharacters != currentAsLiteralCharacter) {
+            if (subarray.count > 0) {
+                [commands addObject:[self dictionaryForSendKeysCommandWithCodePoints:subarray
+                                                                          windowPane:windowPane
+                                                                 asLiteralCharacters:asLiteralCharacters]];
+                [subarray removeAllObjects];
+            }
+            [subarray addObject:number];
+            asLiteralCharacters = currentAsLiteralCharacter;
+            continue;
+        }
+
+        [subarray addObject:number];
+        if (!asLiteralCharacters && subarray.count >= maxHexCharacters) {
+            [commands addObject:[self dictionaryForSendKeysCommandWithCodePoints:subarray
+                                                                      windowPane:windowPane
+                                                             asLiteralCharacters:asLiteralCharacters]];
+            [subarray removeAllObjects];
+            continue;
+        }
+        if (asLiteralCharacters && subarray.count >= maxLiteralCharacters) {
+            [commands addObject:[self dictionaryForSendKeysCommandWithCodePoints:subarray
+                                                                      windowPane:windowPane
+                                                             asLiteralCharacters:asLiteralCharacters]];
+            [subarray removeAllObjects];
+            continue;
+        }
+    }
+
+    if (subarray.count > 0) {
+        [commands addObject:[self dictionaryForSendKeysCommandWithCodePoints:subarray
+                                                                  windowPane:windowPane
+                                                         asLiteralCharacters:asLiteralCharacters]];
     }
 
     [delegate_ tmuxSetSecureLogging:YES];
@@ -884,16 +919,38 @@ error:
     return !([self.minimumServerVersion isEqual:version2_2] && [self.maximumServerVersion isEqual:version2_2]);
 }
 
+- (BOOL) canSendAsLiteralCharacter:(NSNumber *)codePoint {
+    int number = [codePoint intValue];
+    if (number < 0x21 || number >= 0x7f) {
+        return NO;
+    }
+    for (const char *p = "\"#';\\{}"; *p != '\0'; p++) {
+        if (number == *p) {
+            return NO;
+        }
+    }
+    return YES;
+}
+
+- (NSString *)numbersAsLiteralCharacters:(NSArray<NSNumber *> *)codePoints {
+    NSMutableString *result = [NSMutableString stringWithCapacity:codePoints.count];
+    for (NSNumber *number in codePoints) {
+        [result appendFormat:@"%c", number.intValue];
+    }
+    return result;
+}
+
 - (NSDictionary *)dictionaryForSendKeysCommandWithCodePoints:(NSArray<NSNumber *> *)codePoints
-                                                  windowPane:(int)windowPane {
+                                                  windowPane:(int)windowPane
+                                         asLiteralCharacters:(BOOL)asLiteralCharacters {
     NSString *value;
-    if ([codePoints isEqual:@[ @0 ]]) {
-        value = @"C-Space";
+    if (asLiteralCharacters) {
+        value = [self numbersAsLiteralCharacters:codePoints];
     } else {
         value = [codePoints numbersAsHexStrings];
     }
-    NSString *command = [NSString stringWithFormat:@"send-keys -t \"%%%d\" %@",
-                         windowPane, value];
+    NSString *command = [NSString stringWithFormat:@"send %@ %%%d %@",
+                         asLiteralCharacters ? @"-lt" : @"-t", windowPane, value];
     NSDictionary *dict = [self dictionaryForCommand:command
                                      responseTarget:self
                                    responseSelector:@selector(noopResponseSelector:)
