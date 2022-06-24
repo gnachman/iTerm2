4e935d2c3e4f198e7b94e9c5dd08f3d20b8b2454
George Nachman <gnachman@gmail.com>
2022-06-24 16:06:37 -0700

    Add -q to show-options to fix problem in attaching to tmux 3.3. See https://github.com/tmux/tmux/issues/3199

--- sources/TmuxWindowOpener.m
+++ sources/TmuxWindowOpener.m
@@ -237,7 +237,7 @@ NSString *const kTmuxWindowOpenerWindowOptionStyleValueFullScreen = @"FullScreen
 - (NSDictionary *)dictForGetUserVars:(NSNumber *)wp {
     ++pendingRequests_;
     DLog(@"Increment pending requests to %d", pendingRequests_);
-    NSString *command = [NSString stringWithFormat:@"show-options -v -p -t %%%d @uservars",
+    NSString *command = [NSString stringWithFormat:@"show-options -q -v -p -t %%%d @uservars",
                          [wp intValue]];
     return [gateway_ dictionaryForCommand:command
                            responseTarget:self
@@ -382,12 +382,7 @@ static int OctalValue(const char *bytes) {
 }
 
 - (void)getUserVarsResponse:(NSString *)response pane:(NSNumber *)wp {
-    if (!response) {
-        [self didReceiveError];
-        return;
-    }
-
-    if (wp) {
+    if (response && wp) {
         [self.controller setEncodedUserVars:response forPane:wp.intValue];
     }
     [self requestDidComplete];
