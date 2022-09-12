9b0e502d982d4529b7df6e4ed9433535341275fb
George Nachman <gnachman@gmail.com>
2022-09-11 23:26:08 -0700

    Fix some accidentally quadratic behavior in graph encoding. Issue 10572

--- sources/iTermEncoderGraphRecord.h
+++ sources/iTermEncoderGraphRecord.h
@@ -33,6 +33,7 @@ NS_ASSUME_NONNULL_BEGIN
 // You probably want to use arrayWithKey or dictionaryWithKey. This is very low level.
 - (iTermEncoderGraphRecord * _Nullable)childRecordWithKey:(NSString *)key
                                                identifier:(NSString *)identifier;
+- (void)ensureIndexOfGraphRecords;
 
 - (void)enumerateArrayWithKey:(NSString *)key
                         block:(void (^NS_NOESCAPE)(NSString *identifier,
