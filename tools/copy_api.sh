4374d90a076f7e08fc3285fd3e3ec2a89c3c29aa
George Nachman <gnachman@gmail.com>
2022-09-27 11:39:31 -0700

    Add homebrew path to copy_api

--- tools/copy_api.sh
+++ tools/copy_api.sh
@@ -1,6 +1,5 @@
 #!/bin/tcsh
-foreach x (~/Library/ApplicationSupport/iTerm2/iterm2env*/versions/*/lib/python*/site-packages/ /usr/local/lib/python*/site-packages/ ~/Library/Python/*/lib/python/site-packages/ /Library/Python/3.7/site-packages/)
+foreach x (~/Library/ApplicationSupport/iTerm2/iterm2env*/versions/*/lib/python*/site-packages/ /usr/local/lib/python*/site-packages/ ~/Library/Python/*/lib/python/site-packages/ /Library/Python/3.7/site-packages/ /opt/homebrew/lib/python3.9/site-packages/)
     test -d $x && echo to: $x
     test -d $x && cp -f api/library/python/iterm2/iterm2/* $x/iterm2
-    test -d $x && cp -f api/library/python/iterm2-core/iterm2-core/* $x/iterm2-core
 end
