gnachman/iTerm2

commit c31b70021eb78a1b48920b3c747011c9c742d4b6

Make the sandboxed worker not depend on swift concurrency which breaks deployment to older macos versions. ARCify DebugLogging.m

iTerm2.xcodeproj/project.pbxproj
@@ -934,8 +934,6 @@
 		5ECE005F1454E59B004861E9 /* PseudoTerminalRestorer.h in Headers */ = {isa = PBXBuildFile; fileRef = 5ECE005D1454E59B004861E9 /* PseudoTerminalRestorer.h */; };
 		756C32AD2597AC2E0047B3A9 /* iTermImage+Sixel.m in Sources */ = {isa = PBXBuildFile; fileRef = 756C32AC2597AC2E0047B3A9 /* iTermImage+Sixel.m */; };
 		756C32B72597AC8C0047B3A9 /* libsixel.a in Frameworks */ = {isa = PBXBuildFile; fileRef = A67D194E2237045C00BD0D4D /* libsixel.a */; };
-		7577E2752593B73000905402 /* libiTerm2SharedARC.a in Frameworks */ = {isa = PBXBuildFile; fileRef = A667195C1DCE36C3000CE608 /* libiTerm2SharedARC.a */; };
-		7577E2812593B78C00905402 /* libiTerm2Shared.a in Frameworks */ = {isa = PBXBuildFile; fileRef = A6C760501B45C4CF00E3C992 /* libiTerm2Shared.a */; };
 		7577E2A12593B88700905402 /* iTermSandboxedWorkerClient.h in Headers */ = {isa = PBXBuildFile; fileRef = 7577E29F2593B88700905402 /* iTermSandboxedWorkerClient.h */; };
 		7577E2A22593B88700905402 /* iTermSandboxedWorkerClient.m in Sources */ = {isa = PBXBuildFile; fileRef = 7577E2A02593B88700905402 /* iTermSandboxedWorkerClient.m */; };
 		7581C4E120A38E0F00699F99 /* iTermHyperlinkTrigger.h in Headers */ = {isa = PBXBuildFile; fileRef = 7581C4DD20A38DF900699F99 /* iTermHyperlinkTrigger.h */; };
@@ -974,6 +972,11 @@
 		A6024B89254D367E0036D6CF /* iTermColorSuggester.m in Sources */ = {isa = PBXBuildFile; fileRef = A6024B87254D367E0036D6CF /* iTermColorSuggester.m */; };
 		A6024B8B254DDFEE0036D6CF /* iTermNSColorCategoryTests.m in Sources */ = {isa = PBXBuildFile; fileRef = A6024B8A254DDFEE0036D6CF /* iTermNSColorCategoryTests.m */; };
 		A60251681CCD3E5E009BABF1 /* NSURL+iTerm.h in Headers */ = {isa = PBXBuildFile; fileRef = A60251661CCD3E5E009BABF1 /* NSURL+iTerm.h */; };
+		A6038307294305EA00FE11C2 /* DebugLogging.m in Sources */ = {isa = PBXBuildFile; fileRef = 1DD39B19180B82F5004E56D5 /* DebugLogging.m */; };
+		A60383082943067000FE11C2 /* DebugLogging.m in Sources */ = {isa = PBXBuildFile; fileRef = 1DD39B19180B82F5004E56D5 /* DebugLogging.m */; };
+		A6038309294306BA00FE11C2 /* iTermSandboxedWorkerClient.m in Sources */ = {isa = PBXBuildFile; fileRef = 7577E2A02593B88700905402 /* iTermSandboxedWorkerClient.m */; };
+		A603830A294306C400FE11C2 /* iTermAdvancedSettingsModel.m in Sources */ = {isa = PBXBuildFile; fileRef = A697100A18D94CDA007E901D /* iTermAdvancedSettingsModel.m */; };
+		A603830B294306D800FE11C2 /* iTermUserDefaultsObserver.m in Sources */ = {isa = PBXBuildFile; fileRef = A6EF57D021994DDC00C76698 /* iTermUserDefaultsObserver.m */; };
 		A603E9FB283EE53C0028B648 /* SecretServer.swift in Sources */ = {isa = PBXBuildFile; fileRef = A603E9FA283EE53C0028B648 /* SecretServer.swift */; };
 		A603E9FE283EE9BD0028B648 /* ObjCExceptions.h in Headers */ = {isa = PBXBuildFile; fileRef = A603E9FC283EE9BD0028B648 /* ObjCExceptions.h */; };
 		A603E9FF283EE9BD0028B648 /* ObjCExceptions.m in Sources */ = {isa = PBXBuildFile; fileRef = A603E9FD283EE9BD0028B648 /* ObjCExceptions.m */; };
@@ -1028,7 +1031,6 @@
 		A608F22120F07658008E8009 /* iTermImageMark.m in Sources */ = {isa = PBXBuildFile; fileRef = A62C3B411BD40E7C00B5629D /* iTermImageMark.m */; };
 		A608F22220F08369008E8009 /* VT100DCSParser.m in Sources */ = {isa = PBXBuildFile; fileRef = A647E39E18C351F400450FA1 /* VT100DCSParser.m */; };
 		A6099B0518D6B0FD00081FA9 /* iTermWarning.h in Headers */ = {isa = PBXBuildFile; fileRef = A6099B0318D6B0FD00081FA9 /* iTermWarning.h */; };
-		A60AE01A289A18EB00419B14 /* FileProviderLogging.swift in Sources */ = {isa = PBXBuildFile; fileRef = A69412E928555D390086F068 /* FileProviderLogging.swift */; };
 		A60AE01B289A196900419B14 /* FileProviderLogging.swift in Sources */ = {isa = PBXBuildFile; fileRef = A69412E928555D390086F068 /* FileProviderLogging.swift */; };
 		A60BB37E1EB5149100D76C09 /* iTermCopyModeState.h in Headers */ = {isa = PBXBuildFile; fileRef = A60BB37C1EB5149100D76C09 /* iTermCopyModeState.h */; };
 		A60BB37F1EB5149100D76C09 /* iTermCopyModeState.m in Sources */ = {isa = PBXBuildFile; fileRef = A60BB37D1EB5149100D76C09 /* iTermCopyModeState.m */; };
@@ -2628,7 +2630,6 @@
 		A69F1BD928E65A4F0054204E /* uniE0C1_flame-thin.eps in Resources */ = {isa = PBXBuildFile; fileRef = A69F1B9D28E65A4E0054204E /* uniE0C1_flame-thin.eps */; };
 		A69F1BDB28E7570C0054204E /* FileProviderDebugLoggingShim.swift in Sources */ = {isa = PBXBuildFile; fileRef = A69F1BDA28E7570C0054204E /* FileProviderDebugLoggingShim.swift */; };
 		A69F1BDC28E7571A0054204E /* FileProviderDebugLoggingShim.swift in Sources */ = {isa = PBXBuildFile; fileRef = A69F1BDA28E7570C0054204E /* FileProviderDebugLoggingShim.swift */; };
-		A69F1BDD28E7571B0054204E /* FileProviderDebugLoggingShim.swift in Sources */ = {isa = PBXBuildFile; fileRef = A69F1BDA28E7570C0054204E /* FileProviderDebugLoggingShim.swift */; };
 		A6A13AA218C2D23300B241ED /* iTermColorMap.h in Headers */ = {isa = PBXBuildFile; fileRef = A6A13AA018C2D23300B241ED /* iTermColorMap.h */; };
 		A6A13AA718C2D45900B241ED /* NSColor+iTerm.h in Headers */ = {isa = PBXBuildFile; fileRef = A6A13AA518C2D45900B241ED /* NSColor+iTerm.h */; };
 		A6A13AAC18C3347200B241ED /* VT100Output.h in Headers */ = {isa = PBXBuildFile; fileRef = A6A13AAA18C3347200B241ED /* VT100Output.h */; };
@@ -2932,7 +2933,6 @@
 		A6C762C11B45C52B00E3C992 /* NSTextField+iTerm.m in Sources */ = {isa = PBXBuildFile; fileRef = A6DF401B1897607E00F05947 /* NSTextField+iTerm.m */; };
 		A6C762C31B45C52B00E3C992 /* NSView+RecursiveDescription.m in Sources */ = {isa = PBXBuildFile; fileRef = 1D81F0BF183C3C2D00910838 /* NSView+RecursiveDescription.m */; };
 		A6C762C41B45C52B00E3C992 /* NSWorkspace+iTerm.m in Sources */ = {isa = PBXBuildFile; fileRef = A67F57AF1B012BD100B4F135 /* NSWorkspace+iTerm.m */; };
-		A6C762C61B45C52B00E3C992 /* DebugLogging.m in Sources */ = {isa = PBXBuildFile; fileRef = 1DD39B19180B82F5004E56D5 /* DebugLogging.m */; };
 		A6C762C71B45C52B00E3C992 /* iTermHotKeyController.m in Sources */ = {isa = PBXBuildFile; fileRef = 1DB9D8F1183FE9EF0029F0B5 /* iTermHotKeyController.m */; };
 		A6C762CA1B45C52B00E3C992 /* iTermApplicationDelegate.m in Sources */ = {isa = PBXBuildFile; fileRef = 20D5CC6404E7AA0500000106 /* iTermApplicationDelegate.m */; };
 		A6C762CB1B45C52B00E3C992 /* iTermController.m in Sources */ = {isa = PBXBuildFile; fileRef = E8A66F030272453F03A80106 /* iTermController.m */; };
@@ -7362,8 +7362,6 @@
 			buildActionMask = 2147483647;
 			files = (
 				A60D20E62597FAFB004AC979 /* libsandbox.tbd in Frameworks */,
-				7577E2812593B78C00905402 /* libiTerm2Shared.a in Frameworks */,
-				7577E2752593B73000905402 /* libiTerm2SharedARC.a in Frameworks */,
 				756C32B72597AC8C0047B3A9 /* libsixel.a in Frameworks */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
@@ -14806,13 +14804,15 @@
 			isa = PBXSourcesBuildPhase;
 			buildActionMask = 2147483647;
 			files = (
+				A603830B294306D800FE11C2 /* iTermUserDefaultsObserver.m in Sources */,
 				759637A12593AE0700E278CC /* main.m in Sources */,
 				A60D20C92597F496004AC979 /* iTermImage+ImageWithData.m in Sources */,
 				756C32AD2597AC2E0047B3A9 /* iTermImage+Sixel.m in Sources */,
+				A6038309294306BA00FE11C2 /* iTermSandboxedWorkerClient.m in Sources */,
+				A603830A294306C400FE11C2 /* iTermAdvancedSettingsModel.m in Sources */,
 				7596379F2593AE0700E278CC /* iTerm2SandboxedWorker.m in Sources */,
-				A69F1BDD28E7571B0054204E /* FileProviderDebugLoggingShim.swift in Sources */,
-				A60AE01A289A18EB00419B14 /* FileProviderLogging.swift in Sources */,
 				A60D20D22597F4DF004AC979 /* iTermImage.m in Sources */,
+				A60383082943067000FE11C2 /* DebugLogging.m in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;
 		};
@@ -15585,6 +15585,7 @@
 				A6153D5921F4EF1A002976FC /* iTermStatusBarTightlyPackedLayoutAlgorithm.m in Sources */,
 				A63F34D121E1E0F8000C9D52 /* iTermSessionPicker.m in Sources */,
 				A66DABFF20867749006391A3 /* iTermTextViewAccessibilityHelper.m in Sources */,
+				A6038307294305EA00FE11C2 /* DebugLogging.m in Sources */,
 				A6DC989A289F2560009D21C0 /* NSFont+iTerm.m in Sources */,
 				A667C240276C7CC2006B4DEF /* iTermFullScreenWindowManager.m in Sources */,
 				53C166AB20C21FBB003B03AF /* iTermMigrationHelper.m in Sources */,
@@ -15775,7 +15776,6 @@
 				A6C762F01B45C52B00E3C992 /* DVRBuffer.m in Sources */,
 				A6C763331B45C52B00E3C992 /* iTermNumberOfSpacesAccessoryViewController.m in Sources */,
 				A6C7630A1B45C52B00E3C992 /* FindContext.m in Sources */,
-				A6C762C61B45C52B00E3C992 /* DebugLogging.m in Sources */,
 				A62C3A8A1BCAE03300B5629D /* iTermDirectoryTreeNode.m in Sources */,
 				A62132D62786544A00B80724 /* CVector.m in Sources */,
 				A6936B561D2F5CE600521B04 /* iTermAdditionalHotKeyTableCellView.m in Sources */,

sources/DebugLogging.m
@@ -99,7 +99,6 @@ static void WriteDebugLogHeader() {
                         windows,
                         [(iTermApplication *)NSApp orderedWindowsPlusAllHotkeyPanels],
                         pinnedMessages];
-    [gDebugLogHeader release];
     gDebugLogHeader = [header copy];
 }
 
@@ -137,7 +136,6 @@ static void FlushDebugLog() {
     }
 
     [gDebugLogStr setString:@""];
-    [gDebugLogHeader release];
     gDebugLogHeader = nil;
     [GetDebugLogLock() unlock];
 }
@@ -148,7 +146,7 @@ void AppendPinnedDebugLogMessage(NSString *key, NSString *value, ...) {
 
     va_list args;
     va_start(args, value);
-    NSString *s = [[[NSString alloc] initWithFormat:value arguments:args] autorelease];
+    NSString *s = [[NSString alloc] initWithFormat:value arguments:args];
     va_end(args);
 
     NSString *log = [NSString stringWithFormat:@"%lld.%06lld [%@]: %@\n", (long long)tv.tv_sec, (long long)tv.tv_usec, key, s];
@@ -161,7 +159,7 @@ void AppendPinnedDebugLogMessage(NSString *key, NSString *value, ...) {
     if (prev) {
         [prev appendString:log];
     } else {
-        gPinnedMessages[key] = [[log mutableCopy] autorelease];
+        gPinnedMessages[key] = [log mutableCopy];
     }
     [GetDebugLogLock() unlock];
 }
@@ -176,7 +174,7 @@ void SetPinnedDebugLogMessage(NSString *key, NSString *value, ...) {
 
     va_list args;
     va_start(args, value);
-    NSString *s = [[[NSString alloc] initWithFormat:value arguments:args] autorelease];
+    NSString *s = [[NSString alloc] initWithFormat:value arguments:args];
     va_end(args);
 
     NSString *log = [NSString stringWithFormat:@"%lld.%06lld [%@]: %@\n", (long long)tv.tv_sec, (long long)tv.tv_usec, key, s];
@@ -243,8 +241,8 @@ void LogForNextCrash(const char *file, int line, const char *function, NSString*
             fileNumber = (fileNumber + 1) % 3;
             [[NSFileManager defaultManager] removeItemAtPath:path error:nil];
             [[NSFileManager defaultManager] createFileAtPath:path contents:nil attributes:nil];
-            handle = [[NSFileHandle fileHandleForWritingAtPath:path] retain];
-            NSDateFormatter *dateFormatter = [[[NSDateFormatter alloc] init] autorelease];
+            handle = [NSFileHandle fileHandleForWritingAtPath:path];
+            NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
             dateFormatter.dateFormat = [NSDateFormatter dateFormatFromTemplate:@"yyyy-MM-dd HH:mm:ss.SSS ZZZ"
                                                                        options:0
                                                                         locale:nil];
@@ -252,7 +250,7 @@ void LogForNextCrash(const char *file, int line, const char *function, NSString*
             NSString *string = [NSString stringWithFormat:@"%@ %@\n", @(getpid()), [dateFormatter stringFromDate:date]];
             [handle writeData:[string dataUsingEncoding:NSUTF8StringEncoding]];
         }
-        handleToUse = [[handle retain] autorelease];
+        handleToUse = handle;
         numLines++;
     }
 
@@ -301,7 +299,7 @@ static BOOL StopDebugLogging() {
         gDebugLogging = NO;
         FlushDebugLog();
 
-        [gDebugLogStr release];
+        gDebugLogStr = nil;
         result = YES;
     }
     [GetDebugLogLock() unlock];
@@ -320,7 +318,7 @@ BOOL TurnOffDebugLoggingSilently(void) {
 
 void ToggleDebugLogging(void) {
     if (!gDebugLogging) {
-        NSAlert *alert = [[[NSAlert alloc] init] autorelease];
+        NSAlert *alert = [[NSAlert alloc] init];
         alert.messageText = @"Debug Logging Enabled";
         alert.informativeText = @"Please reproduce the bug. Then toggle debug logging again to save the log.";
         [alert addButtonWithTitle:@"OK"];
@@ -328,7 +326,7 @@ void ToggleDebugLogging(void) {
         StartDebugLogging();
     } else {
         StopDebugLogging();
-        NSAlert *alert = [[[NSAlert alloc] init] autorelease];
+        NSAlert *alert = [[NSAlert alloc] init];
         alert.messageText = @"Debug Logging Stopped";
         alert.informativeText = @"Please send /tmp/debuglog.txt to the developers.";
         [alert addButtonWithTitle:@"OK"];
@@ -354,7 +352,7 @@ void DLogC(const char *format, va_list args) {
     NSDictionary *userInfo = self.userInfo;
     NSString *const stackKey = @"original stack";
     if (!userInfo[stackKey]) {
-        NSMutableDictionary *temp = [[userInfo mutableCopy] autorelease];
+        NSMutableDictionary *temp = [userInfo mutableCopy];
         temp[stackKey] = self.callStackSymbols;
         userInfo = temp;
     }

commit d1a7bb5833793d707db5e73eeca29e31057e4658

Mess with build settings to fix the nightly build. It was broken by c31b70021eb78a1b48920b3c747011c9c742d4b6

iTerm2.xcodeproj/project.pbxproj
@@ -972,11 +972,17 @@
 		A6024B89254D367E0036D6CF /* iTermColorSuggester.m in Sources */ = {isa = PBXBuildFile; fileRef = A6024B87254D367E0036D6CF /* iTermColorSuggester.m */; };
 		A6024B8B254DDFEE0036D6CF /* iTermNSColorCategoryTests.m in Sources */ = {isa = PBXBuildFile; fileRef = A6024B8A254DDFEE0036D6CF /* iTermNSColorCategoryTests.m */; };
 		A60251681CCD3E5E009BABF1 /* NSURL+iTerm.h in Headers */ = {isa = PBXBuildFile; fileRef = A60251661CCD3E5E009BABF1 /* NSURL+iTerm.h */; };
-		A6038307294305EA00FE11C2 /* DebugLogging.m in Sources */ = {isa = PBXBuildFile; fileRef = 1DD39B19180B82F5004E56D5 /* DebugLogging.m */; };
+		A6038307294305EA00FE11C2 /* DebugLogging.m in Sources */ = {isa = PBXBuildFile; fileRef = 1DD39B19180B82F5004E56D5 /* DebugLogging.m */; settings = {COMPILER_FLAGS = "-DFILE_PROVIDER_LOGGING_AVAILABLE=1"; }; };
 		A60383082943067000FE11C2 /* DebugLogging.m in Sources */ = {isa = PBXBuildFile; fileRef = 1DD39B19180B82F5004E56D5 /* DebugLogging.m */; };
 		A6038309294306BA00FE11C2 /* iTermSandboxedWorkerClient.m in Sources */ = {isa = PBXBuildFile; fileRef = 7577E2A02593B88700905402 /* iTermSandboxedWorkerClient.m */; };
 		A603830A294306C400FE11C2 /* iTermAdvancedSettingsModel.m in Sources */ = {isa = PBXBuildFile; fileRef = A697100A18D94CDA007E901D /* iTermAdvancedSettingsModel.m */; };
 		A603830B294306D800FE11C2 /* iTermUserDefaultsObserver.m in Sources */ = {isa = PBXBuildFile; fileRef = A6EF57D021994DDC00C76698 /* iTermUserDefaultsObserver.m */; };
+		A603830E2943A92800FE11C2 /* AsyncFileProviderLogging.swift in Sources */ = {isa = PBXBuildFile; fileRef = A603830D2943A92800FE11C2 /* AsyncFileProviderLogging.swift */; };
+		A603830F2943A96900FE11C2 /* AsyncFileProviderLogging.swift in Sources */ = {isa = PBXBuildFile; fileRef = A603830D2943A92800FE11C2 /* AsyncFileProviderLogging.swift */; };
+		A60383102943A96A00FE11C2 /* AsyncFileProviderLogging.swift in Sources */ = {isa = PBXBuildFile; fileRef = A603830D2943A92800FE11C2 /* AsyncFileProviderLogging.swift */; };
+		A60383132943AAB700FE11C2 /* DebugLogging.m in Sources */ = {isa = PBXBuildFile; fileRef = 1DD39B19180B82F5004E56D5 /* DebugLogging.m */; };
+		A60383142943AABF00FE11C2 /* iTermAdvancedSettingsModel.m in Sources */ = {isa = PBXBuildFile; fileRef = A697100A18D94CDA007E901D /* iTermAdvancedSettingsModel.m */; };
+		A60383152943AAD100FE11C2 /* iTermUserDefaultsObserver.m in Sources */ = {isa = PBXBuildFile; fileRef = A6EF57D021994DDC00C76698 /* iTermUserDefaultsObserver.m */; };
 		A603E9FB283EE53C0028B648 /* SecretServer.swift in Sources */ = {isa = PBXBuildFile; fileRef = A603E9FA283EE53C0028B648 /* SecretServer.swift */; };
 		A603E9FE283EE9BD0028B648 /* ObjCExceptions.h in Headers */ = {isa = PBXBuildFile; fileRef = A603E9FC283EE9BD0028B648 /* ObjCExceptions.h */; };
 		A603E9FF283EE9BD0028B648 /* ObjCExceptions.m in Sources */ = {isa = PBXBuildFile; fileRef = A603E9FD283EE9BD0028B648 /* ObjCExceptions.m */; };
@@ -2628,7 +2634,7 @@
 		A69F1BD728E65A4F0054204E /* uniE0C1_flame-thin.eps in Resources */ = {isa = PBXBuildFile; fileRef = A69F1B9D28E65A4E0054204E /* uniE0C1_flame-thin.eps */; };
 		A69F1BD828E65A4F0054204E /* uniE0C1_flame-thin.eps in Resources */ = {isa = PBXBuildFile; fileRef = A69F1B9D28E65A4E0054204E /* uniE0C1_flame-thin.eps */; };
 		A69F1BD928E65A4F0054204E /* uniE0C1_flame-thin.eps in Resources */ = {isa = PBXBuildFile; fileRef = A69F1B9D28E65A4E0054204E /* uniE0C1_flame-thin.eps */; };
-		A69F1BDB28E7570C0054204E /* FileProviderDebugLoggingShim.swift in Sources */ = {isa = PBXBuildFile; fileRef = A69F1BDA28E7570C0054204E /* FileProviderDebugLoggingShim.swift */; };
+		A69F1BDB28E7570C0054204E /* FileProviderDebugLoggingShim.swift in Sources */ = {isa = PBXBuildFile; fileRef = A69F1BDA28E7570C0054204E /* FileProviderDebugLoggingShim.swift */; settings = {COMPILER_FLAGS = "-DFILE_PROVIDER_LOGGING_AVAILABLE=1"; }; };
 		A69F1BDC28E7571A0054204E /* FileProviderDebugLoggingShim.swift in Sources */ = {isa = PBXBuildFile; fileRef = A69F1BDA28E7570C0054204E /* FileProviderDebugLoggingShim.swift */; };
 		A6A13AA218C2D23300B241ED /* iTermColorMap.h in Headers */ = {isa = PBXBuildFile; fileRef = A6A13AA018C2D23300B241ED /* iTermColorMap.h */; };
 		A6A13AA718C2D45900B241ED /* NSColor+iTerm.h in Headers */ = {isa = PBXBuildFile; fileRef = A6A13AA518C2D45900B241ED /* NSColor+iTerm.h */; };
@@ -5255,6 +5261,7 @@
 		A60251661CCD3E5E009BABF1 /* NSURL+iTerm.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = "NSURL+iTerm.h"; sourceTree = "<group>"; };
 		A60251671CCD3E5E009BABF1 /* NSURL+iTerm.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = "NSURL+iTerm.m"; sourceTree = "<group>"; };
 		A602516A1CCD40D9009BABF1 /* iTermNSURLCategoryTest.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; path = iTermNSURLCategoryTest.m; sourceTree = "<group>"; };
+		A603830D2943A92800FE11C2 /* AsyncFileProviderLogging.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AsyncFileProviderLogging.swift; sourceTree = "<group>"; };
 		A603E9FA283EE53C0028B648 /* SecretServer.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SecretServer.swift; sourceTree = "<group>"; };
 		A603E9FC283EE9BD0028B648 /* ObjCExceptions.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = ObjCExceptions.h; sourceTree = "<group>"; };
 		A603E9FD283EE9BD0028B648 /* ObjCExceptions.m */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.objc; path = ObjCExceptions.m; sourceTree = "<group>"; };
@@ -10920,6 +10927,7 @@
 				A69412EF28555D3B0086F068 /* SyncAnchor.swift */,
 				A69412E728555D390086F068 /* SyncCache.swift */,
 				A69412ED28555D3B0086F068 /* WorkingSet.swift */,
+				A603830D2943A92800FE11C2 /* AsyncFileProviderLogging.swift */,
 			);
 			path = iTermFileProvider;
 			sourceTree = "<group>";
@@ -14859,6 +14867,7 @@
 				A63EB048284D1ADC00B2AB89 /* FileProviderService.swift in Sources */,
 				A63EB05C284EC5F900B2AB89 /* SSHIdentity.swift in Sources */,
 				A69F1BDB28E7570C0054204E /* FileProviderDebugLoggingShim.swift in Sources */,
+				A60383102943A96A00FE11C2 /* AsyncFileProviderLogging.swift in Sources */,
 				A69413082856831C0086F068 /* FileProviderLogging.swift in Sources */,
 				A63EB050284D1E1D00B2AB89 /* FileProviderServiceError.swift in Sources */,
 			);
@@ -15423,6 +15432,7 @@
 				A6A4867620B67B9D00493302 /* ProfilePreferencesViewController.m in Sources */,
 				A6A4866120B6763900493302 /* ArrangementPreviewView.m in Sources */,
 				A6D4C26821E18CB5009CF11B /* iTermScriptInspector.m in Sources */,
+				A603830F2943A96900FE11C2 /* AsyncFileProviderLogging.swift in Sources */,
 				535EA50120D0F15400FC81E0 /* iTermQuotedRecognizer.m in Sources */,
 				A6180D7521A36F730073F219 /* iTermMetalPerFrameStateRow.m in Sources */,
 				A6A9839127E5C427003319D1 /* PasswordManagerDataSourceProvider.swift in Sources */,
@@ -15637,11 +15647,13 @@
 			files = (
 				A694130528555D3D0086F068 /* FileProviderFetcher.swift in Sources */,
 				A69412FD28555D3D0086F068 /* FileProviderEnumerator.swift in Sources */,
+				A60383152943AAD100FE11C2 /* iTermUserDefaultsObserver.m in Sources */,
 				A694130028555D3D0086F068 /* SyncAnchor.swift in Sources */,
 				A694130328555D3D0086F068 /* FileListDiff.swift in Sources */,
 				A69412E528555C5F0086F068 /* FileProviderServicing.swift in Sources */,
 				A69412FE28555D3D0086F068 /* WorkingSet.swift in Sources */,
 				A694130128555D3D0086F068 /* CachingPaginatingDataSource.swift in Sources */,
+				A60383132943AAB700FE11C2 /* DebugLogging.m in Sources */,
 				A69412F828555D3D0086F068 /* SyncCache.swift in Sources */,
 				A69412FB28555D3D0086F068 /* FileProviderItem.swift in Sources */,
 				A694130228555D3D0086F068 /* NSFileProviderPage+Additions.swift in Sources */,
@@ -15653,7 +15665,9 @@
 				A69412FC28555D3D0086F068 /* ItemModifier.swift in Sources */,
 				A69412FF28555D3D0086F068 /* ItemDeleter.swift in Sources */,
 				A694130428555D3D0086F068 /* Paginator.swift in Sources */,
+				A60383142943AABF00FE11C2 /* iTermAdvancedSettingsModel.m in Sources */,
 				A63EB04E284D1DB400B2AB89 /* FileProviderServiceServer.swift in Sources */,
+				A603830E2943A92800FE11C2 /* AsyncFileProviderLogging.swift in Sources */,
 				A69412F728555D3D0086F068 /* ItemCreator.swift in Sources */,
 				A694130728555D3D0086F068 /* FileProviderExtension.swift in Sources */,
 				A69412F928555D3D0086F068 /* Remote.swift in Sources */,

iTermFileProvider/AsyncFileProviderLogging.swift
@@ -1,0 +1,29 @@
+//
+//  AsyncFileProviderLogging.swift
+//  iTermFileProvider
+//
+//  Created by George Nachman on 12/9/22.
+//
+
+import Foundation
+
+// The only purpose of this file is to sequester code that uses Swift concurrency where it will
+// not be linked into iTerm2SandboxedWorker. I have a request out to DTS to figure out why
+// but it won't launch because it can't find libswift_concurrency.dylib.
+
+@available(macOS 10.15, *)
+public func logging<T>(_ prefix: String, closure: () async throws -> T) async rethrows -> T {
+    return try await LogContext.$logContexts.withValue(LogContext.logContexts + [prefix]) {
+        log("begin")
+        do {
+            defer {
+                log("end")
+            }
+            return try await closure()
+        } catch {
+            log("Exiting logging scope with uncaught error \(error)")
+            throw error
+        }
+    }
+}
+

iTermFileProvider/FileProviderLogging.swift
@@ -70,20 +70,3 @@ public func logging<T>(_ prefix: String, closure: () throws -> T) rethrows -> T 
         }
     }
 }
-
-@available(macOS 10.15, *)
-public func logging<T>(_ prefix: String, closure: () async throws -> T) async rethrows -> T {
-    return try await LogContext.$logContexts.withValue(LogContext.logContexts + [prefix]) {
-        log("begin")
-        do {
-            defer {
-                log("end")
-            }
-            return try await closure()
-        } catch {
-            log("Exiting logging scope with uncaught error \(error)")
-            throw error
-        }
-    }
-}
-

sources/DebugLogging.m
@@ -275,12 +275,14 @@ void LogForNextCrash(const char *file, int line, const char *function, NSString*
 static void StartDebugLogging() {
     [GetDebugLogLock() lock];
     if (!gDebugLogging) {
+#if FILE_PROVIDER_LOGGING_AVAILABLE
         static dispatch_once_t onceToken;
         dispatch_once(&onceToken, ^{
             FileProviderLogging.callback = ^(NSString *message) {
                 DLog(@"%@", message);
             };
         });
+#endif
         if (![iTermAdvancedSettingsModel appendToExistingDebugLog]) {
             [[NSFileManager defaultManager] removeItemAtURL:[NSURL fileURLWithPath:kDebugLogFilename]
                                                       error:nil];