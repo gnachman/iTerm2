03bdb6ca25f4d7e65c282b67f0a4d9e2b21d43a9
George Nachman <gnachman@gmail.com>
2023-03-07 14:53:14 -0800

    Make spam print double width characters

--- tests/spam.cc
+++ tests/spam.cc
@@ -6,14 +6,12 @@ void setline(char* s, int n) {
   int l = random() % n;
   if (cm) l *= 3;
   int j;
-  for (j = 0; j < l; ++j) {
-    if (cm) {
-      int r = random() %30 + 1;
-      s[j++] = 0xe0;
-      s[j++] = 0xb8;
-      s[j++] = 0x80 | r;
-      s[j++] = 0xcc;
-      s[j] = 0x80;
+  for (j = 0; j < l && j < n; ++j) {
+    if (cm && j + 6 < l && (random() % 3) == 0 ) {
+      int r = random() %26;
+      s[j++] = 0xef;
+      s[j++] = 0xbc;
+      s[j] = 0xa0 + r;
     } else {
       s[j] = 'A' + (random() % 60);
     }
@@ -28,7 +26,7 @@ int main(int argc, char*argv[]) {
   } else {
     n = atoi(argv[1]);
   }
-  cm = argc==3;
+  cm = 1;
   for (int i = 0; n < 0 || i < n; ++i) {
     char buffer[10000];
     setline(buffer, sizeof(buffer)-1);
