9d7d8112c8d4d27b08e35435eb5efb4e3f861959
George Nachman <gnachman@gmail.com>
2023-03-15 10:39:43 -0700

    Add comments to updateVersion explaining how to get the right Python

--- tools/updateVersion.py
+++ tools/updateVersion.py
@@ -1,4 +1,8 @@
 #!/Library/Frameworks/Python.framework/Versions/3.9/bin/python3
+# Get it from:
+# https://www.python.org/ftp/python/3.9.13/python-3.9.13-macos11.pkg
+# Install the package and then do
+# /Library/Frameworks/Python.framework/Versions/3.9/bin/pip3 install pyobjc
 
 # We can't use the python in $PATH because xcode ships with Python and we need one that has pybobjc.
 
