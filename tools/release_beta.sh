6ce71feb5a72c410149c4705acea7116aa4c9c16
George Nachman <gnachman@gmail.com>
2023-09-28 11:25:39 -0700

    Remove echos from release_beta.sh

--- tools/release_beta.sh
+++ tools/release_beta.sh
@@ -139,8 +139,8 @@ git checkout -- version.txt
 
 git tag v${VERSION}
 git commit -am ${VERSION}
-echo git push origin master
-echo git push --tags
-echo cd $SVNDIR
-echo git commit -am v${VERSION}
-echo git push origin master
+git push origin master
+git push --tags
+cd $SVNDIR
+git commit -am v${VERSION}
+git push origin master
