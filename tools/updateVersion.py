0cccba97e9cdfc7a5cc2e215bfcaa205235f8273
George Nachman <gnachman@gmail.com>
2023-10-05 16:31:13 -0700

    Hack update version

--- tools/updateVersion.py
+++ tools/updateVersion.py
@@ -1,56 +1,39 @@
-#!/Library/Frameworks/Python.framework/Versions/3.9/bin/python3
-# Get it from:
-# https://www.python.org/ftp/python/3.9.13/python-3.9.13-macos11.pkg
-# Install the package and then do
-# /Library/Frameworks/Python.framework/Versions/3.9/bin/pip3 install pyobjc
+#!/usr/local/bin/python3
 
-# We can't use the python in $PATH because xcode ships with Python and we need one that has pybobjc.
-
+import subprocess
 import os
 import sys
 import time
-import subprocess
 
 try:
-    del os.environ["MACOSX_DEPLOYMENT_TARGET"]
+        del os.environ["MACOSX_DEPLOYMENT_TARGET"]
 except KeyError:
-    pass
+        pass
 from Foundation import NSMutableDictionary
 
 if os.environ["CONFIGURATION"] == "Development":
-    cmd = "git log -1 --format=\"%H\""
-    output = subprocess.check_output(cmd, shell=True).decode("utf-8")
+        cmd = "git log -1 --format=\"%H\""
+        output = subprocess.check_output(cmd, shell=True).decode("utf-8")
 
-    revision = "git.unknown"
-    for line in output.split("\n"):
-        if len(line.strip()) > 0:
-            revision = "git." + line.strip()[:10]
-            break
+        revision = "git.unknown"
+        for line in output.split("\n"):
+            if len(line.strip()) > 0:
+                revision = "git." + line.strip()[:10]
+                break
 
 elif os.environ["CONFIGURATION"] == "Nightly":
-    revision = time.strftime("%Y%m%d-nightly")
+        revision = time.strftime("%Y%m%d-nightly")
 else:
-    revision = time.strftime("%Y%m%d")
+        revision = time.strftime("%Y%m%d")
+
+buildDir = os.environ["BUILT_PRODUCTS_DIR"]
+infoFile = os.environ["INFOPLIST_PATH"]
+path = os.path.join(buildDir, infoFile)
+plist = NSMutableDictionary.dictionaryWithContentsOfFile_(path)
 version = open("version.txt").read().strip() % {"extra": revision}
+print("Updating versions:", infoFile, version)
+plist["CFBundleShortVersionString"] = version
+plist["CFBundleGetInfoString"] = version
+plist["CFBundleVersion"] = version
+plist.writeToFile_atomically_(path, 1)
 
-def update(path):
-    plist = NSMutableDictionary.dictionaryWithContentsOfFile_(path)
-    print("Updating versions:", path, version)
-    if not plist:
-        print(f"WARNING - FAILED TO LOAD PLIST from {path}")
-    plist["CFBundleShortVersionString"] = version
-    plist["CFBundleGetInfoString"] = version
-    plist["CFBundleVersion"] = version
-    plist.writeToFile_atomically_(path, 1)
-    print(plist)
-
-
-# Update the main app's plist
-
-# /Users/gnachman/git/iterm2/Build/Development
-srcDir = os.environ["SRCROOT"]
-print(f"SRCROOT={srcDir}")
-
-path = os.path.join(srcDir, "plists", "iTerm2.plist")
-
-update(path)
