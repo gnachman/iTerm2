76381ad07f9153dbb2fb69866f9535622b8cccf2
George Nachman <gnachman@gmail.com>
2024-05-28 16:34:43 -0700

    Make iTermAI build with correct architectures. Add build script

---
+++ iTermAI/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+VERSION=1.0
+
+set -x
+
+function die {
+  echo $1
+  exit
+}
+
+echo Enter the notarization password
+read -s NOTPASS
+
+xcodebuild -project iTermAI.xcodeproj -scheme iTermAI -configuration Release -destination 'generic/platform=macOS'
+
+cd Build/Release
+
+PRENOTARIZED_ZIP=iTermAI-${VERSION}-prenotarized.zip
+zip -ry $PRENOTARIZED_ZIP iTermAI.app
+xcrun notarytool submit --team-id H7V7XYVQ7D --apple-id "apple@georgester.com" --password "$NOTPASS" $PRENOTARIZED_ZIP > /tmp/upload.out 2>&1 || die "Notarization failed"
+UUID=$(grep id: /tmp/upload.out | head -1 | sed -e 's/.*id: //')
+echo "uuid is $UUID"
+xcrun notarytool info --team-id H7V7XYVQ7D --apple-id "apple@georgester.com" --password "$NOTPASS" $UUID
+sleep 1
+while xcrun notarytool info --team-id H7V7XYVQ7D --apple-id "apple@georgester.com" --password "$NOTPASS" $UUID 2>&1 | egrep -i "in progress|Could not find the RequestUUID|Submission does not exist or does not belong to your team":
+do
+    echo "Trying again"
+    sleep 1
+done
+NOTARIZED_ZIP=iTermAI-${VERSION}.zip
+xcrun stapler staple iTermAI.app
+zip -ry $NOTARIZED_ZIP iTermAI.app
+echo `pwd`/${NOTARIZED_ZIP}
