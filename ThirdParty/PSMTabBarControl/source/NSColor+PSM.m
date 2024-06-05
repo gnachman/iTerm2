3e73425458c64c64dd55790c8af951457087c7cb
George Nachman <gnachman@gmail.com>
2024-06-05 16:48:31 -0700

    Fix how we tested if a window's background color was dark

--- ThirdParty/PSMTabBarControl/source/NSColor+PSM.m
+++ ThirdParty/PSMTabBarControl/source/NSColor+PSM.m
@@ -9,11 +9,24 @@
 
 @implementation NSColor (PSM)
 
+static BOOL PSMAppearanceIsDark(NSAppearance *appearance) {
+    NSAppearanceName bestMatch = [appearance bestMatchFromAppearancesWithNames:@[ NSAppearanceNameDarkAqua,
+                                                                                  NSAppearanceNameVibrantDark,
+                                                                                  NSAppearanceNameAqua,
+                                                                                  NSAppearanceNameVibrantLight ]];
+    if ([bestMatch isEqualToString:NSAppearanceNameDarkAqua] ||
+        [bestMatch isEqualToString:NSAppearanceNameVibrantDark]) {
+        return YES;
+    }
+    return NO;
+}
+
+
 // http://www.nbdtech.com/Blog/archive/2008/04/27/Calculating-the-Perceived-Brightness-of-a-Color.aspx
 // http://alienryderflex.com/hsp.html
 - (NSColor *)it_srgbForColorInWindow:(NSWindow *)window {
     if ([self isEqual:window.backgroundColor]) {
-        if ([window.effectiveAppearance.name isEqualToString:NSAppearanceNameVibrantDark]) {
+        if (PSMAppearanceIsDark(window.effectiveAppearance)) {
             return [NSColor colorWithSRGBRed:0.25 green:0.25 blue:0.25 alpha:1];
         } else {
             return [NSColor colorWithSRGBRed:0.75 green:0.75 blue:0.75 alpha:1];
