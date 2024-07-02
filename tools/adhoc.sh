a53b276f7f398ed0871e082d982b0c2788c0dd3f
George Nachman <gnachman@gmail.com>
2024-07-02 16:08:13 -0700

    Adhoc build 0.20240702_160409-adhoc

--- tools/adhoc.sh
+++ tools/adhoc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo Enter the notarization password
 read -s NOTPASS
+set -x
 COMPACTDATE=$(date +"%Y%m%d_%H%M%S")
 VERSION="0.$COMPACTDATE-adhoc"
 echo "$VERSION" > version.txt
@@ -11,19 +12,26 @@ rm -rf build/Deployment/iTerm.app
 mv build/Deployment/iTerm2.app build/Deployment/iTerm.app
 pushd build/Deployment
 
+function die {
+  echo $1
+  exit
+}
 # - notarize -
 PRENOTARIZED_ZIP=iTerm2-${NAME}-prenotarized.zip
 zip -ry $PRENOTARIZED_ZIP iTerm.app
-xcrun altool --notarize-app --primary-bundle-id "com.googlecode.iterm2" --username "apple@georgester.com" --password "$NOTPASS" --file $PRENOTARIZED_ZIP > /tmp/upload.out 2>&1 || die "Notarization failed"
-UUID=$(grep RequestUUID /tmp/upload.out | sed -e 's/RequestUUID = //')
-echo "uuid is $UUID"
-xcrun altool --notarization-info $UUID -u "apple@georgester.com" -p "$NOTPASS"
-sleep 1
-while xcrun altool --notarization-info $UUID -u "apple@georgester.com" -p "$NOTPASS" 2>&1 | egrep -i "in progress|Could not find the RequestUUID":
-do
-    echo "Trying again"
-    sleep 1
-done
+
+  xcrun notarytool submit --team-id H7V7XYVQ7D --apple-id "apple@georgester.com" --password "$NOTPASS" $PRENOTARIZED_ZIP > /tmp/upload.out 2>&1 || die "Notarization failed"
+  UUID=$(grep id: /tmp/upload.out | head -1 | sed -e 's/.*id: //')
+  echo "uuid is $UUID"
+  xcrun notarytool info --team-id H7V7XYVQ7D --apple-id "apple@georgester.com" --password "$NOTPASS" $UUID
+  sleep 1
+  while xcrun notarytool info --team-id H7V7XYVQ7D --apple-id "apple@georgester.com" --password "$NOTPASS" $UUID 2>&1 | egrep -i "in progress|Could not find the RequestUUID|Submission does not exist or does not belong to your team":
+  do
+      echo "Trying again"
+      sleep 1
+  done
+
+
 xcrun stapler staple iTerm.app
 # - end notarize - 
 
