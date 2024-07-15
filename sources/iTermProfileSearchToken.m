gnachman/iTerm2

commit 5edec841bdc88ca00167604850a79cbe87d70847

Make snippet search work like profile search

sources/iTermProfileSearchToken.m
@@ -7,9 +7,9 @@
 //
 
 #import "iTermProfileSearchToken.h"
+#import "NSArray+iTerm.h"
 
-static NSString *const kTagRestrictionOperator = @"tag:";
-static NSString *const kTitleRestrictionOperator = @"name:";
+NSString *const kTagRestrictionOperator = @"tag:";
 
 @interface iTermProfileSearchToken()
 @property(nonatomic, copy) NSArray *strings;
@@ -20,6 +20,8 @@ static NSString *const kTitleRestrictionOperator = @"name:";
 
 @implementation iTermProfileSearchToken {
     NSRange _range;
+    NSArray<NSString *> *_operators;
+    NSArray<NSString *> *_nonTagOperators;
 }
 
 // Valid phrases look like
@@ -38,8 +40,17 @@ static NSString *const kTitleRestrictionOperator = @"name:";
 //
 // For backward compatibility, * may occur in place of ^, but it has no effect.
 - (instancetype)initWithPhrase:(NSString *)originalPhrase {
+    return [self initWithPhrase:originalPhrase operators:@[ kTagRestrictionOperator, @"name:" ]];
+}
+
+- (instancetype)initWithPhrase:(NSString *)originalPhrase operators:(NSArray<NSString *> *)operators {
     self = [super init];
     if (self) {
+        _operators = [operators copy];
+        _nonTagOperators = [operators filteredArrayUsingBlock:^BOOL(NSString *op) {
+            return ![op isEqualToString:kTagRestrictionOperator];
+        }];
+
         // First, parse an operator like tag: or name:, which must be at the beginning.
         NSString *phrase = originalPhrase;
         if ([phrase hasPrefix:@"-"]) {
@@ -47,7 +58,6 @@ static NSString *const kTitleRestrictionOperator = @"name:";
             phrase = [originalPhrase substringFromIndex:1];
         }
         NSString *string = phrase;
-        NSArray *operators = @[ kTagRestrictionOperator, kTitleRestrictionOperator ];
         for (NSString *operator in operators) {
             if ([phrase hasPrefix:operator]) {
                 string = [phrase substringFromIndex:operator.length];
@@ -80,15 +90,44 @@ static NSString *const kTitleRestrictionOperator = @"name:";
     return self;
 }
 
-- (BOOL)matchesAnyWordInNameWords:(NSArray *)titleWords {
-    if ([_operator isEqualToString:kTagRestrictionOperator]) {
+- (instancetype)initWithTag:(NSString *)tag {
+    return [self initWithTag:tag operators:@[ kTagRestrictionOperator, @"name:" ]];
+}
+
+- (instancetype)initWithTag:(NSString *)tag operators:(NSArray<NSString *> *)operators {
+    self = [super init];
+    if (self) {
+        _operators = [operators copy];
+        _nonTagOperators = [operators filteredArrayUsingBlock:^BOOL(NSString *op) {
+            return ![op isEqualToString:kTagRestrictionOperator];
+        }];
+        _negated = NO;
+        self.operator = kTagRestrictionOperator;
+        self.anchorStart = NO;
+        self.anchorEnd = NO;
+        self.strings = [tag componentsSeparatedByString:@" "];
+    }
+    return self;
+}
+
+- (BOOL)matchesAnyWordIn:(NSArray<NSString *> *)words operator:(NSString *)operator {
+    if (_operator != nil && ![operator isEqualToString:_operator]) {
+        // This query token has an operator but the words (correspnding to a phrase in the document)
+        // do not match it. For example, token is "title:foo" and words are "foo" with operator "text:".
+        return NO;
+    }
+    return [self matchesAnyWordInWords:words];
+}
+
+- (BOOL)matchesAnyWordInNameWords:(NSArray<NSString *> *)titleWords {
+    if (_operator != nil && ![_nonTagOperators containsObject:_operator]) {
         return NO;
     }
     return [self matchesAnyWordInWords:titleWords];
 }
 
 - (BOOL)matchesAnyWordInTagWords:(NSArray *)tagWords {
-    if ([_operator isEqualToString:kTitleRestrictionOperator]) {
+    if (_operator == nil || [_nonTagOperators containsObject:_operator]) {
         return NO;
     }
     return [self matchesAnyWordInWords:tagWords];
@@ -121,10 +160,8 @@ static NSString *const kTitleRestrictionOperator = @"name:";
     return NO;
 }
 
-- (void)dealloc {
-    [_strings release];
-    [_operator release];
-    [super dealloc];
+- (BOOL)isTag {
+    return ([_operator isEqualToString:kTagRestrictionOperator]);
 }
 
 @end

iTerm2.xcodeproj/project.pbxproj
@@ -3030,7 +3030,6 @@
 		A6C763151B45C52B00E3C992 /* iTermLogoGenerator.m in Sources */ = {isa = PBXBuildFile; fileRef = 1DA3E2B91970ACBE00001E6E /* iTermLogoGenerator.m */; };
 		A6C763161B45C52B00E3C992 /* iTermMinimumSubsequenceMatcher.m in Sources */ = {isa = PBXBuildFile; fileRef = A697E27A1B42501000E175DA /* iTermMinimumSubsequenceMatcher.m */; };
 		A6C763171B45C52B00E3C992 /* iTermMouseCursor.m in Sources */ = {isa = PBXBuildFile; fileRef = A6AE1ECC191FF9DB00780C19 /* iTermMouseCursor.m */; };
-		A6C7631A1B45C52B00E3C992 /* iTermProfileSearchToken.m in Sources */ = {isa = PBXBuildFile; fileRef = 1D468BBF1B056CD600226083 /* iTermProfileSearchToken.m */; };
 		A6C7631D1B45C52B00E3C992 /* iTermSelectionScrollHelper.m in Sources */ = {isa = PBXBuildFile; fileRef = A6E77F711A23D195009B1CB6 /* iTermSelectionScrollHelper.m */; };
 		A6C763211B45C52B00E3C992 /* iTermLaunchServices.m in Sources */ = {isa = PBXBuildFile; fileRef = A6E713B918FCCDD1008D94DD /* iTermLaunchServices.m */; };
 		A6C763231B45C52B00E3C992 /* MovePaneController.m in Sources */ = {isa = PBXBuildFile; fileRef = 1D29732614082676004C5DBE /* MovePaneController.m */; };
@@ -3273,6 +3272,8 @@
 		A6D5985121B78B9700B59C94 /* iTermBroadcastInputHelper.h in Headers */ = {isa = PBXBuildFile; fileRef = A6D5984F21B78B9700B59C94 /* iTermBroadcastInputHelper.h */; };
 		A6D5985221B78B9700B59C94 /* iTermBroadcastInputHelper.m in Sources */ = {isa = PBXBuildFile; fileRef = A6D5985021B78B9700B59C94 /* iTermBroadcastInputHelper.m */; };
 		A6D7D2222BBE0E7B00D884F5 /* FakeWindow.m in Sources */ = {isa = PBXBuildFile; fileRef = 1D173858126C820A004622DC /* FakeWindow.m */; };
+		A6D7E0712C1B5FB1007D0A37 /* ProfileStyleSearchEngine.swift in Sources */ = {isa = PBXBuildFile; fileRef = A6D7E0702C1B5FB1007D0A37 /* ProfileStyleSearchEngine.swift */; };
+		A6D7E0722C1B6AF5007D0A37 /* iTermProfileSearchToken.m in Sources */ = {isa = PBXBuildFile; fileRef = 1D468BBF1B056CD600226083 /* iTermProfileSearchToken.m */; };
 		A6D8973A22154A8800325F6A /* AnnotateTrigger.h in Headers */ = {isa = PBXBuildFile; fileRef = A6D8973822154A8800325F6A /* AnnotateTrigger.h */; };
 		A6D8973B22154A8800325F6A /* AnnotateTrigger.m in Sources */ = {isa = PBXBuildFile; fileRef = A6D8973922154A8800325F6A /* AnnotateTrigger.m */; };
 		A6D8CC3220CE417000E79512 /* URLAction.m in Sources */ = {isa = PBXBuildFile; fileRef = A6CFDAD1185D2587005DC94B /* URLAction.m */; };
@@ -7036,6 +7037,7 @@
 		A6D5724227BCE06C002E6E9F /* iTermTextDataSource.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = iTermTextDataSource.h; sourceTree = "<group>"; };
 		A6D5984F21B78B9700B59C94 /* iTermBroadcastInputHelper.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = iTermBroadcastInputHelper.h; sourceTree = "<group>"; };
 		A6D5985021B78B9700B59C94 /* iTermBroadcastInputHelper.m */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.objc; path = iTermBroadcastInputHelper.m; sourceTree = "<group>"; };
+		A6D7E0702C1B5FB1007D0A37 /* ProfileStyleSearchEngine.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ProfileStyleSearchEngine.swift; sourceTree = "<group>"; };
 		A6D8973822154A8800325F6A /* AnnotateTrigger.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = AnnotateTrigger.h; sourceTree = "<group>"; };
 		A6D8973922154A8800325F6A /* AnnotateTrigger.m */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.objc; path = AnnotateTrigger.m; sourceTree = "<group>"; };
 		A6DBC0361FF9B30000F1466D /* iTermTexturePool.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; name = iTermTexturePool.h; path = Metal/Infrastructure/iTermTexturePool.h; sourceTree = "<group>"; };
@@ -10800,6 +10802,7 @@
 				A6CC498B2C08E03A003484B2 /* AttributedStringSaver.swift */,
 				A6CC498D2C08E567003484B2 /* CommandShareMenuProvider.swift */,
 				A6CC498F2C08EFC1003484B2 /* CommandURLBuilder.swift */,
+				A6D7E0702C1B5FB1007D0A37 /* ProfileStyleSearchEngine.swift */,
 			);
 			name = Helpers;
 			sourceTree = "<group>";
@@ -15273,6 +15276,7 @@
 				A6FEFC7B2B1FA23400D98EB9 /* ToolCodecierge.swift in Sources */,
 				A6153D5121F30FF4002976FC /* iTermStatusBarJobComponent.m in Sources */,
 				537BFDDF2102B4060098C91F /* iTermPublisher.m in Sources */,
+				A6D7E0712C1B5FB1007D0A37 /* ProfileStyleSearchEngine.swift in Sources */,
 				A61A85A224F1A70500B03880 /* iTermGlobalSearchWindowController.m in Sources */,
 				A6354B942A1A8828002836A3 /* BookmarkDialogViewController.swift in Sources */,
 				A6916941281346A400083F99 /* ExclusiveSelectionTextView.swift in Sources */,
@@ -15360,6 +15364,7 @@
 				A6EC937E24E859D100EEADEF /* ToolDirectoriesView.m in Sources */,
 				A60C034B20881D6000FE2F1F /* iTermWebSocketCookieJar.m in Sources */,
 				A6E1524C27613BF100D0F41C /* LineBuffer.m in Sources */,
+				A6D7E0722C1B6AF5007D0A37 /* iTermProfileSearchToken.m in Sources */,
 				5370679021C9D2780088D0F3 /* SIGArchiveChunk.m in Sources */,
 				A62EE251274473C700D57B54 /* iTermProfilePreferencesTabViewWrapperView.m in Sources */,
 				A665C1D2243D962C00F623F0 /* TerminalFile.m in Sources */,
@@ -16260,7 +16265,6 @@
 				A67778B51CFD4A7300DEED78 /* iTermHotKeyProfileBindingController.m in Sources */,
 				A6C7633A1B45C52B00E3C992 /* SplitPanel.m in Sources */,
 				A6C763921B45C52B00E3C992 /* iTermFileDescriptorClient.c in Sources */,
-				A6C7631A1B45C52B00E3C992 /* iTermProfileSearchToken.m in Sources */,
 				A6936B521D2F5CA200521B04 /* iTermAdditionalHotKeyObjectValue.m in Sources */,
 				A6C762FB1B45C52B00E3C992 /* FileTransferManager.m in Sources */,
 				A658716B1D85E6750061CEEE /* PFMoveApplication.m in Sources */,

sources/NSStringITerm.h
@@ -103,7 +103,7 @@ int decode_utf8_char(const unsigned char * restrict datap,
 
 // Same as componentsInShellCommand but \r, \n, \t, and \a map to the letters r, n, t, and a,
 // not to controls.
-- (NSArray *)componentsBySplittingProfileListQuery;
+- (NSArray<NSString *> *)componentsBySplittingProfileListQuery;
 
 - (NSString *)stringByReplacingBackreference:(int)n withString:(NSString *)s;
 - (NSString *)stringByReplacingEscapedChar:(unichar)echar withString:(NSString *)s;

sources/NSStringITerm.m
@@ -205,8 +205,81 @@
             stringByReplacingOccurrencesOfString:@"\n" withString:@"\r"];
 }
 
-- (NSArray *)componentsBySplittingProfileListQuery {
-    return [self componentsBySplittingStringWithQuotesAndBackslashEscaping:@{}];
+- (NSArray<NSString *> *)componentsBySplittingProfileListQuery {
+    NSMutableArray<NSString *> *result = [NSMutableArray array];
+
+    BOOL inDoubleQuotes = NO; // Are we inside double quotes?
+    BOOL escape = NO;  // Should this char be escaped?
+    NSMutableString *currentValue = [NSMutableString string];
+    BOOL isFirstCharacterOfWord = YES;
+
+    for (NSInteger i = 0; i <= self.length; i++) {
+        unichar c;
+        if (i < self.length) {
+            c = [self characterAtIndex:i];
+            if (c == 0) {
+                // Pretty sure this can't happen, but better to be safe.
+                c = ' ';
+            }
+        } else {
+            // Signifies end-of-string.
+            c = 0;
+            escape = NO;
+        }
+
+        if (c == '\\' && !escape) {
+            escape = YES;
+            continue;
+        }
+
+        if (escape) {
+            isFirstCharacterOfWord = NO;
+            escape = NO;
+            if (inDoubleQuotes) {
+                // Determined by testing with bash.
+                if (c == '"') {
+                    [currentValue appendString:@"\""];
+                } else if (c == '\\') {
+                    [currentValue appendString:@"\\"];
+                } else {
+                    [currentValue appendFormat:@"\\%C", c];
+                }
+            } else {
+                [currentValue appendFormat:@"%C", c];
+            }
+            continue;
+        }
+
+        if (c == '"') {
+            inDoubleQuotes = !inDoubleQuotes;
+            isFirstCharacterOfWord = NO;
+            continue;
+        }
+        if (c == 0) {
+            inDoubleQuotes = NO;
+        }
+
+        // Treat end-of-string like whitespace.
+        BOOL isWhitespace = (c == 0 || iswspace(c));
+
+        if (!inDoubleQuotes && isWhitespace) {
+            if (!isFirstCharacterOfWord) {
+                [result addObject:currentValue];
+
+                currentValue = [NSMutableString string];
+                isFirstCharacterOfWord = YES;
+            }
+            // Ignore whitespace not in quotes or escaped.
+            continue;
+        }
+
+        if (isFirstCharacterOfWord) {
+            isFirstCharacterOfWord = NO;
+        }
+        [currentValue appendFormat:@"%C", c];
+    }
+
+    return result;
 }
 
 - (NSString *)it_substringToIndex:(NSInteger)index {

sources/ProfileStyleSearchEngine.swift
@@ -1,0 +1,251 @@
+//
+//  ProfileStyleSearchEngine.swift
+//  iTerm2SharedARC
+//
+//  Created by George Nachman on 6/13/24.
+//
+
+import Foundation
+
+extension NSAttributedString {
+    @objc(highlightMatchesForQuery:phraseIdentifier:)
+    func highlight(query: String, phraseIdentifier: String) -> NSAttributedString {
+        if query.isEmpty {
+            return self
+        }
+        var tags = [String]()
+        var phrases = [String: String]()
+        if phraseIdentifier == kTagRestrictionOperator {
+            tags.append(string)
+        } else {
+            phrases[phraseIdentifier] = string
+        }
+        let doc = ProfileStyleSearchEngineDocument(phrases: phrases, tags: tags)
+        let query = ProfileStyleSearchEngineQuery(query: query, operators: [phraseIdentifier])
+        let engine = ProfileStyleSearchEngine(query: query)
+        if let result = engine.search(document: doc, sloppy: true), let indexes = result.phraseIndexes[phraseIdentifier] {
+            return highlight(indexes: indexes)
+        }
+        return self
+    }
+
+    func highlight(indexes: IndexSet) -> NSAttributedString {
+        let result = mutableCopy() as! NSMutableAttributedString
+        for range in indexes.rangeView {
+            result.highlight(range: NSRange(range))
+        }
+
+        return result
+    }
+}
+
+extension NSMutableAttributedString {
+    func highlight(range: NSRange) {
+        let fontManager = NSFontManager.shared
+        enumerateAttributes(in: range, options: []) { attrs, range, stopPtr in
+            var modifiedAttrs = attrs
+            modifiedAttrs[.backgroundColor] = NSColor.yellow
+            modifiedAttrs[.foregroundColor] = NSColor.black
+            if let obj = attrs[.font], let font = obj as? NSFont {
+                let bold = fontManager.convert(font, toHaveTrait: .boldFontMask)
+                modifiedAttrs[.font] = bold
+            }
+            setAttributes(modifiedAttrs, range: range)
+        }
+    }
+}
+
+@objc(iTermProfileStyleSearchEngineResult)
+class ProfileStyleSearchEngineResult: NSObject {
+    // Maps a phrase identifier to an indexset of matches in the corresponding phrase.
+    // For tags, the phrase identifier is "tag:"+tagname.
+    // For other parts, the phrase identifier equals the operator name (e.g., "text:").
+    fileprivate(set) var phraseIndexes = [String: IndexSet]()
+
+    @objc func highlight(attributedString: NSAttributedString,
+                         operator op: String) -> NSAttributedString {
+        if let indexSet = phraseIndexes[op] {
+            return attributedString.highlight(indexes: indexSet)
+        }
+        return attributedString
+    }
+}
+
+@objc(iTermProfileStyleSearchEngineQuery)
+class ProfileStyleSearchEngineQuery: NSObject {
+    private(set) var tokens: [iTermProfileSearchToken]
+
+    @objc var hasTags: Bool {
+        return tokens.anySatisfies { token in
+            token.isTag
+        }
+    }
+    
+    @objc
+    init(query: String, operators: [String]) {
+        let phrases = (query as NSString).componentsBySplittingProfileListQuery() ?? []
+        tokens = phrases.map {
+            iTermProfileSearchToken(phrase: $0, operators: operators)
+        }
+    }
+
+    @objc(addTag:)
+    func add(tag: String) {
+        tokens.append(iTermProfileSearchToken(tag: tag))
+    }
+}
+
+@objc(iTermProfileStyleSearchEngineDocument)
+class ProfileStyleSearchEngineDocument: NSObject {
+    let phrases: [String: String]
+    let tags: [String]
+
+    // Phrases map operator (e.g., "text:") to value (e.g., the content of a document)
+    @objc(initWithPhrases:tags:)
+    init(phrases: [String: String], tags: [String]) {
+        self.phrases = phrases
+        self.tags = tags
+    }
+}
+
+@objc(iTermProfileStyleSearchEngine)
+class ProfileStyleSearchEngine: NSObject {
+    let query: ProfileStyleSearchEngineQuery
+
+    @objc
+    init(query: ProfileStyleSearchEngineQuery) {
+        self.query = query
+    }
+
+    // Returns nil if the document does not match the query.
+    // Sloppy search doesn't quit when a token fails to match. It's used when you want to know which
+    // ranges matched (perhaps when only part of the document is available so match failures are expected).
+    @objc
+    func search(document: ProfileStyleSearchEngineDocument,
+                sloppy: Bool = false) -> ProfileStyleSearchEngineResult? {
+        let result = ProfileStyleSearchEngineResult()
+        if query.tokens.isEmpty {
+            return result
+        }
+        // First iterate over query tokens because all must be satisfied.
+        for queryToken in query.tokens {
+            let status = search(queryToken: queryToken, document: document, result: result)
+            if sloppy {
+                continue
+            }
+            switch status {
+            case .excluded:
+                return nil
+            case .matched:
+                break
+            case .none:
+                if !queryToken.negated {
+                    return nil
+                }
+            }
+        }
+        return result
+    }
+
+    private enum SearchStatus {
+        case none
+        case matched
+        case excluded
+    }
+
+    private func search(queryToken: iTermProfileSearchToken,
+                        document: ProfileStyleSearchEngineDocument,
+                        result: ProfileStyleSearchEngineResult) -> SearchStatus {
+        var foundAnyMatches = false
+
+        switch searchPhrases(queryToken: queryToken, phrases: document.phrases, result: result) {
+        case .none:
+            break
+        case .matched:
+            foundAnyMatches = true
+        case .excluded:
+            return .excluded
+        }
+
+        switch searchTags(queryToken: queryToken, tags: document.tags, result: result) {
+        case .none:
+            break
+        case .matched:
+            foundAnyMatches = true
+        case .excluded:
+            return .excluded
+        }
+
+        return foundAnyMatches ? .matched : .none
+    }
+
+    private func searchPhrases(queryToken: iTermProfileSearchToken,
+                               phrases: [String: String],
+                               result: ProfileStyleSearchEngineResult) -> SearchStatus {
+        var status = SearchStatus.none
+        for (i, kvp) in phrases.enumerated() {
+            let (op, content) = (kvp.key, kvp.value)
+            guard let matchingIndexes = search(queryToken: queryToken, phrase: content, operator: op) else {
+                return .excluded
+            }
+            if matchingIndexes.count > 0 {
+                status = .matched
+                result.phraseIndexes[op, default: IndexSet()].insert(integersIn: matchingIndexes)
+            }
+        }
+        return status
+
+    }
+
+    private func searchTags(queryToken: iTermProfileSearchToken,
+                            tags: [String],
+                            result: ProfileStyleSearchEngineResult) -> SearchStatus {
+        var status = SearchStatus.none
+        for (j, tag) in tags.enumerated() {
+            let tagWords = tag.components(separatedBy: .whitespaces)
+            let found = queryToken.matchesAnyWord(inTagWords: tagWords)
+            if found {
+                if queryToken.negated {
+                    return .excluded
+                }
+                if let range = Range(queryToken.range) {
+                    result.phraseIndexes[kTagRestrictionOperator + tag, default: IndexSet()].insert(integersIn: range)
+                    status = .matched
+                }
+            }
+        }
+        return status
+    }
+
+    // Returns nil if the phrase includes a negated token.
+    // Returns an empty range if no match was found.
+    // Otherwise, returns the range that matched.
+    private func search(queryToken: iTermProfileSearchToken, phrase: String, operator op: String) -> Range<Int>? {
+        let phraseWords = phrase.components(separatedBy: .whitespacesAndNewlines)
+        let found = queryToken.matchesAnyWord(in: phraseWords, operator: op)
+        guard found else {
+            return 0..<0
+        }
+        if queryToken.negated {
+            return nil
+        }
+        return Range(queryToken.range)
+    }
+
+    private func search(phrase: String) -> IndexSet? {
+        let documentTokens = phrase.components(separatedBy: .whitespacesAndNewlines)
+        var result = IndexSet()
+        for queryToken in query.tokens {
+            let found = queryToken.matchesAnyWord(inNameWords: documentTokens)
+            if found {
+                if queryToken.negated {
+                    return nil
+                }
+                if let range = Range(queryToken.range) {
+                    result.insert(integersIn: range)
+                }
+            }
+        }
+        return result
+    }
+}

sources/iTerm2SharedARC-Bridging-Header.h
@@ -38,6 +38,7 @@
 #import "iTermOrderEnforcer.h"
 #import "iTermPreferences.h"
 #import "iTermProfilePreferences.h"
+#import "iTermProfileSearchToken.h"
 #import "iTermPromise.h"
 #import "iTermPublisher.h"
 #import "iTermPythonRuntimeDownloader.h"

sources/iTermProfileSearchToken.h
@@ -8,14 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
+extern NSString *const kTagRestrictionOperator;
+
 @interface iTermProfileSearchToken : NSObject
 
 @property(nonatomic, readonly) NSRange range;
 @property(nonatomic, readonly) BOOL negated;
+@property(nonatomic, readonly) BOOL isTag;
+
+- (instancetype)initWithPhrase:(NSString *)phrase operators:(NSArray<NSString *> *)operators;
+- (instancetype)initWithTag:(NSString *)tag operators:(NSArray<NSString *> *)operators;
 
 - (instancetype)initWithPhrase:(NSString *)phrase;
+- (instancetype)initWithTag:(NSString *)tag;
 
-- (BOOL)matchesAnyWordInNameWords:(NSArray *)nameWords;
-- (BOOL)matchesAnyWordInTagWords:(NSArray *)tagWords;
+// This assumes the operator is not tag:
+- (BOOL)matchesAnyWordIn:(NSArray<NSString * > *)words operator:(NSString *)operator;
+
+- (BOOL)matchesAnyWordInNameWords:(NSArray<NSString * > *)nameWords;
+- (BOOL)matchesAnyWordInTagWords:(NSArray<NSString * > *)tagWords;
 
 @end
+
+NS_ASSUME_NONNULL_END

sources/iTermSnippetsEditingViewController.m
@@ -72,48 +72,13 @@ static NSString *const iTermSnippetsEditingPasteboardType = @"com.googlecode.ite
 
 - (void)load {
     _allSnippets = [[[iTermSnippetsModel sharedInstance] snippets] copy];
-    _filteredSnippets = [_allSnippets filteredArrayUsingBlock:^BOOL(iTermSnippet *snippet) {
-        return [self snippetMatchesQuery:snippet];
-    }];
-}
-
-- (iTermTuple<NSString *, NSSet<NSString *> *> *)phraseAndTagsFromQuery {
-    NSString *query = self.query;
-    if (query.length == 0) {
-        return [iTermTuple tupleWithObject:@"" andObject:nil];
-    }
-    NSMutableSet<NSString *> *tags = [NSMutableSet set];
-    NSRange tagRange = [query rangeOfString:@"tag:"];
-    while (tagRange.location != NSNotFound) {
-        if (tagRange.location == 0 || [query characterAtIndex:tagRange.location - 1] == ' ') {
-            NSString *tag = [[[query substringFromIndex:tagRange.location + @"tag:".length] componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] firstObject];
-            if (tag.length) {
-                NSString *before = [[query substringToIndex:tagRange.location] stringByTrimmingTrailingWhitespace];
-                NSString *after = [[query substringFromIndex:NSMaxRange(tagRange) + tag.length] stringByTrimmingLeadingWhitespace];
-                query = [before stringByAppendingString:after];
-            }
-            [tags addObject:tag];
-            tagRange = [query rangeOfString:@"tag:"];
-        } else {
-            break;
-        }
-    }
-    return [iTermTuple tupleWithObject:query andObject:tags];
+    _filteredSnippets = [[iTermSnippetsModel sharedInstance] snippetsMatchingSearchQuery:self.query
+                                                                          additionalTags:@[]
+                                                                               tagsFound:nil];
 }
 
 - (BOOL)snippetMatchesQuery:(iTermSnippet *)snippet {
-    iTermTuple<NSString *, NSSet<NSString *> *> *phraseTags = [self phraseAndTagsFromQuery];
-    NSString *query = phraseTags.firstObject;
-    NSSet<NSString *> *tags = phraseTags.secondObject;
-    if (tags.count && ![tags isSubsetOfSet:[NSSet setWithArray:snippet.tags]]) {
-        return NO;
-    }
-    if (query.length == 0) {
-        return YES;
-    }
-    return ([snippet.tags containsObject:query] ||
-            [snippet.title localizedCaseInsensitiveContainsString:query] ||
-            [snippet.value localizedCaseInsensitiveContainsString:query]);
+    return [iTermSnippetsModel snippet:snippet matchesQuery:self.query];
 }
 
 - (NSInteger)filteredIndex:(NSInteger)unfilteredIndex {
@@ -454,7 +419,8 @@ static NSString *const iTermSnippetsEditingPasteboardType = @"com.googlecode.ite
     NSAttributedString *titleAttributedString =
     [NSAttributedString attributedStringWithString:[@" " stringByAppendingString:trimmedTitle]
                                         attributes:attributes];
-    titleAttributedString = [self attributedString:titleAttributedString byHighlightingSubstring:self.query];
+    titleAttributedString = [titleAttributedString highlightMatchesForQuery:self.query
+                                                           phraseIdentifier:@"title:"];
 
     if (_filteredSnippets[row].tags.count) {
         NSArray<NSAttributedString *> *substrings = [_filteredSnippets[row].tags flatMapWithBlock:^id _Nullable(NSString * _Nonnull tag) {
@@ -475,32 +441,6 @@ static NSString *const iTermSnippetsEditingPasteboardType = @"com.googlecode.ite
         textField.attributedStringValue = titleAttributedString;
     }
     return cellView;
-}
-
-- (NSAttributedString *)attributedString:(NSAttributedString *)document byHighlightingSubstring:(NSString *)query {
-    const NSRange range = [document.string rangeOfString:query
-                                                 options:NSCaseInsensitiveSearch
-                                                   range:NSMakeRange(0, document.string.length)];
-    if (range.location == NSNotFound) {
-        return document;
-    }
-
-    NSFontManager *fontManager = [NSFontManager sharedFontManager];
-    NSMutableAttributedString *result = [document mutableCopy];
-    [result enumerateAttributesInRange:range options:0 usingBlock:^(NSDictionary<NSAttributedStringKey,id> * _Nonnull attrs, NSRange range, BOOL * _Nonnull stop) {
-        NSMutableDictionary *modifiedAttrs = [attrs mutableCopy];
-        modifiedAttrs[NSBackgroundColorAttributeName] = [NSColor yellowColor];
-        modifiedAttrs[NSForegroundColorAttributeName] = [NSColor blackColor];
-        NSFont *font = attrs[NSFontAttributeName];
-        if (font) {
-            NSFont *boldFont = [fontManager convertFont:font toHaveTrait:NSBoldFontMask];
-            if (boldFont) {
-                modifiedAttrs[NSFontAttributeName] = boldFont;
-            }
-        }
-        [result setAttributes:modifiedAttrs range:range];
-    }];
-    return result;
 }
 
 - (NSView *)viewForValueColumnOnRow:(NSInteger)row {
@@ -545,9 +485,8 @@ static NSString *const iTermSnippetsEditingPasteboardType = @"com.googlecode.ite
     NSAttributedString *unhighlightedAttributedString =
     [NSAttributedString attributedStringWithString:string
                                         attributes:attributes];
-    textField.attributedStringValue = [self attributedString:unhighlightedAttributedString
-                                     byHighlightingSubstring:self.query];
-
+    textField.attributedStringValue = [unhighlightedAttributedString highlightMatchesForQuery:self.query
+                                                                             phraseIdentifier:@"text:"];
     return cellView;
 }
 

commit 4f59971856978d4064a9b5e3f89db80662c18804

Fix a bug introduced in commit 5edec84 that broke searching profile tags. Issue 11713

sources/iTermProfileSearchToken.m
@@ -127,7 +127,7 @@ NSString *const kTagRestrictionOperator = @"tag:";
 }
 
 - (BOOL)matchesAnyWordInTagWords:(NSArray *)tagWords {
-    if (_operator == nil || [_nonTagOperators containsObject:_operator]) {
+    if (_operator != nil && [_nonTagOperators containsObject:_operator]) {
         return NO;
     }
     return [self matchesAnyWordInWords:tagWords];