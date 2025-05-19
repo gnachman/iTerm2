7af994665794be3502b289629d17cb41ef6e218d
George Nachman <gnachman@gmail.com>
2025-05-19 13:07:40 -0700

    Add click to set instructions on image well

--- sources/iTermImageWell.m
+++ sources/iTermImageWell.m
@@ -8,13 +8,78 @@
 
 #import "iTermImageWell.h"
 
+@interface iTermImageWell () {
+    NSVisualEffectView *_effectView;
+    NSTextField       *_overlayLabel;
+}
+@end
+
 @implementation iTermImageWell
 
 - (void)awakeFromNib {
+    [super awakeFromNib];
     self.wantsLayer = YES;
     self.layer.borderColor = [[NSColor whiteColor] CGColor];
     self.layer.borderWidth = 2.0;
     self.layer.cornerRadius = 6.0;
+
+    _effectView = [[NSVisualEffectView alloc] initWithFrame:NSZeroRect];
+    _effectView.material = NSVisualEffectMaterialHUDWindow;
+    _effectView.blendingMode = NSVisualEffectBlendingModeWithinWindow;
+    _effectView.wantsLayer = YES;
+    _effectView.layer.cornerRadius = 4.0;
+    [self addSubview:_effectView];
+
+    _overlayLabel = [[NSTextField alloc] initWithFrame:NSZeroRect];
+    _overlayLabel.font = [NSFont systemFontOfSize:[NSFont systemFontSize]];
+    _overlayLabel.textColor = [NSColor textColor];
+    _overlayLabel.alignment = NSTextAlignmentCenter;
+    _overlayLabel.drawsBackground = NO;
+    _overlayLabel.bordered = NO;
+    _overlayLabel.editable = NO;
+    _overlayLabel.selectable = NO;
+    [self addSubview:_overlayLabel];
+
+    [self updateOverlayText];
+}
+
+- (void)layout {
+    [super layout];
+
+    const CGFloat paddingX = 4.0;
+    const CGFloat paddingY = 4.0;
+    const NSSize textSize = [_overlayLabel fittingSize];
+    const NSRect bounds = self.bounds;
+    const NSRect labelFrame = NSMakeRect((NSWidth(bounds) - textSize.width) / 2,
+                                   (NSHeight(bounds) - textSize.height) / 2,
+                                   textSize.width,
+                                   textSize.height);
+
+    const NSRect effectFrame = NSMakeRect(labelFrame.origin.x - paddingX,
+                                          labelFrame.origin.y - paddingY,
+                                          labelFrame.size.width + 2 * paddingX,
+                                          labelFrame.size.height + 2 * paddingY);
+
+    _overlayLabel.frame = labelFrame;
+    _effectView.frame = effectFrame;
+
+    _overlayLabel.hidden = bounds.size.width < 250;
+    _effectView.hidden = bounds.size.width < 250;
+}
+
+- (void)setImage:(NSImage *)image {
+    [super setImage:image];
+    [self updateOverlayText];
+}
+
+- (void)updateOverlayText {
+    if (self.image == nil) {
+        _overlayLabel.stringValue = @"No Image Selected\u2009—\u2009Click to set";
+    }
+    else {
+        _overlayLabel.stringValue = @"Click to change";
+    }
+    [self setNeedsLayout:YES];
 }
 
 - (BOOL)performDragOperation:(id<NSDraggingInfo>)draggingInfo {
@@ -56,3 +121,4 @@
 }
 
 @end
+
