e67582305dcf3ae5fd5c40c65904739b7c43cff7
George Nachman <gnachman@gmail.com>
2025-06-03 10:05:38 -0700

    Add add attachment button

--- sources/ChatInputView.swift
+++ sources/ChatInputView.swift
@@ -17,11 +17,18 @@ protocol ChatInputViewDelegate: AnyObject {
 //     SendButton
 @objc
 class ChatInputView: NSView, NSTextFieldDelegate {
+    struct Attachment {
+        var path: String
+    }
     private class ChatViewControllerInputStackView: NSStackView {}
     private class SendButton: NSButton { }
+    private class AddAttachmentButton: NSButton { }
+
     private let vev = NSVisualEffectView()
     private let inputTextFieldContainer = ChatInputTextFieldContainer()
     private var sendButton: SendButton!
+    private var attachments = [Attachment]()
+    private var addAttachmentButton: AddAttachmentButton!
 
     weak var delegate: ChatInputViewDelegate?
 
@@ -54,7 +61,26 @@ class ChatInputView: NSView, NSTextFieldDelegate {
         sendButton.setContentHuggingPriority(.defaultHigh, for: .horizontal)
         sendButton.setContentCompressionResistancePriority(.required, for: .horizontal)
 
-        let inputStack = ChatViewControllerInputStackView(views: [inputTextFieldContainer, sendButton])
+        if #available(macOS 11.0, *) {
+            if let image = NSImage(systemSymbolName: "plus", accessibilityDescription: "Attach files") {
+                addAttachmentButton = AddAttachmentButton(image: image, target: self, action: #selector(attachmentButtonClicked))
+                addAttachmentButton.imageScaling = .scaleProportionallyUpOrDown
+                addAttachmentButton.imagePosition = .imageOnly
+                addAttachmentButton.bezelStyle = .regularSquare
+                addAttachmentButton.isBordered = false
+                addAttachmentButton.setButtonType(.momentaryPushIn)
+                addAttachmentButton.toolTip = "Attach files"
+            } else {
+                addAttachmentButton = AddAttachmentButton(title: "+", target: self, action: #selector(attachmentButtonClicked))
+            }
+        } else {
+            addAttachmentButton = AddAttachmentButton(title: "+", target: self, action: #selector(attachmentButtonClicked))
+        }
+        addAttachmentButton.translatesAutoresizingMaskIntoConstraints = false
+        addAttachmentButton.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        addAttachmentButton.setContentCompressionResistancePriority(.required, for: .horizontal)
+
+        let inputStack = ChatViewControllerInputStackView(views: [addAttachmentButton, inputTextFieldContainer, sendButton])
         inputStack.orientation = .horizontal
         inputStack.spacing = 8
         inputStack.translatesAutoresizingMaskIntoConstraints = false
@@ -86,8 +112,6 @@ class ChatInputView: NSView, NSTextFieldDelegate {
             vev.rightAnchor.constraint(equalTo: rightAnchor),
             vev.bottomAnchor.constraint(equalTo: bottomAnchor),
             vev.heightAnchor.constraint(equalTo: heightAnchor),
-
-            inputTextFieldContainer.leadingAnchor.constraint(equalTo: inputStack.leadingAnchor),
         ])
     }
 
@@ -98,6 +122,28 @@ class ChatInputView: NSView, NSTextFieldDelegate {
     @objc
     func sendButtonClicked(_ sender: Any) {
         delegate?.sendButtonClicked(text: inputTextFieldContainer.stringValue)
+    }
+
+    @objc private func attachmentButtonClicked() {
+        guard let window else {
+            return
+        }
+        let openPanel = NSOpenPanel()
+        openPanel.allowsMultipleSelection = true
+        openPanel.canChooseDirectories = false
+        openPanel.canChooseFiles = true
+
+        openPanel.beginSheetModal(for: window) { [weak self] response in
+            guard response == .OK, let self else {
+                return
+            }
+            for url in openPanel.urls {
+                let path = url.path
+                if !attachments.contains(where: { $0.path == path }) {
+                    attachments.append(Attachment(path: path))
+                }
+            }
+        }
     }
 
     var isEnabled: Bool {
