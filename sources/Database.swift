0a58f9e012aa4706d46dd01f393b93cbcff5c17e
George Nachman <gnachman@gmail.com>
2025-06-19 23:11:27 -0700

    Reorg some database code

---
+++ sources/Database.swift
@@ -0,0 +1,25 @@
+//
+//  Database.swift
+//  iTerm2
+//
+//  Created by George Nachman on 6/19/25.
+//
+
+protocol iTermDatabaseInitializable {
+    init?(dbResultSet: iTermDatabaseResultSet)
+}
+
+struct Migration {
+    var query: String
+    var args: [String]
+}
+
+protocol iTermDatabaseElement: iTermDatabaseInitializable {
+    static func schema() -> String
+    static func fetchAllQuery() -> String
+    static func tableInfoQuery() -> String
+    func appendQuery() -> (String, [Any])
+    func updateQuery() -> (String, [Any])
+    func removeQuery() -> (String, [Any])
+}
+
