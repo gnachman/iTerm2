9d5253354fb4b64ba8f1feb98f1228d3fe0273c6
George Nachman <gnachman@gmail.com>
2025-07-01 22:12:52 -0700

    rm dead code

--- sources/iTermTabBarAccessoryViewController.m
+++ sources/iTermTabBarAccessoryViewController.m
@@ -8,53 +8,16 @@
 #import "iTermTabBarAccessoryViewController.h"
 #import "iTermAdvancedSettingsModel.h"
 
-// TODO: FB7781183
-@interface iTermHackAroundBigSurBugView: NSView
-- (instancetype)init API_DEPRECATED("Bug supposedly fixed in macOS 12.", macosx(10.16, 11));
-@end
-
-@implementation iTermHackAroundBigSurBugView: NSView
-
-- (instancetype)init {
-    return [super init];
-}
-- (BOOL)isFlipped {
-    return YES;
-}
-- (void)resizeSubviewsWithOldSize:(NSSize)oldSize {
-    [self layoutSubviews];
-}
-- (void)addSubview:(NSView *)view {
-    [super addSubview:view];
-    [self layoutSubviews];
-}
-- (void)layoutSubviews {
-    for (NSView *view in self.subviews) {
-        view.frame = NSMakeRect(0, 0, self.frame.size.width, self.frame.size.height - 4);
-    }
-}
-@end
-
 @interface iTermTabBarAccessoryViewController ()
 @end
 
 @implementation iTermTabBarAccessoryViewController {
     NSView *_view;
-    iTermHackAroundBigSurBugView *_hack;
 }
 
 - (instancetype)initWithView:(NSView *)view {
     self = [super initWithNibName:nil bundle:nil];
     if (self) {
-        if (@available(macOS 12.0, *)) {
-            // Apple says they fixed the bug in Monterey (macOS 12)
-        } else {
-            if ([iTermAdvancedSettingsModel allowTabbarInTitlebarAccessoryBigSur]) {
-                if (@available(macOS 10.16, *)) {
-                    _hack = [[iTermHackAroundBigSurBugView alloc] init];
-                }
-            }
-        }
         _view = view;
     }
     return self;
@@ -72,12 +35,6 @@
 }
 
 - (void)loadView {
-    if (_hack) {
-        _hack.frame = NSMakeRect(0, 0, _view.frame.size.width, _view.frame.size.height + 4);
-        [_hack addSubview:_view];
-        self.view = _hack;
-        return;
-    }
     self.view = _view;
 }
 
