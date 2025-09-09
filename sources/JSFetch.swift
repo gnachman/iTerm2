3db0a5db75d3e222f1bc52059eaec782960d2ec7
George Nachman <gnachman@gmail.com>
2025-09-08 23:04:12 -0700

    Fix data race on AI NSURLSessionDataTask

--- sources/JSFetch.swift
+++ sources/JSFetch.swift
@@ -11,7 +11,7 @@ import JavaScriptCore
 class PluginClient {
     static let instance = PluginClient()
     private var session: URLSession?
-    private var task: URLSessionDataTask?
+    private let task = MutableAtomicObject<URLSessionDataTask?>(nil)
 
     private class HTTPStreamDelegate: NSObject, URLSessionDataDelegate {
         var receivedData = Data()
@@ -63,8 +63,10 @@ class PluginClient {
     }
 
     func cancel() {
-        task?.cancel()
-        task = nil
+        task.mutableAccess { currentTask in
+            currentTask?.cancel()
+            currentTask = nil
+        }
     }
 
     func call<RequestType: Codable & CustomDebugStringConvertible,
@@ -129,7 +131,9 @@ class PluginClient {
         config.timeoutIntervalForResource =  iTermPreferences.double(forKey: kPreferenceKeyAITimeout)
         let delegate = HTTPStreamDelegate { [weak self] first, second in
             if second != nil {
-                self?.task = nil
+                self?.task.mutableAccess { currentTask in
+                    currentTask = nil
+                }
             }
             callback(first, second)
         }
@@ -139,9 +143,12 @@ class PluginClient {
         session = URLSession(configuration: config,
                              delegate: delegate,
                              delegateQueue: nil)
-        self.task = session?.dataTask(with: request)
+        let newTask = session?.dataTask(with: request)
+        task.mutableAccess { currentTask in
+            currentTask = newTask
+        }
         DLog("resume session")
-        self.task?.resume()
+        newTask?.resume()
     }
 
     private func registerFunctions(context: JSContext, stream: ((String) -> ())?) {
