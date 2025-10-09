de397b45b0c46bc0a224300876f1a9370ebe50ff
George Nachman <gnachman@gmail.com>
2025-10-09 09:11:01 -0700

    Fix crash in browser smart selection

--- sources/MultiCursor/NSRange+MultiCursor.swift
+++ sources/MultiCursor/NSRange+MultiCursor.swift
@@ -12,6 +12,20 @@ extension NSRange {
         return NSRange(location: location + delta, length: length)
     }
 
+    var droppingFirst: NSRange? {
+        if length == 0 {
+            return nil
+        }
+        return NSRange(location: location + 1, length: length - 1)
+    }
+
+    var droppingLast: NSRange? {
+        if length == 0 {
+            return nil
+        }
+        return NSRange(location: location, length: length - 1)
+    }
+
     // Not inclusive of `to`
     init(from: Int, to: Int) {
         self.init(location: min(from, to), length: max(from, to) -  min(from, to))
