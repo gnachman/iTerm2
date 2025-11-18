gnachman/iTerm2

commit f40c60e71d1eeb33fdf346d1963060b1a19481d5

Fix a bug where images disappear when folding and unfolding a region containing an image; also, when folding, restarting & restoring state, and then unfolding would also lose the image. Issue 12547

sources/FoldMark.swift
@@ -12,6 +12,7 @@ protocol FoldMarkReading: AnyObject, iTermMarkProtocol {
     var savedLines: [ScreenCharArray]? { get }
     var savedITOs: [SavedIntervalTreeObject]? { get }
     var contentString: String { get }
+    var imageCodes: Set<Int32> { get }
 }
 
 @objc(iTermSavedIntervalTreeObject)
@@ -112,6 +113,12 @@ class SavedIntervalTreeObject: NSObject {
         case object = "object"
         case start = "start"
         case end = "end"
+    }
+
+    init(_ source: SavedIntervalTreeObject) {
+        start = source.start
+        end = source.end
+        object = source.object
     }
 
     init?(dictionaryValue: [AnyHashable : Any]) {
@@ -235,18 +242,30 @@ class FoldMark: iTermMark, FoldMarkReading {
     let savedLines: [ScreenCharArray]?
     let savedITOs: [SavedIntervalTreeObject]?
     private let promptLength: Int
+    let imageCodes: Set<Int32>
 
     private static let savedLinesKey = "saved lines"
     private static let savedITOsKey = "saved ITOs"
     private static let promptLengthKey = "prompt length"
+    private static let imageCodesKey = "image codes"
 
-    @objc(initWithLines:savedITOs:promptLength:)
+    @objc(initWithLines:savedITOs:promptLength:imageCodes:)
     init(savedLines: [ScreenCharArray]?,
          savedITOs: [SavedIntervalTreeObject],
-         promptLength: Int) {
+         promptLength: Int,
+         imageCodes: Set<Int32>) {
         self.savedLines = savedLines
         self.savedITOs = savedITOs
         self.promptLength = promptLength
+        self.imageCodes = imageCodes
+        super.init()
+    }
+
+    init(_ source: FoldMark) {
+        savedLines = source.savedLines?.map { $0.clone() }
+        savedITOs = source.savedITOs?.map { SavedIntervalTreeObject($0) }
+        promptLength = source.promptLength
+        imageCodes = source.imageCodes
         super.init()
     }
 
@@ -258,13 +277,15 @@ class FoldMark: iTermMark, FoldMarkReading {
             SavedIntervalTreeObject(dictionaryValue: dict)
         })
         promptLength = (dict[Self.promptLengthKey] as? Int) ?? 0
+        imageCodes = Set((dict[Self.imageCodesKey] as? [Int32]) ?? [])
         super.init(dictionary: dict)
     }
 
     override func dictionaryValue() -> [AnyHashable : Any]! {
         let unsafeDict: [String: Any?] = [ Self.savedLinesKey: savedLines?.map { $0.dictionaryValue },
                                            Self.savedITOsKey: savedITOs?.map { $0.dictionaryValue },
-                                           Self.promptLengthKey: promptLength ]
+                                           Self.promptLengthKey: promptLength,
+                                           Self.imageCodesKey: Array(imageCodes) ]
         return unsafeDict.filter { element in
             element.value != nil
         } as [AnyHashable : Any]
@@ -274,5 +295,41 @@ class FoldMark: iTermMark, FoldMarkReading {
         (savedLines ?? []).dropFirst(promptLength).map {
             $0.stringValueIncludingNewline
         }.joined(separator: "")
+    }
+
+    @objc(foldedContentUsesImageWithCode:)
+    func foldedContentUsesImage(code: Int32) -> Bool {
+        return imageCodes.contains(code)
+    }
+
+    // Call after restoring state to ensure images that are in the fold (and
+    // will therefore not have marks) do not get garbage collected.
+    @objc(recursivelyClearProvisionalFlagForSavedImageMarks)
+    func recursivelyClearProvisionalFlagForSavedImageMarks() {
+        for code in imageCodes {
+            ScreenCharClearProvisionalFlagForImageWithCode(code)
+        }
+    }
+
+    // The default implementation serializes and deserializes. That causes
+    // duplicate image marks to be created. That is a problem because the
+    // lifecycle of the progenitor image mark is used to determine when to
+    // release images. If we do not keep a strong reference to the image mark,
+    // the image will be released even though we have a copy of the image mark.
+    // So instead, we use cloning functionality that preserves references to
+    // marks.
+    override func copy(with zone: NSZone? = nil) -> Any {
+        return FoldMark(self)
+    }
+
+    // Since we overrode copyWithZone we must ensure that the fold mark's
+    // doppelganger only references mark doppelgangers to avoid data races.
+    override func becomeDoppelganger() {
+        super.becomeDoppelganger()
+        if let savedITOs {
+            for ito in savedITOs {
+                ito.object = ito.object.doppelganger()
+            }
+        }
     }
 }

sources/iTermMark.h
@@ -32,4 +32,7 @@
 // PTYAnnotation (which needs a delegate to function).
 + (id<IntervalTreeObject>)intervalTreeObjectWithDictionaryWithTypeInformation:(NSDictionary *)dict;
 
+// This is here for subclasses to override. They should always call it.
+- (void)becomeDoppelganger;
+
 @end

sources/iTermMark.m
@@ -69,11 +69,15 @@
         assert(!_isDoppelganger);
         if (!_doppelganger) {
             _doppelganger = [self copy];
-            _doppelganger->_isDoppelganger = YES;
-            _doppelganger->_progenitor = self;
+            [_doppelganger becomeDoppelganger];
         }
         return _doppelganger;
     }
+}
+
+- (void)becomeDoppelganger {
+    _isDoppelganger = YES;
+    _progenitor = self;
 }
 
 - (NSString *)shortDebugDescription {

sources/VT100ScreenMutableState.m
@@ -3992,6 +3992,25 @@ void VT100ScreenEraseCell(screen_char_t *sct,
             blockMarks:nil];
 }
 
+- (NSSet<NSNumber *> *)imageCodesUsedInAbsRange:(VT100GridAbsCoordRange)absRange {
+    const VT100GridCoordRange range = VT100GridCoordRangeFromAbsCoordRange(absRange, self.cumulativeScrollbackOverflow);
+    VT100GridRange gridRange = VT100GridRangeMake(range.start.y, range.end.y - range.start.y + 1);
+
+    NSArray<id<iTermFoldMarkReading>> *foldMarks = [self foldMarksInRange:gridRange];
+
+    // Image marks occur below the image so search one extra line.
+    gridRange.length += 1;
+    NSArray<id<iTermImageMarkReading>> *imageMarks = [self imageMarksInRange:gridRange];
+
+    NSSet<NSNumber *> *imageMarkCodes = [NSSet setWithArray:[imageMarks mapWithBlock:^id(id<iTermImageMarkReading> imageMark) {
+        return imageMark.imageCode;
+    }]];
+    NSSet<NSNumber *> *foldedImageCodes = [NSSet setWithArray:[foldMarks flatMapWithBlock:^id(id<iTermFoldMarkReading> foldMark) {
+        return [foldMark imageCodes].allObjects;
+    }]];
+    return [imageMarkCodes setByAddingObjectsFromSet:foldedImageCodes];
+}
+
 - (void)replaceRange:(VT100GridAbsCoordRange)absRange
            withLines:(NSArray<ScreenCharArray *> *)lines
         promptLength:(NSInteger)promptLength
@@ -3999,6 +4018,7 @@ void VT100ScreenEraseCell(screen_char_t *sct,
     DLog(@"replaceRange:withLine: (fold)");
     NSArray<iTermSavedIntervalTreeObject *> *savedITOs = nil;
     NSArray<ScreenCharArray *> *savedLines = nil;
+    NSSet<NSNumber *> *imageCodes = [self imageCodesUsedInAbsRange:absRange];
     const VT100GridAbsCoordRange markRange = [self replaceRange:absRange
                                                       withLines:lines
                                      removedIntervalTreeObjects:&savedITOs
@@ -4028,7 +4048,8 @@ void VT100ScreenEraseCell(screen_char_t *sct,
         Interval *interval = [self intervalForGridAbsCoordRange:markRange];
         iTermFoldMark *mark = [[iTermFoldMark alloc] initWithLines:savedLines
                                                          savedITOs:savedITOs
-                                                      promptLength:promptLength];
+                                                      promptLength:promptLength
+                                                        imageCodes:imageCodes];
         [self.mutableIntervalTree addObject:mark withInterval:interval];
     }
     if (self.config.useLineStyleMarks) {
@@ -5214,6 +5235,9 @@ lengthExcludingInBandSignaling:data.length
             } else if ([object isKindOfClass:[iTermImageMark class]]) {
                 id<iTermImageMarkReading> imageMark = (id<iTermImageMarkReading>)object;
                 ScreenCharClearProvisionalFlagForImageWithCode(imageMark.imageCode.intValue);
+            } else if ([object isKindOfClass:[iTermFoldMark class]]) {
+                iTermFoldMark *foldMark = (iTermFoldMark *)object;
+                [foldMark recursivelyClearProvisionalFlagForSavedImageMarks];
             }
         }
     }

sources/VT100ScreenState.h
@@ -56,6 +56,7 @@ extern NSString *const kScreenStateKittyImageDrawsKey;
 @class iTermMarkCache;
 @protocol iTermBlockMarkReading;
 @class VT100ScreenMutableState;
+@protocol iTermImageMarkReading;
 
 @protocol VT100ScreenState<NSObject>
 
@@ -336,6 +337,7 @@ extern NSString *const kScreenStateKittyImageDrawsKey;
 - (BOOL)haveFoldsInRange:(NSRange)absLineRange;
 - (NSIndexSet *)foldsInRange:(VT100GridRange)gridRange;
 - (NSArray<id<iTermFoldMarkReading>> *)foldMarksInRange:(VT100GridRange)range;
+- (NSArray<id<iTermImageMarkReading>> *)imageMarksInRange:(VT100GridRange)range;
 
 // WARNING - If you add any new APIs that return interval tree objects update VT100ScreenStateSanitizingAdapter
 

sources/VT100ScreenState.m
@@ -327,6 +327,12 @@ NSString *VT100ScreenTerminalStateKeyPath = @"Path";
     }
 }
 
+static NSRange NSRangeFromBounds(NSInteger lowerBound, NSInteger upperBound) {
+    NSInteger location = MAX(0, lowerBound);
+    NSInteger length = MAX(0, upperBound - location);
+    return NSMakeRange(location, length);
+}
+
 // Search cells backwards from the image mark to see if any of it is still referenced.
 - (BOOL)imageInUse:(iTermImageMark *)mark above:(long long)aboveAbsY searchHeight:(int)searchHeight {
     const int code = mark.imageCode.intValue;
@@ -339,6 +345,10 @@ NSString *VT100ScreenTerminalStateKeyPath = @"Path";
     if (aboveY < 0) {
         DLog(@"Image has scrolled off");
         return NO;
+    }
+    if ([self anyFoldInAbsRange:NSRangeFromBounds(aboveAbsY - searchHeight, aboveAbsY)
+              containsImageMark:mark]) {
+        return YES;
     }
     const long long startY = MAX(0, aboveY - imageInfo.size.height);
     __block BOOL found = NO;
@@ -354,6 +364,16 @@ NSString *VT100ScreenTerminalStateKeyPath = @"Path";
         }
     }];
     return found;
+}
+
+- (BOOL)anyFoldInAbsRange:(NSRange)lineRange containsImageMark:(iTermImageMark *)imageMark {
+    if (!imageMark.imageCode) {
+        return NO;
+    }
+    NSArray<iTermFoldMark *> *foldMarks = [self foldMarksInRange:lineRange max:NSUIntegerMax];
+    return [foldMarks anyWithBlock:^BOOL(iTermFoldMark *foldMark) {
+        return [foldMark foldedContentUsesImageWithCode:imageMark.imageCode.intValue];
+    }];
 }
 
 - (BOOL)screenCharArray:(ScreenCharArray *)sca containsImageCode:(int)code {
@@ -621,6 +641,10 @@ NSString *VT100ScreenTerminalStateKeyPath = @"Path";
 
 - (NSArray<iTermFoldMark *> *)foldMarksInRange:(NSRange)absLineRange max:(NSUInteger)maxCount {
     return [self marksInRange:absLineRange max:maxCount ofClass:[iTermFoldMark class]];
+}
+
+- (NSArray<iTermImageMark *> *)imageMarksInRange:(NSRange)absLineRange max:(NSUInteger)maxCount {
+    return [self marksInRange:absLineRange max:maxCount ofClass:[iTermImageMark class]];
 }
 
 - (NSArray *)marksInRange:(NSRange)absLineRange
@@ -985,6 +1009,11 @@ NSString *VT100ScreenTerminalStateKeyPath = @"Path";
 
 - (NSArray<id<iTermFoldMarkReading>> *)foldMarksInRange:(VT100GridRange)range {
     return [self foldMarksInRange:NSMakeRange(MAX(0, range.location) + self.cumulativeScrollbackOverflow, range.length)
+                              max:NSUIntegerMax];
+}
+
+- (NSArray<id<iTermImageMarkReading>> *)imageMarksInRange:(VT100GridRange)range {
+    return [self imageMarksInRange:NSMakeRange(MAX(0, range.location) + self.cumulativeScrollbackOverflow, range.length)
                               max:NSUIntegerMax];
 }
 

sources/iTerm2SharedARC-Bridging-Header.h
@@ -44,6 +44,7 @@
 #import "iTermGlobalSearchResult.h"
 #import "iTermHistogram.h"
 #import "iTermImageInfo.h"
+#import "iTermImageMark.h"
 #import "iTermIndicatorsHelper.h"
 #import "iTermKeyboardNavigatableTableView.h"
 #import "iTermLogoGenerator.h"

sources/iTermImageMark.h
@@ -15,7 +15,7 @@
 @end
 
 // Invisible marks used to record where images are located so they can be freed.
-@interface iTermImageMark : iTermMark
+@interface iTermImageMark : iTermMark<iTermImageMarkReading>
 - (instancetype)initWithImageCode:(NSNumber *)imageCode NS_DESIGNATED_INITIALIZER;
 - (instancetype)init NS_UNAVAILABLE;
 @property(nonatomic, strong, readonly) NSNumber *imageCode;

commit ffc207e733320d5623f32f6c961a8f19826c9b37

Fix crash introduced by f40c60e71d1eeb33fdf346d1963060b1a19481d5 that incorrectly set the progenitor of marks

sources/FoldMark.swift
@@ -324,8 +324,8 @@ class FoldMark: iTermMark, FoldMarkReading {
 
     // Since we overrode copyWithZone we must ensure that the fold mark's
     // doppelganger only references mark doppelgangers to avoid data races.
-    override func becomeDoppelganger() {
-        super.becomeDoppelganger()
+    override func becomeDoppelganger(withProgenitor progenitor: iTermMark) {
+        super.becomeDoppelganger(withProgenitor: progenitor)
         if let savedITOs {
             for ito in savedITOs {
                 ito.object = ito.object.doppelganger()

sources/iTermMark.h
@@ -33,6 +33,6 @@
 + (id<IntervalTreeObject>)intervalTreeObjectWithDictionaryWithTypeInformation:(NSDictionary *)dict;
 
 // This is here for subclasses to override. They should always call it.
-- (void)becomeDoppelganger;
+- (void)becomeDoppelgangerWithProgenitor:(iTermMark *)progenitor;
 
 @end

sources/iTermMark.m
@@ -69,15 +69,15 @@
         assert(!_isDoppelganger);
         if (!_doppelganger) {
             _doppelganger = [self copy];
-            [_doppelganger becomeDoppelganger];
+            [_doppelganger becomeDoppelgangerWithProgenitor:self];
         }
         return _doppelganger;
     }
 }
 
-- (void)becomeDoppelganger {
+- (void)becomeDoppelgangerWithProgenitor:(iTermMark *)progenitor {
     _isDoppelganger = YES;
-    _progenitor = self;
+    _progenitor = progenitor;
 }
 
 - (NSString *)shortDebugDescription {