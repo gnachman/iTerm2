df4c424973f15c728fc4a1868a7c92075e977d9d
George Nachman <gnachman@gmail.com>
2026-09-08 13:25:44 -0700

    Fix python docs build scripts for new path to MainMenu.xib

--- api/library/python/iterm2/docs/build.sh
+++ api/library/python/iterm2/docs/build.sh
@@ -3,7 +3,7 @@ set -x
 rm -rf _build
 mkdir _build
 
-./generate_menu_ids.py "$PWD"/../../../../../Interfaces/MainMenu.xib > menu_ids.rst
+./generate_menu_ids.py "$PWD"/../../../../../sources/MainMenu/MainMenu.xib > menu_ids.rst
 make html
 
 cp _static/css/custom.css _build/html/_static/alabaster.css
