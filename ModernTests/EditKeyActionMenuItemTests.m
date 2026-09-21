gnachman/iTerm2

commit 817462d8d9fd0e2ad12b72896b0806d0f04dfd14

Improvements to menu item selection, and add Select Menu Item to pointer bindings. Issue 9781

SearchableComboListView/SearchableComboListView.framework/Versions/A/_CodeSignature/CodeResources
@@ -10,15 +10,15 @@
 		</data>
 		<key>Resources/SearchableComboView.nib/keyedobjects-101300.nib</key>
 		<data>
-		7k90uzs/PMpLTa9H+0pq1LZrgNY=
+		zF6Hh3/ccI+4pV+nXYPo/Q9WoiI=
 		</data>
 		<key>Resources/SearchableComboView.nib/keyedobjects-110000.nib</key>
 		<data>
-		eMZ/0uDro3Yr5m5jt4Ij29RV2Xs=
+		HLN8LbTXA4q9tta87fEN8eGy0j0=
 		</data>
 		<key>Resources/SearchableComboView.nib/keyedobjects.nib</key>
 		<data>
-		aNA8NJOj9MPMcFG/sa6rosuWo74=
+		dqE5HdJFSv1ZCSQ9H+cdXnZnnbM=
 		</data>
 	</dict>
 	<key>files2</key>
@@ -27,7 +27,7 @@
 		<dict>
 			<key>hash2</key>
 			<data>
-			xtSpm0pZU6gByaHkThjF6pVjJIcXoHYB9P8l6nrsn6w=
+			cBfMDQSkJEc5cWFn/9gZg+/kZi75bCvC1Hr9HByNzqc=
 			</data>
 		</dict>
 		<key>Headers/SearchableComboListView.h</key>
@@ -41,28 +41,28 @@
 		<dict>
 			<key>hash2</key>
 			<data>
-			y0z37rmNmF+fqiYANJ/6OA2MdlhVybZvbMX5in7rUfk=
+			9eEd87O75yqpJsZKm6EQT2KpYws71vgFsWFmAj5LNEo=
 			</data>
 		</dict>
 		<key>Modules/SearchableComboListView.swiftmodule/Project/arm64.swiftsourceinfo</key>
 		<dict>
 			<key>hash2</key>
 			<data>
-			y0z37rmNmF+fqiYANJ/6OA2MdlhVybZvbMX5in7rUfk=
+			9eEd87O75yqpJsZKm6EQT2KpYws71vgFsWFmAj5LNEo=
 			</data>
 		</dict>
 		<key>Modules/SearchableComboListView.swiftmodule/Project/x86_64-apple-macos.swiftsourceinfo</key>
 		<dict>
 			<key>hash2</key>
 			<data>
-			F3m4ZiEG4fKXynl0OwJOuHlOMZwgF6ZY/0HEsh63XCI=
+			iZ4bUCgnHQd/XTRUjp7d84Ol6uYQrw3nCm6zOZYgGAs=
 			</data>
 		</dict>
 		<key>Modules/SearchableComboListView.swiftmodule/Project/x86_64.swiftsourceinfo</key>
 		<dict>
 			<key>hash2</key>
 			<data>
-			F3m4ZiEG4fKXynl0OwJOuHlOMZwgF6ZY/0HEsh63XCI=
+			iZ4bUCgnHQd/XTRUjp7d84Ol6uYQrw3nCm6zOZYgGAs=
 			</data>
 		</dict>
 		<key>Modules/SearchableComboListView.swiftmodule/arm64-apple-macos.swiftdoc</key>
@@ -76,7 +76,7 @@
 		<dict>
 			<key>hash2</key>
 			<data>
-			oKjLvrdNRI3FBSzsEPBhEoG6OSd+c6uOT8SWLgR0bxU=
+			5esMdVq8Y7r8O/MxVK8YbS5WiMbz00TG0mDCDD8N6zM=
 			</data>
 		</dict>
 		<key>Modules/SearchableComboListView.swiftmodule/arm64.swiftdoc</key>
@@ -90,7 +90,7 @@
 		<dict>
 			<key>hash2</key>
 			<data>
-			oKjLvrdNRI3FBSzsEPBhEoG6OSd+c6uOT8SWLgR0bxU=
+			5esMdVq8Y7r8O/MxVK8YbS5WiMbz00TG0mDCDD8N6zM=
 			</data>
 		</dict>
 		<key>Modules/SearchableComboListView.swiftmodule/x86_64-apple-macos.swiftdoc</key>
@@ -104,7 +104,7 @@
 		<dict>
 			<key>hash2</key>
 			<data>
-			Ca9U42/K4AVaH1L1UG56KADvOhsIEykKD0zrmNrWSCw=
+			CQT2M1TnSC+KoKUvkvxTa1M7v62Zm4N4SoJftgRCw3w=
 			</data>
 		</dict>
 		<key>Modules/SearchableComboListView.swiftmodule/x86_64.swiftdoc</key>
@@ -118,7 +118,7 @@
 		<dict>
 			<key>hash2</key>
 			<data>
-			Ca9U42/K4AVaH1L1UG56KADvOhsIEykKD0zrmNrWSCw=
+			CQT2M1TnSC+KoKUvkvxTa1M7v62Zm4N4SoJftgRCw3w=
 			</data>
 		</dict>
 		<key>Modules/module.modulemap</key>
@@ -139,21 +139,21 @@
 		<dict>
 			<key>hash2</key>
 			<data>
-			AdEkNU1r0cZMedfufBWXoq1N00LRmTU1hkw7f7G9dpI=
+			2ctF2sYmXhT4CRXg+gRpub++FL0l563XSBZgHEsRq14=
 			</data>
 		</dict>
 		<key>Resources/SearchableComboView.nib/keyedobjects-110000.nib</key>
 		<dict>
 			<key>hash2</key>
 			<data>
-			yvT9xW4b9vA6Tj2tBB83YD0y4KrlucuIE8mB/TcvYAk=
+			TZpx+FbgUJVvM5DO2n3cN/e58I6VXUan6COqLxS+W1U=
 			</data>
 		</dict>
 		<key>Resources/SearchableComboView.nib/keyedobjects.nib</key>
 		<dict>
 			<key>hash2</key>
 			<data>
-			qXgRVdjPyJAiZpl3pS4qvLbPweb4vEIsBBKIkdnnd7A=
+			faSW2M8tndDXqZ6yp06IT95H4E9/ntJF1HaSbNTccao=
 			</data>
 		</dict>
 	</dict>

SearchableComboListView/SearchableComboView/SearchableComboTableViewController.swift
@@ -13,6 +13,8 @@ protocol MouseObservingTableView {
 }
 
 fileprivate class RowView: NSTableRowView {
+    private var backgroundView: NSVisualEffectView?
+
     private var enclosingTableView: (NSTableView & MouseObservingTableView)? {
         var view = superview
         while view != nil {
@@ -35,6 +37,31 @@ fileprivate class RowView: NSTableRowView {
 
         return myFrameInWindowCoords.contains(mouseLocationInWindowCoords)
     }
+
+    func updateBackground() {
+        let hasBackgroundView = (backgroundView != nil)
+        if hasBackgroundView == isGroupRowStyle {
+            return
+        }
+        backgroundView?.removeFromSuperview()
+        backgroundView = nil
+        if !isGroupRowStyle {
+            return
+        }
+        backgroundView = NSVisualEffectView()
+        guard let backgroundView = backgroundView else {
+            return
+        }
+        backgroundView.material = .menu
+        backgroundView.blendingMode = .behindWindow
+        backgroundView.frame = bounds
+        backgroundView.autoresizingMask = [.width, .height]
+        if let firstSubview = subviews.first {
+            addSubview(backgroundView, positioned: .below, relativeTo: firstSubview)
+        } else {
+            addSubview(backgroundView)
+        }
+    }
 }
 
 protocol SearchableComboTableViewControllerDelegate: NSObjectProtocol {
@@ -52,6 +79,52 @@ protocol SearchableComboTableViewControllerDelegate: NSObjectProtocol {
 
 class SearchableComboTableViewController: NSViewController {
     weak var delegate: SearchableComboTableViewControllerDelegate?
+    lazy var widestItemWidth: CGFloat = {
+        if unfilteredRows.isEmpty {
+            return 0
+        }
+        guard let sampleRowView = tableView.rowView(atRow: 0, makeIfNecessary: true) else {
+            return 0
+        }
+        let sampleColumnWidths = tableView.tableColumns.map { $0.width }.reduce(0.0) { $0 + $1 }
+        let overhead = sampleRowView.bounds.width - sampleColumnWidths
+        let widths = unfilteredRows.enumerated().map { (index, _) -> CGFloat in
+            return sumOfColumnWidths(row: index) + overhead
+        }
+        // I have no idea where 16 comes from. It's necessary on macOS 12 to prevent TableView.tile()
+        // from making the table view wider than the scrollview. It seems to want this much extra space
+        // for the last column.
+        return (widths.max() ?? 0) + 16
+    }()
+
+    // This shouldn't be necessary but tile() always grows the tableview to be larger than the
+    // scrollview on macOS 12. I suspect a bug in NSTableView but 🤷
+    func updateColumnWidths() {
+        let saved = filter
+        // Remove the filter temporarily so we can measure the max width.
+        filter = ""
+        defer {
+            filter = saved
+        }
+        let widths = unfilteredRows.enumerated().map { (index, _) -> CGFloat in
+            return sumOfColumnWidths(row: index)
+        }
+        if let maxWidth = widths.max() {
+            tableView.tableColumns[1].width = maxWidth
+        }
+    }
+
+    private func sumOfColumnWidths(row: Int) -> CGFloat {
+        let columnWidths = tableView.tableColumns.map { column -> CGFloat in
+            let view = tableView(tableView, viewFor: column, row: row) as? NSTextField
+            view?.sizeToFit()
+            return view?.bounds.width ?? 0
+        }
+        return columnWidths.reduce(0.0) { accumulator, value in
+            return accumulator + value
+        }
+    }
+
     private struct Query {
         let queryTokens: [String]
         init(_ query: String) {
@@ -181,7 +254,7 @@ class SearchableComboTableViewController: NSViewController {
         guard filteredRows.count > 0 else {
             return 0
         }
-        return tableView.frameOfCell(atColumn: 0, row: filteredRows.count - 1).maxY
+        return tableView.fittingSize.height
     }
 
     // MARK:- Initializers
@@ -338,7 +411,7 @@ class SearchableComboTableViewController: NSViewController {
 
     private func newGroupLabelTextField(_ value: String) -> NSTextField {
         let identifier = NSUserInterfaceItemIdentifier("SearchableComboViewGroupLabelCell")
-        let font = NSFont.systemFont(ofSize: groupLabelFontSize)
+        let font = NSFont.boldSystemFont(ofSize: groupLabelFontSize)
         if let textField = tableView.makeView(withIdentifier: identifier, owner: self) as? NSTextField {
             textField.font = font
             textField.stringValue = value
@@ -460,9 +533,7 @@ extension SearchableComboTableViewController: NSTableViewDataSource {
     }
 
     public func tableView(_ tableView: NSTableView, didAdd rowView: NSTableRowView, forRow row: Int) {
-        if self.tableView(tableView, isGroupRow: row) {
-            rowView.backgroundColor = NSColor.underPageBackgroundColor
-        }
+        (rowView as? RowView)?.updateBackground()
     }
 
     public func tableView(_ tableView: NSTableView, isGroupRow row: Int) -> Bool {
@@ -495,6 +566,12 @@ extension SearchableComboTableViewController: NSTableViewDelegate {
         selectedTag = filteredRows[row].tag
         delegate?.searchableComboTableViewController(self, didSelectItem: itemWithTag(selectedTag))
     }
+
+    public func tableView(_ tableView: NSTableView, selectionIndexesForProposedSelection proposedSelectionIndexes: IndexSet) -> IndexSet {
+        return proposedSelectionIndexes.filteredIndexSet { index in
+            return filteredRows[index].isSelectable
+        }
+    }
 }
 
 
@@ -506,6 +583,12 @@ extension SearchableComboTableViewController: SearchableComboTableViewDelegate {
     }
 
     func searchableComboTableView(_ tableView: SearchableComboTableView, didClickRow row: Int) {
+        guard row >= 0 && row < filteredRows.count else {
+            return
+        }
+        guard filteredRows[row].isSelectable else {
+            return
+        }
         select(row)
     }
     func searchableComboTableView(_ tableView: SearchableComboTableView,

SearchableComboListView/SearchableComboView/SearchableComboView.swift
@@ -34,21 +34,31 @@ public class SearchableComboViewGroup: NSObject {
 
 @objc(iTermSearchableComboViewItem)
 public class SearchableComboViewItem: NSObject {
-    let label: String
-    let tag: Int
-    let labelTokens: [String]
-    weak var group: SearchableComboViewGroup?
+    public let label: String
+    public let tag: Int
+    public let identifier: String?
+    public let labelTokens: [String]
+    internal(set) public weak var group: SearchableComboViewGroup?
 
     @objc(initWithLabel:tag:)
     public init(_ label: String, tag: Int) {
         self.label = label
         self.tag = tag
+        self.identifier = nil
+        labelTokens = label.tokens
+    }
+
+    @objc(initWithLabel:tag:identifier:)
+    public init(_ label: String, tag: Int, identifier: String?) {
+        self.label = label
+        self.tag = tag
+        self.identifier = identifier
         labelTokens = label.tokens
     }
 }
 
 @objc(iTermSearchableComboView)
-public class SearchableComboView: NSPopUpButton {
+open class SearchableComboView: NSPopUpButton {
     public class Panel: NSPanel {
         public override var canBecomeKey: Bool {
             return true
@@ -76,6 +86,7 @@ public class SearchableComboView: NSPopUpButton {
     private let minimumHeight = CGFloat(99)
     // Is the panel above the button?
     private var isAbove = false
+    private let defaultTitle: String
 
     private var panel: Panel {
         get {
@@ -107,13 +118,26 @@ public class SearchableComboView: NSPopUpButton {
         listViewController.delegate = self
     }
 
-    required init?(coder: NSCoder) {
-        preconditionFailure()
+    @objc open class func groups() -> [SearchableComboViewGroup] {
+        return []
     }
 
-    @objc(initWithGroups:)
-    public init(_ groups: [SearchableComboViewGroup]) {
+    open class func defaultTitleValue() -> String {
+        return "Select Action…"
+    }
+
+    required public init?(coder: NSCoder) {
+        listViewController = SearchableComboListViewController(groups: Self.groups())
+        defaultTitle = Self.defaultTitleValue()
+        super.init(coder: coder)
+        addItem(withTitle: "")
+        postInit()
+    }
+
+    @objc(initWithGroups:defaultTitle:)
+    public init(_ groups: [SearchableComboViewGroup], defaultTitle: String) {
         listViewController = SearchableComboListViewController(groups: groups)
+        self.defaultTitle = defaultTitle
         super.init(frame: NSRect.zero, pullsDown: true)
         addItem(withTitle: "")
         postInit()
@@ -125,24 +149,66 @@ public class SearchableComboView: NSPopUpButton {
         }
     }
 
+    private func removeSelection() {
+        setTitle(defaultTitle)
+        listViewController.selectedItem = nil
+    }
+
     override public func selectItem(withTag tag: Int) -> Bool {
         if let item = listViewController.item(withTag: tag) {
             setTitle(item.label)
             listViewController.selectedItem = item
             return true
         }
-        setTitle("Selection Action…")
-        listViewController.selectedItem = nil
+        removeSelection()
         return false
+    }
+
+    public func selectItem(withIdentifier identifier: NSUserInterfaceItemIdentifier) -> Bool {
+        if let item = listViewController.item(withIdentifier: identifier) {
+            setTitle(item.label)
+            listViewController.selectedItem = item
+            return true
+        }
+        removeSelection()
+        return false
+    }
+
+    open override func selectItem(withTitle title: String) {
+        if let item = listViewController.item(withTitle: title) {
+            setTitle(item.label)
+            listViewController.selectedItem = item
+            return
+        }
+        removeSelection()
     }
 
     override public func selectedTag() -> Int {
         return listViewController.tableViewController?.selectedTag ?? -1
     }
 
+
     override public func willOpenMenu(_ menu: NSMenu, with event: NSEvent) {
         menu.cancelTrackingWithoutAnimation()
         showPanel()
+    }
+
+    @objc open override var selectedItem: NSMenuItem? {
+        let item = NSMenuItem()
+        guard let myItem = listViewController.tableViewController?.selectedItem else {
+            return nil
+        }
+        item.title = myItem.label
+        item.identifier = myItem.identifier.map { NSUserInterfaceItemIdentifier($0) }
+        return item
+    }
+
+    @objc var selectedTitle: String? {
+        return selectedItem?.title
+    }
+
+    @objc var selectedIdentifier: NSUserInterfaceItemIdentifier? {
+        return selectedItem?.identifier
     }
 
     private var myFrameInScreenCoords: NSRect {
@@ -181,7 +247,7 @@ public class SearchableComboView: NSPopUpButton {
 
     private func desiredPanelFrameWhenBelow(_ desiredHeight: CGFloat) -> CGRect {
         let insets = listViewController.insets
-        let initialSize = CGSize(width: bounds.width + insets.left + insets.right,
+        let initialSize = CGSize(width: max(listViewController.widestItemWidth, bounds.width) + insets.left + insets.right,
                                  height: min(maxPanelHeightWhenBelow, desiredHeight))
         return NSRect(origin: CGPoint(x: myFrameInScreenCoords.minX - insets.left,
                                       y: myFrameInScreenCoords.maxY - initialSize.height + insets.top),
@@ -193,6 +259,12 @@ public class SearchableComboView: NSPopUpButton {
         window?.addChildWindow(panel, ordered: .above)
         panel.setFrame(desiredPanelFrame(listViewController.desiredHeight), display: true)
         listViewController.view.frame = panel.contentView!.bounds
+
+        // Work around an apparent bug in NSTableView that causes it to want to grow 16 points larger
+        // than the scrollview.
+        listViewController.tableViewController?.updateColumnWidths()
+
+        listViewController.tableView.tile()
         panel.makeKeyAndOrderFront(nil)
         listViewController.tableViewController?.selectOnlyItem()
     }

commit eb2992d96634ee8065dc16c45bc94f03c5bb264d

Fix Select Menu Item key bindings losing their menu item when only the keystroke is edited
The key binding editor reads the chosen menu item back out of its popup on
OK, via iTermMenuItemPopupView's selectedTitle/selectedIdentifier, which go
through SearchableComboView.selectedItem. That accessor returned the table
view's selected row, which is the transient row the mouse is over, rather
than the committed, checkmarked selection held in selectedTag. A row is only
ever selected by user interaction, so when the sheet restored a binding
programmatically with selectItemWithIdentifier: and the user changed only the
keystroke without opening the popup, selectedItem was nil and the parameter
was written as empty, erasing the menu item. The sheet still displayed the
right title the whole time because selectItemWithIdentifier: calls setTitle,
which is why this looked like the shortcut edit itself was at fault.

SearchableComboView.selectedItem now uses a new checkedItem accessor that
resolves selectedTag against the unfiltered rows, so it reflects a
programmatic selection and survives a search filter that hides the selected
row. This also fixes the same data loss for Toggle Setting key bindings and
for menu item pointer bindings, which read their popups the same way.

Broken since 817462d8d (v3.5.0beta3), when the plain NSPopUpButton was
replaced with the searchable combo view. Includes the rebuilt framework
binary.

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>
(cherry picked from commit 54db3916c5121b5582fd2e82f02d2ffba32251ed)

SearchableComboListView/SearchableComboListView.framework/Versions/A/_CodeSignature/CodeResources
@@ -33,21 +33,21 @@
 		<dict>
 			<key>hash2</key>
 			<data>
-			ESzT8PpIWoyqrBCx/kpRpVTuXwgGrlVVB6ZTglKpwX0=
+			NiDK+7BVSJQm0vz2+zEXhLXHihjRIHgmqi+9DTx8MwY=
 			</data>
 		</dict>
 		<key>Modules/SearchableComboListView.swiftmodule/Project/x86_64-apple-macos.swiftsourceinfo</key>
 		<dict>
 			<key>hash2</key>
 			<data>
-			5M79y5LMf+3tWYX5p94/5nDmXRKsgBogdLt/SbXQ1no=
+			jp+bWxBmbcqjEBvXxJdTS5GCZVPuGzcKg3DIdcuOQ1A=
 			</data>
 		</dict>
 		<key>Modules/SearchableComboListView.swiftmodule/arm64-apple-macos.abi.json</key>
 		<dict>
 			<key>hash2</key>
 			<data>
-			nmWWgT3YSIgP664df8vZuVAMpTrYIMPNb8WtrVS0xwU=
+			Xnd5Su9D4EE4OYDjhtQ8OJ+gMEsgI/WTbqd+5yawdFc=
 			</data>
 		</dict>
 		<key>Modules/SearchableComboListView.swiftmodule/arm64-apple-macos.private.swiftinterface</key>
@@ -75,14 +75,14 @@
 		<dict>
 			<key>hash2</key>
 			<data>
-			iPX2ZprBcr+n8gBtWbGTh+Bj/ws0k4DKjTX8LCzz86Y=
+			vvFLr2jvmRK9fPrXyfEB4n0d7b7Ao9rioC7YYwpKBNk=
 			</data>
 		</dict>
 		<key>Modules/SearchableComboListView.swiftmodule/x86_64-apple-macos.abi.json</key>
 		<dict>
 			<key>hash2</key>
 			<data>
-			nmWWgT3YSIgP664df8vZuVAMpTrYIMPNb8WtrVS0xwU=
+			Xnd5Su9D4EE4OYDjhtQ8OJ+gMEsgI/WTbqd+5yawdFc=
 			</data>
 		</dict>
 		<key>Modules/SearchableComboListView.swiftmodule/x86_64-apple-macos.private.swiftinterface</key>
@@ -110,7 +110,7 @@
 		<dict>
 			<key>hash2</key>
 			<data>
-			arwhn/6pRZggsoXoW9QizgbDvXvx6msusAcdKsiDZNg=
+			7v+JKMifq7hGyR7OcWhF9oDwMgLzM86toD2V8N90OKI=
 			</data>
 		</dict>
 		<key>Modules/module.modulemap</key>

SearchableComboListView/SearchableComboView/SearchableComboTableViewController.swift
@@ -237,6 +237,26 @@ class SearchableComboTableViewController: NSViewController {
         return filteredRows[row].item
     }
 
+    // The item with the check mark. Unlike selectedItem, this reflects the
+    // committed selection, including one made programmatically, and it
+    // survives a filter that hides the selected row.
+    var checkedItem: SearchableComboViewItem? {
+        guard let tag = selectedTag else {
+            return nil
+        }
+        for row in unfilteredRows {
+            switch row {
+            case .group(_, _):
+                break
+            case .item(item: let item, index: _):
+                if item.tag == tag {
+                    return item
+                }
+            }
+        }
+        return nil
+    }
+
     var filter: String {
         get {
             return internalFilter

SearchableComboListView/SearchableComboView/SearchableComboView.swift
@@ -195,7 +195,7 @@ open class SearchableComboView: NSPopUpButton {
 
     @objc open override var selectedItem: NSMenuItem? {
         let item = NSMenuItem()
-        guard let myItem = listViewController.tableViewController?.selectedItem else {
+        guard let myItem = listViewController.tableViewController?.checkedItem else {
             return nil
         }
         item.title = myItem.label

ModernTests/EditKeyActionMenuItemTests.m
@@ -1,0 +1,71 @@
+//
+//  EditKeyActionMenuItemTests.m
+//  ModernTests
+//
+//  End-to-end check that opening the key binding editor on a Select Menu Item
+//  binding, changing only the keystroke, and clicking OK preserves the menu
+//  item. The editor reads the menu item back out of its popup on OK, so a
+//  popup that fails to report a programmatically restored selection silently
+//  erases the binding's parameter.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "iTermEditKeyActionWindowController.h"
+#import "iTermKeystroke.h"
+
+// ok: is an IBAction wired up in the xib, so it isn't in the public header.
+@interface iTermEditKeyActionWindowController (Testing)
+- (IBAction)ok:(id)sender;
+@end
+
+@interface EditKeyActionMenuItemTests : XCTestCase
+@end
+
+@implementation EditKeyActionMenuItemTests
+
+// Finds a menu item in the app's main menu so the test only runs against a
+// menu that really contains the item it binds to.
+- (NSMenuItem *)menuItemWithIdentifier:(NSString *)identifier inMenu:(NSMenu *)menu {
+    for (NSMenuItem *item in menu.itemArray) {
+        if ([item.identifier isEqualToString:identifier]) {
+            return item;
+        }
+        if (item.hasSubmenu) {
+            NSMenuItem *found = [self menuItemWithIdentifier:identifier inMenu:item.submenu];
+            if (found) {
+                return found;
+            }
+        }
+    }
+    return nil;
+}
+
+- (void)testChangingOnlyTheKeystrokePreservesTheMenuItem {
+    NSMenuItem *menuItem = [self menuItemWithIdentifier:@"Edit Tab Title" inMenu:NSApp.mainMenu];
+    XCTAssertNotNil(menuItem, @"Test host's main menu lacks Edit Tab Title");
+
+    NSString *parameter = [NSString stringWithFormat:@"%@\n%@", menuItem.title, menuItem.identifier];
+    iTermEditKeyActionWindowController *controller =
+        [[iTermEditKeyActionWindowController alloc] initWithContext:iTermVariablesSuggestionContextSession
+                                                              mode:iTermEditKeyActionWindowControllerModeKeyboardShortcut
+                                                       profileType:ProfileTypeTerminal];
+    [controller setAction:KEY_ACTION_SELECT_MENU_ITEM
+                parameter:parameter
+                applyMode:iTermActionApplyModeCurrentSession];
+    controller.currentKeystroke = [iTermKeystroke withCharacter:'t'
+                                                  modifierFlags:NSEventModifierFlagControl];
+    // Loads the window, which populates the menu item popup from the parameter.
+    (void)controller.window;
+
+    // The user changes only the keystroke and clicks OK.
+    controller.currentKeystroke = [iTermKeystroke withCharacter:'y'
+                                                  modifierFlags:NSEventModifierFlagControl];
+    [controller ok:nil];
+
+    XCTAssertTrue(controller.ok);
+    XCTAssertEqual(controller.action, KEY_ACTION_SELECT_MENU_ITEM);
+    XCTAssertEqualObjects(controller.parameterValue, parameter);
+}
+
+@end

ModernTests/SearchableComboViewSelectionTests.swift
@@ -1,0 +1,48 @@
+//
+//  SearchableComboViewSelectionTests.swift
+//  iTerm2
+//
+//  Regression test for a bug where a programmatically selected item was
+//  forgotten unless the user opened the popup and clicked a row. That caused
+//  editing only the keystroke of a Select Menu Item key binding to erase the
+//  menu item.
+//
+
+import XCTest
+import SearchableComboListView
+
+class SearchableComboViewSelectionTests: XCTestCase {
+    private func makeComboView() -> SearchableComboView {
+        let group = SearchableComboViewGroup("Window > Tab", items: [
+            SearchableComboViewItem("Edit Tab Title", tag: 1, identifier: "Edit Tab Title"),
+            SearchableComboViewItem("Select Next Tab", tag: 2, identifier: "Select Next Tab")
+        ])
+        return SearchableComboView([group], defaultTitle: "Select Menu Item…")
+    }
+
+    func testSelectedItemReflectsProgrammaticSelectionByIdentifier() {
+        let comboView = makeComboView()
+        XCTAssertTrue(comboView.selectItem(withIdentifier: NSUserInterfaceItemIdentifier("Edit Tab Title")))
+        XCTAssertEqual(comboView.selectedItem?.title, "Edit Tab Title")
+        XCTAssertEqual(comboView.selectedItem?.identifier?.rawValue, "Edit Tab Title")
+    }
+
+    func testSelectedItemReflectsProgrammaticSelectionByTitle() {
+        let comboView = makeComboView()
+        comboView.selectItem(withTitle: "Select Next Tab")
+        XCTAssertEqual(comboView.selectedItem?.title, "Select Next Tab")
+        XCTAssertEqual(comboView.selectedItem?.identifier?.rawValue, "Select Next Tab")
+    }
+
+    func testSelectedItemIsNilWhenNothingIsSelected() {
+        let comboView = makeComboView()
+        XCTAssertNil(comboView.selectedItem)
+    }
+
+    func testFailedSelectionClearsSelection() {
+        let comboView = makeComboView()
+        XCTAssertTrue(comboView.selectItem(withIdentifier: NSUserInterfaceItemIdentifier("Edit Tab Title")))
+        XCTAssertFalse(comboView.selectItem(withIdentifier: NSUserInterfaceItemIdentifier("Nonexistent")))
+        XCTAssertNil(comboView.selectedItem)
+    }
+}